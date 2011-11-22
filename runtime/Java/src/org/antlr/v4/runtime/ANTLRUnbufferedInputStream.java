f3b0ffb8b2ffaf4e2d3d7035cea60e8d672d9f70
parrt <parrt@antlr.org>
2011-11-21 17:55:39 -0800

    almost working
    [git-p4: depot-paths = "//depot/code/antlr4/main/": change = 9423]

--- runtime/Java/src/org/antlr/v4/runtime/ANTLRUnbufferedInputStream.java
+++ runtime/Java/src/org/antlr/v4/runtime/ANTLRUnbufferedInputStream.java
@@ -1,40 +1,42 @@
 /*
  [The "BSD license"]
- Copyright (c) 2011 Terence Parr
- All rights reserved.
+  Copyright (c) 2011 Terence Parr
+  All rights reserved.
 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
 
- 1. Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
- 2. Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
- 3. The name of the author may not be used to endorse or promote products
-    derived from this software without specific prior written permission.
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+  3. The name of the author may not be used to endorse or promote products
+     derived from this software without specific prior written permission.
 
- THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package org.antlr.v4.runtime;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class ANTLRUnbufferedInputStream implements CharStream {
     /** A buffer of the data being scanned */
-   	protected char[] data = new char[256];
+   	protected int[] data = new int[256];
 
    	/** How many characters are actually in the buffer */
    	protected int n;
@@ -44,36 +46,55 @@ public class ANTLRUnbufferedInputStream implements CharStream {
 
     protected int minMarker = -1;
 
-    protected InputStream input;
+	/** Absolute char index. It's the index of the char about to be
+	 *  read via LA(1). Goes from 0 to numchar-1.
+	 */
+	protected int currentElementIndex = 0;
 
-   	/** What is name or source of this char stream? */
-   	public String name;
+    protected Reader input;
 
-    public ANTLRUnbufferedInputStream(InputStream input) {
-        this.input = input;
-        sync(1); // prime buffer with at least 1 char
-    }
+	/** What is name or source of this char stream? */
+	public String name;
 
-    @Override
-    public void consume() {
-        p++;
-        // have we hit end of buffer when no markers?
-        if ( p==n && minMarker<0 ) {
-            // if so, it's an opportunity to start filling at index 0 again
+	public ANTLRUnbufferedInputStream(InputStream input) {
+		this.input = new InputStreamReader(input);
+	}
+
+	public ANTLRUnbufferedInputStream(Reader input) {
+		this.input = input;
+	}
+
+	public void reset() {
+		p = 0;
+		minMarker = -1;
+		currentElementIndex = 0;
+		n = 0;
+	}
+
+	@Override
+	public void consume() {
+		sync(1);
+		p++;
+		// have we hit end of buffer when no markers?
+		if ( p==n && minMarker<0 ) {
+			// if so, it's an opportunity to start filling at index 0 again
             p = 0;
+			n = 0;
         }
-        sync(1); // get another to replace consumed token
     }
 
-    /** Make sure we have i tokens from current position p */
-    public void sync(int i) {
-        if ( p+i-1 > (n-1) ) {       // out of tokens?
-            int need = (p+i-1) - (n-1); // get need tokens
-            fill(need);
-        }
-    }
-    public void fill(int n) { // add n tokens
-        for (int i=1; i<=n; i++) {
+	/** Make sure we have 'need' elements from current position p. Last valid
+	 *  p index is data.size()-1.  p+need-1 is the data index 'need' elements
+	 *  ahead.  If we need 1 element, (p+1-1)==p must be < data.size().
+	 */
+	protected void sync(int want) {
+		int need = (p+want-1) - n + 1; // how many more elements we need?
+		if ( need > 0 ) fill(need);    // out of elements?
+	}
+
+	/** add n elements to buffer */
+	public void fill(int n) {
+		for (int i=1; i<=n; i++) {
             try {
                 int c = input.read();
                 add(c);
@@ -81,23 +102,24 @@ public class ANTLRUnbufferedInputStream implements CharStream {
             catch (IOException ioe) {
                 throw new RuntimeException(ioe);
             }
-        }
-    }
+		}
+	}
 
     protected void add(int c) {
         if ( p>=data.length ) {
-            char[] newdata = new char[data.length*2]; // resize
+			int[] newdata = new int[data.length*2]; // resize
             System.arraycopy(data, 0, newdata, 0, data.length);
             data = newdata;
         }
-        data[n++] = (char)c;
+        data[n++] = c;
     }
 
     @Override
     public int LA(int i) {
         sync(i);
         int index = p + i - 1;
-        if ( index<0 || index >= n ) throw new IndexOutOfBoundsException();
+        if ( index < 0 ) throw new IndexOutOfBoundsException();
+		if ( index > n ) return CharStream.EOF;
         return data[index];
     }
 
--- tool/test/org/antlr/v4/test/TestUnbufferedInputStream.java
+++ tool/test/org/antlr/v4/test/TestUnbufferedInputStream.java
@@ -1,67 +1,114 @@
 /*
  [The "BSD license"]
- Copyright (c) 2011 Terence Parr
- All rights reserved.
+  Copyright (c) 2011 Terence Parr
+  All rights reserved.
 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
 
- 1. Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
- 2. Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
- 3. The name of the author may not be used to endorse or promote products
-    derived from this software without specific prior written permission.
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+  3. The name of the author may not be used to endorse or promote products
+     derived from this software without specific prior written permission.
 
- THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package org.antlr.v4.test;
 
 import org.antlr.v4.runtime.ANTLRUnbufferedInputStream;
-import org.antlr.v4.runtime.BufferedTokenStream;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.tool.LexerGrammar;
-import org.antlr.v4.tool.interp.LexerInterpreter;
 import org.junit.Test;
 
-import java.io.StringBufferInputStream;
+import java.io.StringReader;
 
 public class TestUnbufferedInputStream extends BaseTest {
-    @Test public void testFirstToken() throws Exception {
-        LexerGrammar g = new LexerGrammar(
-                "lexer grammar t;\n"+
-                        "ID : 'a'..'z'+;\n" +
-                        "INT : '0'..'9'+;\n" +
-                        "SEMI : ';';\n" +
-                        "ASSIGN : '=';\n" +
-                        "PLUS : '+';\n" +
-                        "MULT : '*';\n" +
-                        "WS : ' '+;\n");
-        // Tokens: 012345678901234567
-        // Input:  x = 3 * 0 + 2 * 0;
-        CharStream input = new ANTLRUnbufferedInputStream(
-                new StringBufferInputStream("x = 3 * 0 + 2 * 0;")
-        );
-        LexerInterpreter lexEngine = new LexerInterpreter(g);
-        lexEngine.setInput(input);
-        BufferedTokenStream tokens = new BufferedTokenStream(lexEngine);
-        System.out.println(tokens);
-        String result = tokens.LT(1).getText();
-        String expecting = "x";
-        assertEquals(expecting, result);
-    }
+	@Test public void testNoChar() throws Exception {
+		CharStream input = new ANTLRUnbufferedInputStream(
+				new StringReader("")
+		);
+		assertEquals(CharStream.EOF, input.LA(1));
+		input.consume();
+		assertEquals(CharStream.EOF, input.LA(1));
+		input.consume();
+		assertEquals(CharStream.EOF, input.LA(1));
+	}
 
+	@Test public void test1Char() throws Exception {
+		CharStream input = new ANTLRUnbufferedInputStream(
+				new StringReader("x")
+		);
+		assertEquals('x', input.LA(1));
+		input.consume();
+		assertEquals(CharStream.EOF, input.LA(1));
+	}
 
+	@Test public void test2Char() throws Exception {
+		CharStream input = new ANTLRUnbufferedInputStream(
+				new StringReader("xy")
+		);
+		assertEquals('x', input.LA(1));
+		input.consume();
+		assertEquals('y', input.LA(1));
+		input.consume();
+		assertEquals(CharStream.EOF, input.LA(1));
+	}
+
+	@Test public void test2CharAhead() throws Exception {
+		CharStream input = new ANTLRUnbufferedInputStream(
+				new StringReader("xy")
+		);
+		assertEquals('x', input.LA(1));
+		assertEquals('y', input.LA(2));
+		assertEquals(CharStream.EOF, input.LA(3));
+	}
+
+	@Test public void test1Mark() throws Exception {
+		CharStream input = new ANTLRUnbufferedInputStream(
+				new StringReader("xyz")
+		);
+		int m = input.mark();
+		assertEquals('x', input.LA(1));
+		assertEquals('y', input.LA(2));
+		assertEquals('z', input.LA(3));
+		input.release(m);
+		assertEquals(CharStream.EOF, input.LA(4));
+	}
+
+//    @Test public void testFirstToken() throws Exception {
+//        LexerGrammar g = new LexerGrammar(
+//                "lexer grammar t;\n"+
+//                        "ID : 'a'..'z'+;\n" +
+//                        "INT : '0'..'9'+;\n" +
+//                        "SEMI : ';';\n" +
+//                        "ASSIGN : '=';\n" +
+//                        "PLUS : '+';\n" +
+//                        "MULT : '*';\n" +
+//                        "WS : ' '+;\n");
+//        // Tokens: 012345678901234567
+//        // Input:  x = 3 * 0 + 2 * 0;
+//        CharStream input = new ANTLRUnbufferedInputStream(
+//                new StringBufferInputStream("x = 3 * 0 + 2 * 0;")
+//        );
+//        LexerInterpreter lexEngine = new LexerInterpreter(g);
+//        lexEngine.setInput(input);
+//        BufferedTokenStream tokens = new BufferedTokenStream(lexEngine);
+//        System.out.println(tokens);
+//        String result = tokens.LT(1).getText();
+//        String expecting = "x";
+//        assertEquals(expecting, result);
+//    }
 }
