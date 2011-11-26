30d89abc4a26ae73e5dbbd849c8f8e4a7799e370
parrt <parrt@antlr.org>
2011-11-25 18:16:46 -0800

    rm type param
    [git-p4: depot-paths = "//depot/code/antlr4/main/": change = 9462]

--- runtime/Java/src/org/antlr/v4/runtime/BailErrorStrategy.java
+++ runtime/Java/src/org/antlr/v4/runtime/BailErrorStrategy.java
@@ -32,7 +32,7 @@ package org.antlr.v4.runtime;
 /** Bail out of parser at first syntax error. Do this to use it:
  *      myparser.setErrorHandler(new BailErrorStrategy<Token>());
  */
-public class BailErrorStrategy<Symbol> extends DefaultErrorStrategy<Symbol> {
+public class BailErrorStrategy extends DefaultErrorStrategy<Token> {
     /** Instead of recovering from exception e, Re-throw wrote it wrapped
      *  in a generic RuntimeException so it is not caught by the
      *  rule function catches.  Exception e is the "cause" of the
@@ -47,7 +47,7 @@ public class BailErrorStrategy<Symbol> extends DefaultErrorStrategy<Symbol> {
      *  successfully recovers, it won't throw an exception.
      */
     @Override
-    public Symbol recoverInline(BaseRecognizer recognizer)
+    public Token recoverInline(BaseRecognizer recognizer)
         throws RecognitionException
     {
         throw new RuntimeException(new InputMismatchException(recognizer));
