40af40282233da9ddbdb8a405cd03c3f0146ac36
Sam Harwell <sam@tunnelvisionlabs.com>
2012-02-15 10:47:50 -0600

    Allow enabling TEST_IN_SAME_PROCESS via jvm argument

--- tool/test/org/antlr/v4/test/BaseTest.java
+++ tool/test/org/antlr/v4/test/BaseTest.java
@@ -71,7 +71,7 @@ public abstract class BaseTest {
 	public static final String newline = System.getProperty("line.separator");
 	public static final String pathSep = System.getProperty("path.separator");
 
-	public static final boolean TEST_IN_SAME_PROCESS = false;
+	public static final boolean TEST_IN_SAME_PROCESS = Boolean.parseBoolean(System.getProperty("antlr.testinprocess"));
 
     /**
      * Build up the full classpath we need, including the surefire path (if present)
