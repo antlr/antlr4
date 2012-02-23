aebd59fa66e90d878da43f6d559cc149f4c913ae
Sam Harwell <sam@tunnelvisionlabs.com>
2012-02-23 10:27:35 -0600

    Extract interface from ParseTreeVisitor. Current ParseTreeVisitor class renamed to AbstractParseTreeVisitor.

--- runtime/Java/src/org/antlr/v4/runtime/tree/AbstractParseTreeVisitor.java
+++ runtime/Java/src/org/antlr/v4/runtime/tree/AbstractParseTreeVisitor.java
@@ -31,16 +31,39 @@ package org.antlr.v4.runtime.tree;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 
-/**
- *
- * @author Sam Harwell
- */
-public interface ParseTreeVisitor<T> {
+/** T is return type of visit methods. Use T=Void for no return type. */
+public abstract class AbstractParseTreeVisitor<T> implements ParseTreeVisitor<T> {
+	@Override
+	public T visit(ParserRuleContext<?> ctx) {
+		return ctx.accept(this);
+	}
 
-	T visit(ParserRuleContext<?> ctx);
+	/** Visit all rule, nonleaf children. Not that useful if you are using T as
+	 *  non-Void.  This returns value returned from last child visited,
+	 *  losing all computations from first n-1 children.  Works fine for
+	 *  ctxs with one child then.
+	 *  Handy if you are just walking the tree with a visitor and only
+	 *  care about some nodes.  The ParserRuleContext.accept() method
+	 *  walks all children by default; i.e., calls this method.
+	 */
+	@Override
+	public T visitChildren(ParserRuleContext<? extends Token> ctx) {
+		T result = null;
+		for (ParseTree c : ctx.children) {
+			if ( c instanceof ParseTree.RuleNode) {
+				ParseTree.RuleNode r = (ParseTree.RuleNode)c;
+				ParserRuleContext<?> rctx = (ParserRuleContext<? extends Token>)r.getRuleContext();
+				result = visit(rctx);
+			}
+			else {
+				result = visitTerminal(ctx, ((ParseTree.TerminalNode<? extends Token>)c).getSymbol());
+			}
+		}
+		return result;
+	}
 
-	T visitChildren(ParserRuleContext<? extends Token> ctx);
-
-	T visitTerminal(ParserRuleContext<? extends Token> ctx, Token symbol);
-
+	@Override
+	public T visitTerminal(ParserRuleContext<? extends Token> ctx, Token symbol) {
+		return null;
+	}
 }
