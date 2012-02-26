e0949987181276ed23fe580ffaefa0e0ccba3848
Sam Harwell <sam@tunnelvisionlabs.com>
2012-02-26 17:15:49 -0600

    Generic Graph<T>

--- tool/test/org/antlr/v4/test/TestTopologicalSort.java
+++ tool/test/org/antlr/v4/test/TestTopologicalSort.java
@@ -37,7 +37,7 @@ import java.util.List;
 public class TestTopologicalSort extends BaseTest {
     @Test
     public void testFairlyLargeGraph() throws Exception {
-        Graph g = new Graph();
+        Graph<String> g = new Graph<String>();
         g.addEdge("C", "F");
         g.addEdge("C", "G");
         g.addEdge("C", "A");
@@ -51,63 +51,63 @@ public class TestTopologicalSort extends BaseTest {
         g.addEdge("E", "F");
 
         String expecting = "[H, F, E, D, G, A, B, C]";
-        List nodes = g.sort();
+        List<String> nodes = g.sort();
         String result = nodes.toString();
         assertEquals(expecting, result);
     }
 
     @Test
     public void testCyclicGraph() throws Exception {
-        Graph g = new Graph();
+        Graph<String> g = new Graph<String>();
         g.addEdge("A", "B");
         g.addEdge("B", "C");
         g.addEdge("C", "A");
         g.addEdge("C", "D");
 
         String expecting = "[D, C, B, A]";
-        List nodes = g.sort();
+        List<String> nodes = g.sort();
         String result = nodes.toString();
         assertEquals(expecting, result);
     }
 
     @Test
     public void testRepeatedEdges() throws Exception {
-        Graph g = new Graph();
+        Graph<String> g = new Graph<String>();
         g.addEdge("A", "B");
         g.addEdge("B", "C");
         g.addEdge("A", "B"); // dup
         g.addEdge("C", "D");
 
         String expecting = "[D, C, B, A]";
-        List nodes = g.sort();
+        List<String> nodes = g.sort();
         String result = nodes.toString();
         assertEquals(expecting, result);
     }
 
     @Test
     public void testSimpleTokenDependence() throws Exception {
-        Graph g = new Graph();
+        Graph<String> g = new Graph<String>();
         g.addEdge("Java.g", "MyJava.tokens"); // Java feeds off manual token file
         g.addEdge("Java.tokens", "Java.g");
         g.addEdge("Def.g", "Java.tokens");    // walkers feed off generated tokens
         g.addEdge("Ref.g", "Java.tokens");
 
         String expecting = "[MyJava.tokens, Java.g, Java.tokens, Ref.g, Def.g]";
-        List nodes = g.sort();
+        List<String> nodes = g.sort();
         String result = nodes.toString();
         assertEquals(expecting, result);
     }
 
     @Test
     public void testParserLexerCombo() throws Exception {
-        Graph g = new Graph();
+        Graph<String> g = new Graph<String>();
         g.addEdge("JavaLexer.tokens", "JavaLexer.g");
         g.addEdge("JavaParser.g", "JavaLexer.tokens");
         g.addEdge("Def.g", "JavaLexer.tokens");
         g.addEdge("Ref.g", "JavaLexer.tokens");
 
         String expecting = "[JavaLexer.g, JavaLexer.tokens, JavaParser.g, Ref.g, Def.g]";
-        List nodes = g.sort();
+        List<String> nodes = g.sort();
         String result = nodes.toString();
         assertEquals(expecting, result);
     }
