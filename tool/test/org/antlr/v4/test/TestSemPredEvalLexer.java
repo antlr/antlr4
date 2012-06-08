b255509e961bfa798e209c4656738b2937d2b1c3
Terence Parr <parrt@antlr.org>
2012-06-07 17:31:18 -0700

    fix a bug related to semantic predicates in the lexer and generally cleaned up variable and method names in the simulator. I moved all of the predicates to the right side of lexer rules in the unit tests. Later, we should ensure that predicates only occur on the right edge of lexer rules. We should state that the rule is not been accepted so we can't test things like getText(), we have to use more raw indexes into the character stream. In the lexer simulator, the addDFAState() method now does not try to compute whether there is a predicate in the configurations. That information has already been set into the ATNConfigSet by the getEpsilonTarget() method. [I should also point out that I have not tested the Java parsing in a while and now it hits a landmine on a number of common Java files in jdk :(. I'm not sure where that crept in]

--- tool/test/org/antlr/v4/test/TestSemPredEvalLexer.java
+++ tool/test/org/antlr/v4/test/TestSemPredEvalLexer.java
@@ -1,6 +1,5 @@
 package org.antlr.v4.test;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestSemPredEvalLexer extends BaseTest {
@@ -8,31 +7,15 @@ public class TestSemPredEvalLexer extends BaseTest {
 	@Test public void testDisableRule() throws Exception {
 		String grammar =
 			"lexer grammar L;\n"+
-			"E1 : {false}? 'enum' ;\n" +
-			"E2 : {true}? 'enum' ;\n" +  // winner not E1 or ID
+			"E1 : 'enum' {false}? ;\n" +
+			"E2 : 'enum' {true}? ;\n" +  // winner not E1 or ID
 			"ID : 'a'..'z'+ ;\n"+
 			"WS : (' '|'\\n') {skip();} ;";
 		String found = execLexer("L.g4", grammar, "L", "enum abc", true);
 		String expecting =
 			"[@0,0:3='enum',<2>,1:0]\n" +
 			"[@1,5:7='abc',<3>,1:5]\n" +
-			"[@2,8:7='<EOF>',<-1>,1:8]\n"; // no dfa since preds on left edge
-		assertEquals(expecting, found);
-	}
-
-	@Test public void testDisableRuleAfterMatch() throws Exception {
-		String grammar =
-			"lexer grammar L;\n"+
-			"E1 : 'enum' {false}? ;\n" +
-			"E2 : 'enum' {true}? ;\n" +  // winner not E1 or ID
-			"ID : 'a'..'z'+ ;\n"+
-			"WS : (' '|'\\n') {skip();} ;";
-		String found = execLexer("L.g4", grammar, "L", "enum abc enum", true);
-		String expecting =
-			"[@0,0:3='enum',<2>,1:0]\n" +
-			"[@1,5:7='abc',<3>,1:5]\n" +
-			"[@2,9:12='enum',<2>,1:9]\n" +
-			"[@3,13:12='<EOF>',<-1>,1:13]\n" +
+			"[@2,8:7='<EOF>',<-1>,1:8]\n" +
 			"s0-' '->:s4=>4\n" +
 			"s0-'a'->:s5=>3\n" +
 			"s0-'e'->:s1=>3\n" +
@@ -60,25 +43,51 @@ public class TestSemPredEvalLexer extends BaseTest {
 			"s0-'e'->:s1=>2\n" +
 			":s1=>2-'n'->:s2=>2\n" +
 			":s2=>2-'u'->:s3=>2\n" +
-			":s3=>2-'m'->:s4=>2\n" +
 			":s4=>2-'b'->:s4=>2\n" +
-			":s4=>2-'c'->:s4=>2\n";
+			":s4=>2-'c'->:s4=>2\n"; // no 'm'-> transition...conflicts with pred
 		assertEquals(expecting, found);
 	}
 
-	@Ignore
-	public void testMatchNChar() throws Exception { // can't do locals yet
+	@Test public void testIDnotEnum() throws Exception {
 		String grammar =
 			"lexer grammar L;\n"+
-			"B : {int n=0;} ({n<=2}? DIGIT {n++})+ ;\n" +
-			"fragment DIGIT : '0'..'9' ;\n"+
+			"ENUM : [a-z]+ {false}? ;\n" +
+			"ID   : [a-z]+ ;\n"+
 			"WS : (' '|'\\n') {skip();} ;";
-		String found = execLexer("L.g4", grammar, "L", "1234 56", true);
+		String found = execLexer("L.g4", grammar, "L", "enum abc enum", true);
 		String expecting =
-			"[@0,0:3='enum',<4>,1:0]\n" +
-			"[@1,5:7='abc',<5>,1:5]\n" +
-			"[@2,8:8='<EOF>',<-1>,1:8]\n"; // no dfa since preds on left edge
+			"[@0,0:3='enum',<2>,1:0]\n" +
+			"[@1,5:7='abc',<2>,1:5]\n" +
+			"[@2,9:12='enum',<2>,1:9]\n" +
+			"[@3,13:12='<EOF>',<-1>,1:13]\n" +
+			"s0-' '->:s2=>3\n"; // no DFA for enum/id. all paths lead to pred.
 		assertEquals(expecting, found);
 	}
 
+	@Test public void testIndent() throws Exception {
+		String grammar =
+			"lexer grammar L;\n"+
+			"ID : [a-z]+ ;\n"+
+			"INDENT : [ \\t]+ {_tokenStartCharPositionInLine==0}? ;"+
+			"NL     : '\\n' ;"+
+			"WS     : [ \\t]+ ;";
+		String found = execLexer("L.g4", grammar, "L", "abc\n  def  \n", true);
+		String expecting =
+			"[@0,0:2='abc',<1>,1:0]\n" +		// ID
+			"[@1,3:3='\\n',<3>,1:3]\n" +  		// NL
+			"[@2,4:5='  ',<2>,2:0]\n" +			// INDENT
+			"[@3,6:8='def',<1>,2:2]\n" +		// ID
+			"[@4,9:10='  ',<4>,2:5]\n" +		// WS
+			"[@5,11:11='\\n',<3>,2:7]\n" +
+			"[@6,12:11='<EOF>',<-1>,3:8]\n" +
+			"s0-'\n" +
+			"'->:s2=>3\n" +
+			"s0-'a'->:s1=>1\n" +
+			"s0-'d'->:s1=>1\n" +
+			":s1=>1-'b'->:s1=>1\n" +
+			":s1=>1-'c'->:s1=>1\n" +
+			":s1=>1-'e'->:s1=>1\n" +
+			":s1=>1-'f'->:s1=>1\n";
+		assertEquals(expecting, found);
+	}
 }
