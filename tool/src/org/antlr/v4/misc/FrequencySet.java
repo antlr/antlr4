2056e019a5a5a24a26d1e2c0c9062e25665924e6
Sam Harwell <sam@tunnelvisionlabs.com>
2012-07-18 15:05:27 -0500

    Use HashMap instead of Hashtable

--- tool/src/org/antlr/v4/misc/FrequencySet.java
+++ tool/src/org/antlr/v4/misc/FrequencySet.java
@@ -29,10 +29,10 @@
 
 package org.antlr.v4.misc;
 
-import java.util.Hashtable;
+import java.util.HashMap;
 
 /** Count how many of each key we have; not thread safe */
-public class FrequencySet<T> extends Hashtable<T, MutableInt> {
+public class FrequencySet<T> extends HashMap<T, MutableInt> {
 	public int count(T key) {
 		MutableInt value = get(key);
 		if (value == null) return 0;
