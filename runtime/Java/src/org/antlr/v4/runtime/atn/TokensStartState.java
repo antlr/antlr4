0c073062f73b5258470811443600c1b39b4ed805
Sam Harwell <sam@tunnelvisionlabs.com>
2012-08-12 11:52:52 -0500

    TokensStartState is not really a BlockStartState because it doesn't have an associated end state

--- runtime/Java/src/org/antlr/v4/runtime/atn/TokensStartState.java
+++ runtime/Java/src/org/antlr/v4/runtime/atn/TokensStartState.java
@@ -30,5 +30,5 @@
 package org.antlr.v4.runtime.atn;
 
 /** The Tokens rule start state linking to each lexer rule start state */
-public class TokensStartState extends BlockStartState {
+public class TokensStartState extends DecisionState {
 }
