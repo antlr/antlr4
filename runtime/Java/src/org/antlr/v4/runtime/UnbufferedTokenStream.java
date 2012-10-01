cd2589048629501dd8b427e2b29ebe51de87b1d9
Sam Harwell <sam@tunnelvisionlabs.com>
2012-10-01 08:30:39 -0500

    Many updates to UnbufferedTokenStream:
    * Fix LT(-1) after a seek operation
    * Prevent consume() after EOF is reached
    * Use the EOF token provided by the TokenSource (no need to create a new one)
    * Fix assigned token index when marks are in place and when add() is called by derived types
    * Throw exception for some invalid use of mark/release/seek
    * Prevent fill() from adding multiple EOF tokens
    * Remove diagnostic println
    * Updated field documentation
    * Implement getTokenSource

--- runtime/Java/src/org/antlr/v4/runtime/UnbufferedTokenStream.java
+++ runtime/Java/src/org/antlr/v4/runtime/UnbufferedTokenStream.java
@@ -1,44 +1,91 @@
+/*
+ * [The "BSD license"]
+ *  Copyright (c) 2012 Terence Parr
+ *  Copyright (c) 2012 Sam Harwell
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.antlr.v4.runtime;
 
 import org.antlr.v4.runtime.misc.Interval;
-
-import java.util.Arrays;
+import org.antlr.v4.runtime.misc.NotNull;
 
 public class UnbufferedTokenStream<T extends Token> implements TokenStream {
 	protected TokenSource tokenSource;
 
-	/** A moving window buffer of the data being scanned. While there's a
-	 *  marker, we keep adding to buffer.  Otherwise, consume() resets
-	 *  so we start filling at index 0 again.
+	/**
+	 * A moving window buffer of the data being scanned. While there's a marker,
+	 * we keep adding to buffer. Otherwise, {@link #consume consume()} resets so
+	 * we start filling at index 0 again.
 	 */
 	protected Token[] tokens;
 
-	/** How many tokens are actually in the buffer; this is not
-	 *  the buffer size, that's tokens.length.
+	/**
+	 * The number of tokens currently in {@link #tokens tokens}.
+	 * <p/>
+	 * This is not the buffer capacity, that's {@code tokens.length}.
 	 */
 	protected int n;
 
-	/** 0..n-1 index into tokens of next token; tokens[p] is LT(1).
-	 *  If p == n, we are out of buffered tokens.
+	/**
+	 * 0..n-1 index into {@link #tokens tokens} of next token.
+	 * <p/>
+	 * The {@code LT(1)} token is {@code tokens[p]}. If {@code p == n}, we are
+	 * out of buffered tokens.
 	 */
 	protected int p=0;
 
-	/** Count up with mark() and down with release(). When we release()
-	 *  and hit zero, reset buffer to beginning. Copy data[p]..data[n-1]
-	 *  to data[0]..data[(n-1)-p].
+	/**
+	 * Count up with {@link #mark mark()} and down with
+	 * {@link #release release()}. When we {@code release()} the last mark,
+	 * {@code numMarkers} reaches 0 and we reset the buffer. Copy
+	 * {@code tokens[p]..tokens[n-1]} to {@code tokens[0]..tokens[(n-1)-p]}.
 	 */
 	protected int numMarkers = 0;
 
+	/**
+	 * This is the {@code LT(-1)} token for the current position.
+	 */
 	protected Token lastToken;
 
-	/** Absolute token index. It's the index of the token about to be
-	 *  read via LA(1). Goes from 0 to numtokens-1 in entire stream.
+	/**
+	 * When {@code numMarkers > 0}, this is the {@code LT(-1)} token for the
+	 * first token in {@link #tokens}. Otherwise, this is {@code null}.
 	 */
-	protected int currentTokenIndex = 0; // simple counter to set token index in tokens
+	protected Token lastTokenBufferStart;
 
-    /** Skip tokens on any channel but this one; this is how we skip whitespace... */
-	//  TODO: skip off-channel tokens!!!
-    protected int channel = Token.DEFAULT_CHANNEL;
+	/**
+	 * Absolute token index. It's the index of the token about to be read via
+	 * {@code LT(1)}. Goes from 0 to the number of tokens in the entire stream,
+	 * although the stream size is unknown before the end is reached.
+	 * <p/>
+	 * This value is used to set the token indexes if the stream provides tokens
+	 * that implement {@link WritableToken}.
+	 */
+	protected int currentTokenIndex = 0;
 
 	public UnbufferedTokenStream(TokenSource tokenSource) {
 		this(tokenSource, 256);
@@ -63,29 +110,32 @@ public class UnbufferedTokenStream<T extends Token> implements TokenStream {
 
 	@Override
 	public Token LT(int i) {
-		if ( i==-1 ) return lastToken; // special case
-        sync(i);
-        int index = p + i - 1;
-        if ( index < 0 ) throw new IndexOutOfBoundsException("LT("+i+") gives negative index");
-		if ( index > n ) {
-			TokenFactory<?> factory = tokenSource.getTokenFactory();
-			int cpos = tokenSource.getCharPositionInLine();
-			// The character position for EOF is one beyond the position of
-			// the previous token's last character
-			Token eof = factory.create(tokenSource, Token.EOF, null, Token.DEFAULT_CHANNEL,
-									   index(), index()-1,
-									   tokenSource.getLine(), cpos);
-			return eof;
+		if ( i==-1 ) {
+			return lastToken;
 		}
-        return tokens[index];
+
+		sync(i);
+        int index = p + i - 1;
+        if ( index < 0 ) {
+			throw new IndexOutOfBoundsException("LT("+i+") gives negative index");
+		}
+
+		if ( index >= n ) {
+			assert n > 0 && tokens[n-1].getType() == Token.EOF;
+			return tokens[n-1];
+		}
+
+		return tokens[index];
 	}
 
 	@Override
-	public int LA(int i) { return LT(i).getType(); }
+	public int LA(int i) {
+		return LT(i).getType();
+	}
 
 	@Override
 	public TokenSource getTokenSource() {
-		return null;
+		return tokenSource;
 	}
 
 	@Override
@@ -105,80 +155,101 @@ public class UnbufferedTokenStream<T extends Token> implements TokenStream {
 
 	@Override
 	public void consume() {
-		// buf always has at least data[p==0] in this method due to ctor
-		if ( p==0 ) lastToken = null; // we're at first token; no LA(-1)
-		else lastToken = tokens[p];   // track last char for LT(-1)
+		if (LA(1) == Token.EOF) {
+			throw new IllegalStateException("cannot consume EOF");
+		}
+
+		// buf always has at least tokens[p==0] in this method due to ctor
+		lastToken = tokens[p];   // track last token for LT(-1)
 
 		// if we're at last token and no markers, opportunity to flush buffer
-		if ( p == n-1 && numMarkers==0 ) { // can we release buffer?
-//			System.out.println("consume: reset");
+		if ( p == n-1 && numMarkers==0 ) {
 			n = 0;
 			p = -1; // p++ will leave this at 0
+			lastTokenBufferStart = lastToken;
 		}
 
 		p++;
 		currentTokenIndex++;
-//		System.out.println("consume p="+p+", numMarkers="+numMarkers+
-//						   ", currentCharIndex="+currentCharIndex+", n="+n);
 		sync(1);
 	}
 
-	/** Make sure we have 'need' elements from current position p. Last valid
-	 *  p index is tokens.size()-1.  p+need-1 is the tokens index 'need' elements
-	 *  ahead.  If we need 1 element, (p+1-1)==p must be < tokens.size().
+	/** Make sure we have 'need' elements from current position {@link #p p}. Last valid
+	 *  {@code p} index is {@code tokens.length-1}.  {@code p+need-1} is the tokens index 'need' elements
+	 *  ahead.  If we need 1 element, {@code (p+1-1)==p} must be less than {@code tokens.length}.
 	 */
 	protected void sync(int want) {
 		int need = (p+want-1) - n + 1; // how many more elements we need?
-		if ( need > 0 ) fill(need);    // out of elements?
-	}
-
-	/** add n elements to buffer */
-	public void fill(int n) {
-		for (int i=1; i<=n; i++) {
-			Token t = tokenSource.nextToken();
-			if ( t instanceof WritableToken ) {
-				((WritableToken)t).setTokenIndex(currentTokenIndex);
-			}
-			add(t);
+		if ( need > 0 ) {
+			fill(need);
 		}
 	}
 
-	protected void add(Token t) {
+	/**
+	 * Add {@code n} elements to the buffer. Returns the number of tokens
+	 * actually added to the buffer. If the return value is less than {@code n},
+	 * then EOF was reached before {@code n} tokens could be added.
+	 */
+	protected int fill(int n) {
+		for (int i=0; i<n; i++) {
+			if (this.n > 0 && tokens[this.n-1].getType() == Token.EOF) {
+				return i;
+			}
+
+			Token t = tokenSource.nextToken();
+			add(t);
+		}
+
+		return n;
+	}
+
+	protected void add(@NotNull Token t) {
 		if ( n>=tokens.length ) {
 			Token[] newtokens = new Token[tokens.length*2]; // resize
 			System.arraycopy(tokens, 0, newtokens, 0, tokens.length);
 			tokens = newtokens;
 		}
+
+		if (t instanceof WritableToken) {
+			((WritableToken)t).setTokenIndex(getBufferStartIndex() + n);
+		}
+
 		tokens[n++] = t;
 	}
 
-
-	/** Return a marker that we can release later.  Marker happens to be
-	 *  index into buffer (not index()).
+	/**
+	 * Return a marker that we can release later.
+	 * <p/>
+	 * The specific marker value used for this class allows for some level of
+	 * protection against misuse where {@code seek()} is called on a mark or
+	 * {@code release()} is called in the wrong order.
 	 */
 	@Override
 	public int mark() {
-		int m = p;
+		if (numMarkers == 0) {
+			lastTokenBufferStart = lastToken;
+		}
+
+		int mark = -numMarkers - 1;
 		numMarkers++;
-		return m;
+		return mark;
 	}
 
 	@Override
 	public void release(int marker) {
-		if ( numMarkers==0 ) {
-			throw new IllegalStateException("release() called w/o prior matching mark()");
+		int expectedMark = -numMarkers;
+		if ( marker!=expectedMark ) {
+			throw new IllegalStateException("release() called with an invalid marker.");
 		}
-//		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-//		System.out.println(stackTrace[2].getMethodName()+": release " + marker);
+
 		numMarkers--;
 		if ( numMarkers==0 ) { // can we release buffer?
-			System.out.println("release: shift "+p+".."+(n-1)+" to 0: '"+
-								   Arrays.toString(Arrays.copyOfRange(tokens,p,n))+"'");
-			// Copy data[p]..data[n-1] to data[0]..data[(n-1)-p], reset ptrs
+			// Copy tokens[p]..tokens[n-1] to tokens[0]..tokens[(n-1)-p], reset ptrs
 			// p is last valid token; move nothing if p==n as we have no valid char
-			System.arraycopy(tokens, p, tokens, 0, n - p); // shift n-p char from p to 0
+			System.arraycopy(tokens, p, tokens, 0, n - p); // shift n-p tokens from p to 0
 			n = n - p;
 			p = 0;
+			lastTokenBufferStart = lastToken;
 		}
 	}
 
@@ -189,13 +260,33 @@ public class UnbufferedTokenStream<T extends Token> implements TokenStream {
 
 	@Override
 	public void seek(int index) { // seek to absolute index
+		if (index == currentTokenIndex) {
+			return;
+		}
+
+		if (index > currentTokenIndex) {
+			sync(index - currentTokenIndex);
+			index = Math.min(index, getBufferStartIndex() + n - 1);
+		}
+
 		int bufferStartIndex = getBufferStartIndex();
 		int i = index - bufferStartIndex;
-		if ( i < 0 || i >= n ) {
+		if ( i < 0 ) {
+			throw new IllegalArgumentException("cannot seek to negative index " + index);
+		}
+		else if (i >= n) {
 			throw new UnsupportedOperationException("seek to index outside buffer: "+
 													index+" not in "+ bufferStartIndex +".."+(bufferStartIndex +n));
 		}
+
 		p = i;
+		currentTokenIndex = index;
+		if (p == 0) {
+			lastToken = lastTokenBufferStart;
+		}
+		else {
+			lastToken = tokens[p-1];
+		}
 	}
 
 	@Override
@@ -232,7 +323,7 @@ public class UnbufferedTokenStream<T extends Token> implements TokenStream {
 		return buf.toString();
 	}
 
-	public int getBufferStartIndex() {
+	protected final int getBufferStartIndex() {
 		return currentTokenIndex - p;
 	}
 }
