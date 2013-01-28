816d3ee77929dad2ca44cf372aeadf3c407b9d78
Tim Neumann <mail@tim-neumann.eu>
2013-01-28 17:57:33 +0100

    consistently use tabs for indentation

--- tool/src/org/antlr/v4/Tool.java
+++ tool/src/org/antlr/v4/Tool.java
@@ -128,8 +128,8 @@ public class Tool {
 	public String msgFormat = "antlr";
 	public boolean launch_ST_inspector = false;
 	public boolean ST_inspector_wait_for_close = false;
-    public boolean force_atn = false;
-    public boolean log = false;
+	public boolean force_atn = false;
+	public boolean log = false;
 	public boolean gen_listener = true;
 	public boolean gen_visitor = false;
 	public boolean gen_dependencies = false;
@@ -138,11 +138,11 @@ public class Tool {
 	public boolean warnings_are_errors = false;
 	public boolean longMessages = false;
 
-    public static Option[] optionDefs = {
+	public static Option[] optionDefs = {
 		new Option("inputDirectory",	"-in", OptionArgType.STRING, "specify directory to search for input files"),
-        new Option("outputDirectory",	"-o", OptionArgType.STRING, "specify output directory where all output is generated"),
-        new Option("libDirectory",		"-lib", OptionArgType.STRING, "specify location of grammars, tokens files"),
-        new Option("generate_ATN_dot",	"-atn", "generate rule augmented transition network diagrams"),
+		new Option("outputDirectory",	"-o", OptionArgType.STRING, "specify output directory where all output is generated"),
+		new Option("libDirectory",		"-lib", OptionArgType.STRING, "specify location of grammars, tokens files"),
+		new Option("generate_ATN_dot",	"-atn", "generate rule augmented transition network diagrams"),
 		new Option("grammarEncoding",	"-encoding", OptionArgType.STRING, "specify grammar file encoding; e.g., euc-jp"),
 		new Option("msgFormat",			"-message-format", OptionArgType.STRING, "specify output style for messages in antlr, gnu, vs2005"),
 		new Option("longMessages",		"-long-messages", "show exception details when available for errors and warnings"),
@@ -154,9 +154,9 @@ public class Tool {
 		new Option("gen_dependencies",	"-depend", "generate file dependencies"),
 		new Option("",					"-D<option>=value", "set/override a grammar-level option"),
 		new Option("warnings_are_errors", "-Werror", "treat warnings as errors"),
-        new Option("launch_ST_inspector", "-XdbgST", "launch StringTemplate visualizer on generated code"),
+		new Option("launch_ST_inspector", "-XdbgST", "launch StringTemplate visualizer on generated code"),
 		new Option("ST_inspector_wait_for_close", "-XdbgSTWait", "wait for STViz to close before continuing"),
-        new Option("force_atn",			"-Xforce-atn", "use the ATN simulator for all predictions"),
+		new Option("force_atn",			"-Xforce-atn", "use the ATN simulator for all predictions"),
 		new Option("log",   			"-Xlog", "dump lots of logging info to antlr-timestamp.log"),
 	};
 
@@ -164,9 +164,9 @@ public class Tool {
 	protected boolean haveOutputDir = false;
 	protected boolean return_dont_exit = false;
 
-    // The internal options are for my use on the command line during dev
-    public static boolean internalOption_PrintGrammarTree = false;
-    public static boolean internalOption_ShowATNConfigsInDFA = false;
+	// The internal options are for my use on the command line during dev
+	public static boolean internalOption_PrintGrammarTree = false;
+	public static boolean internalOption_ShowATNConfigsInDFA = false;
 
 
 	public final String[] args;
@@ -174,7 +174,7 @@ public class Tool {
 	protected List<String> grammarFiles = new ArrayList<String>();
 
 	public ErrorManager errMgr;
-    public LogManager logMgr = new LogManager();
+	public LogManager logMgr = new LogManager();
 
 	List<ANTLRToolListener> listeners = new CopyOnWriteArrayList<ANTLRToolListener>();
 
@@ -184,23 +184,23 @@ public class Tool {
 	DefaultToolListener defaultListener = new DefaultToolListener(this);
 
 	public static void main(String[] args) {
-        Tool antlr = new Tool(args);
-        if ( args.length == 0 ) { antlr.help(); antlr.exit(0); }
+		Tool antlr = new Tool(args);
+		if ( args.length == 0 ) { antlr.help(); antlr.exit(0); }
 
-        try {
-            antlr.processGrammarsOnCommandLine();
-        }
-        finally {
-            if ( antlr.log ) {
-                try {
-                    String logname = antlr.logMgr.save();
-                    System.out.println("wrote "+logname);
-                }
-                catch (IOException ioe) {
-                    antlr.errMgr.toolError(ErrorType.INTERNAL_ERROR, ioe);
-                }
-            }
-        }
+		try {
+			antlr.processGrammarsOnCommandLine();
+		}
+		finally {
+			if ( antlr.log ) {
+				try {
+					String logname = antlr.logMgr.save();
+					System.out.println("wrote "+logname);
+				}
+				catch (IOException ioe) {
+					antlr.errMgr.toolError(ErrorType.INTERNAL_ERROR, ioe);
+				}
+			}
+		}
 		if ( antlr.return_dont_exit ) return;
 
 		if (antlr.errMgr.getNumErrors() > 0) {
@@ -670,7 +670,7 @@ public class Tool {
 						writeDOTFile(g, r, dot);
 					}
 				}
-                catch (IOException ioe) {
+				catch (IOException ioe) {
 					errMgr.toolError(ErrorType.CANNOT_WRITE_FILE, ioe);
 				}
 			}
@@ -810,8 +810,8 @@ public class Tool {
 		}
 	}
 
-    public void log(@Nullable String component, String msg) { logMgr.log(component, msg); }
-    public void log(String msg) { log(null, msg); }
+	public void log(@Nullable String component, String msg) { logMgr.log(component, msg); }
+	public void log(String msg) { log(null, msg); }
 
 	public int getNumErrors() { return errMgr.getNumErrors(); }
 
