8d4c3252f5ae27684633eb4d3175aa95624f8fdf
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-16 09:45:18 -0600

    Add Runtime.Substring and Arrays.CopyOf

---
+++ Antlr4.Runtime/Sharpen/Arrays.cs
@@ -0,0 +1,13 @@
+﻿namespace Sharpen
+{
+    using System;
+
+    internal static class Arrays
+    {
+        public static T[] CopyOf<T>(T[] array, int newSize)
+        {
+            Array.Resize(ref array, newSize);
+            return array;
+        }
+    }
+}
---
+++ Antlr4.Runtime/Sharpen/Runtime.cs
@@ -0,0 +1,15 @@
+﻿namespace Sharpen
+{
+    using System;
+
+    internal static class Runtime
+    {
+        public static string Substring(string str, int beginOffset, int endOffset)
+        {
+            if (str == null)
+                throw new ArgumentNullException("str");
+
+            return str.Substring(beginOffset, endOffset - beginOffset);
+        }
+    }
+}
