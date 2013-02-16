bfbcc2816cfeb66a84f5c1537f4a17b6ac876453
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-16 13:58:52 -0600

    Add Arrays.AsList

--- Antlr4.Runtime/Sharpen/Arrays.cs
+++ Antlr4.Runtime/Sharpen/Arrays.cs
@@ -1,12 +1,18 @@
 ﻿namespace Sharpen
 {
     using System;
+    using System.Collections.Generic;
 
     internal static class Arrays
     {
         public static T[] CopyOf<T>(T[] array, int newSize)
         {
             Array.Resize(ref array, newSize);
+            return array;
+        }
+
+        public static IList<T> AsList<T>(params T[] array)
+        {
             return array;
         }
     }
