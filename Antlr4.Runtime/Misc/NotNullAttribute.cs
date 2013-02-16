3ba462d403b00ab77ee2f9574a9780cfe98541fa
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-16 09:44:48 -0600

    Add NotNullAttribute and NullableAttribute

---
+++ Antlr4.Runtime/Misc/NotNullAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Antlr4.Runtime.Misc
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, Inherited = true, AllowMultiple = false)]
+    public sealed class NotNullAttribute : Attribute
+    {
+    }
+}
