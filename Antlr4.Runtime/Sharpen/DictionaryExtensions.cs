d55c2176e1bd07b6ed9afdd8e81e22b97369e78f
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-18 15:27:51 -0600

    Add DictionaryExtensions and ListExtensions

---
+++ Antlr4.Runtime/Sharpen/DictionaryExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Sharpen
+{
+    using System.Collections.Generic;
+
+    public static class DictionaryExtensions
+    {
+        public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : class
+        {
+            TValue value;
+            if (!dictionary.TryGetValue(key, out value))
+                return null;
+
+            return value;
+        }
+
+        public static TValue Put<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+            where TValue : class
+        {
+            TValue previous;
+            if (!dictionary.TryGetValue(key, out previous))
+                previous = null;
+
+            dictionary[key] = value;
+            return previous;
+        }
+    }
+}
---
+++ Antlr4.Runtime/Sharpen/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Sharpen
+{
+    using System.Collections.Generic;
+
+    public static class ListExtensions
+    {
+        public static T Set<T>(this IList<T> list, int index, T value)
+            where T : class
+        {
+            T previous = list[index];
+            list[index] = value;
+            return previous;
+        }
+    }
+}
