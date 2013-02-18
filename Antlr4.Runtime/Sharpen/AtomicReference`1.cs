0a1f1f7e43a507a2146e3ce20fc66eef7aca6a18
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-17 20:56:48 -0600

    Add AtomicReference<T>

---
+++ Antlr4.Runtime/Sharpen/AtomicReference`1.cs
@@ -0,0 +1,45 @@
+﻿namespace Sharpen
+{
+    using Interlocked = System.Threading.Interlocked;
+    using Volatile = System.Threading.Volatile;
+
+    public class AtomicReference<T>
+        where T : class
+    {
+        private T _value;
+
+        public AtomicReference()
+        {
+        }
+
+        public AtomicReference(T value)
+        {
+            _value = value;
+        }
+
+        public T Get()
+        {
+            return Volatile.Read(ref _value);
+        }
+
+        public void Set(T value)
+        {
+            Volatile.Write(ref _value, value);
+        }
+
+        public void LazySet(T value)
+        {
+            _value = value;
+        }
+
+        public bool CompareAndSet(T expect, T update)
+        {
+            return Interlocked.CompareExchange(ref _value, update, expect) == expect;
+        }
+
+        public T GetAndSet(T value)
+        {
+            return Interlocked.Exchange(ref _value, value);
+        }
+    }
+}
