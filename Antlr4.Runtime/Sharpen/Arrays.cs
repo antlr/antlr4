4b92e747a46955a5b1a5118416f0543e38a9e9f6
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-18 08:21:15 -0600

    Add Arrays.Fill

--- Antlr4.Runtime/Sharpen/Arrays.cs
+++ Antlr4.Runtime/Sharpen/Arrays.cs
@@ -15,5 +15,11 @@
         {
             return array;
         }
+
+        public static void Fill<T>(T[] array, T value)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = value;
+        }
     }
 }
