4bee942e2fae3bc31835d8e68d53f1a9e0bbbdf8
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-19 19:44:22 -0600

    Implement Arrays.HashCode, Equals, ToString

--- Antlr4.Runtime/Sharpen/Arrays.cs
+++ Antlr4.Runtime/Sharpen/Arrays.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using StringBuilder = System.Text.StringBuilder;
 
     internal static class Arrays
     {
@@ -20,6 +21,60 @@
         {
             for (int i = 0; i < array.Length; i++)
                 array[i] = value;
+        }
+
+        public static int HashCode<T>(T[] array)
+        {
+            if (array == null)
+                return 0;
+
+            int result = 1;
+            foreach (object o in array)
+                result = 31 * result + (o == null ? 0 : o.GetHashCode());
+
+            return result;
+        }
+
+        public static bool Equals<T>(T[] left, T[] right)
+        {
+            if (left == right)
+                return true;
+            else if (left == null || right == null)
+                return false;
+
+            if (left.Length != right.Length)
+                return false;
+
+            for (int i = 0; i < left.Length; i++)
+            {
+                if (!object.Equals(left[i], right[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static string ToString<T>(T[] array)
+        {
+            if (array == null)
+                return "null";
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append('[');
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i > 0)
+                    builder.Append(", ");
+
+                T o = array[i];
+                if (o == null)
+                    builder.Append("null");
+                else
+                    builder.Append(o);
+            }
+
+            builder.Append(']');
+            return builder.ToString();
         }
     }
 }
