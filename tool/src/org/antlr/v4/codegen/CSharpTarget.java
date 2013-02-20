fc37abee1cefd68367e0c54ba5414600758f982a
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-20 11:16:24 -0600

    Add stub CSharpTarget class, copy Java.stg to CSharp.stg, and add initial pom.xml to build the target jar

---
+++ tool/src/org/antlr/v4/codegen/CSharpTarget.java
@@ -0,0 +1,12 @@
+package org.antlr.v4.codegen;
+
+import org.antlr.v4.codegen.CodeGenerator;
+import org.antlr.v4.codegen.Target;
+
+public class CSharpTarget extends Target {
+
+	public CSharpTarget(CodeGenerator gen) {
+		super(gen);
+	}
+
+}
