7c02865ccbdbd2d52dd04c9aaf8303aefc197e8d
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-24 14:49:17 -0600

    Add using statements

--- runtime/CSharp/Antlr4.Runtime.Test/TestPerformance.cs
+++ runtime/CSharp/Antlr4.Runtime.Test/TestPerformance.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Atn;
+using Antlr4.Runtime.Tree;
 
 namespace Antlr4.Runtime.Test
 {
