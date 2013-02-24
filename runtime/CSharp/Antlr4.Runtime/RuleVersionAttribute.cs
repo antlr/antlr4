a988b668d321f846555fa38bb4d9d0ebb95775dc
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-23 23:12:51 -0600

    Add RuleVersionAttribute

---
+++ runtime/CSharp/Antlr4.Runtime/RuleVersionAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace Antlr4.Runtime
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class RuleVersionAttribute : Attribute
+    {
+        private readonly int _version;
+
+        public RuleVersionAttribute(int version)
+        {
+            _version = version;
+        }
+
+        public int Version
+        {
+            get
+            {
+                return _version;
+            }
+        }
+    }
+}
