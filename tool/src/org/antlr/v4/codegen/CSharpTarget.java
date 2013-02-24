48485e0cdafb507ef088dedd61504118b1b13a3f
Sam Harwell <sam@tunnelvisionlabs.com>
2013-02-24 14:31:32 -0600

    Initial implementation of CSharpTarget.java with proper C# escape sequences

--- tool/src/org/antlr/v4/codegen/CSharpTarget.java
+++ tool/src/org/antlr/v4/codegen/CSharpTarget.java
@@ -1,12 +1,99 @@
 package org.antlr.v4.codegen;
 
-import org.antlr.v4.codegen.CodeGenerator;
-import org.antlr.v4.codegen.Target;
+import org.antlr.v4.tool.ast.GrammarAST;
 
 public class CSharpTarget extends Target {
 
 	public CSharpTarget(CodeGenerator gen) {
-		super(gen);
+		super(gen, "CSharp");
+		targetCharValueEscape[0] = "\\0";
+		targetCharValueEscape[0x0007] = "\\a";
+		targetCharValueEscape[0x000B] = "\\v";
+	}
+
+	@Override
+	public String encodeIntAsCharEscape(int v) {
+		if (v < Character.MIN_VALUE || v > Character.MAX_VALUE) {
+			throw new IllegalArgumentException(String.format("Cannot encode the specified value: %d", v));
+		}
+
+		if (v >= 0 && v < targetCharValueEscape.length && targetCharValueEscape[v] != null) {
+			return targetCharValueEscape[v];
+		}
+
+		if (v >= 0x20 && v < 127 && (v < '0' || v > '9') && (v < 'a' || v > 'f') && (v < 'A' || v > 'F')) {
+			return String.valueOf((char)v);
+		}
+
+		return String.format("\\x%X", v & 0xFFFF);
+	}
+
+	@Override
+	public String getTargetStringLiteralFromANTLRStringLiteral(
+		CodeGenerator generator,
+		String literal, boolean addQuotes)
+	{
+		StringBuilder sb = new StringBuilder();
+		String is = literal;
+
+		if ( addQuotes ) sb.append('"');
+
+		for (int i = 1; i < is.length() -1; i++) {
+			if  (is.charAt(i) == '\\') {
+				// Anything escaped is what it is! We assume that
+				// people know how to escape characters correctly. However
+				// we catch anything that does not need an escape in Java (which
+				// is what the default implementation is dealing with and remove
+				// the escape. The C target does this for instance.
+				//
+				switch (is.charAt(i+1)) {
+					// Pass through any escapes that Java also needs
+					//
+					case    '"':
+					case    'n':
+					case    'r':
+					case    't':
+					case    'b':
+					case    'f':
+					case    '\\':
+						// Pass the escape through
+						sb.append('\\');
+						break;
+
+					case    'u':    // Assume unnnn
+						// Pass the escape through as double \\
+						// so that Java leaves as \u0000 string not char
+						sb.append('\\');
+						sb.append('\\');
+						break;
+
+					default:
+						// Remove the escape by virtue of not adding it here
+						// Thus \' becomes ' and so on
+						break;
+				}
+
+				// Go past the \ character
+				i++;
+			} else {
+				// Characters that don't need \ in ANTLR 'strings' but do in Java
+				if (is.charAt(i) == '"') {
+					// We need to escape " in Java
+					sb.append('\\');
+				}
+			}
+			// Add in the next character, which may have been escaped
+			sb.append(is.charAt(i));
+		}
+
+		if ( addQuotes ) sb.append('"');
+
+		return sb.toString();
+	}
+
+	@Override
+	protected boolean visibleGrammarSymbolCausesIssueInGeneratedCode(GrammarAST idNode) {
+		return false;
 	}
 
 }
