7235d71cc0e6cd21792f059cf95952206b531461
Sam Harwell <sam@tunnelvisionlabs.com>
2013-04-04 13:07:43 -0500

    Fix NPE revealed by updated testing method

--- tool/src/org/antlr/v4/Tool.java
+++ tool/src/org/antlr/v4/Tool.java
@@ -527,7 +527,7 @@ public class Tool {
 	/** Manually get option node from tree; return null if no defined. */
 	public static GrammarAST findOptionValueAST(GrammarRootAST root, String option) {
 		GrammarAST options = (GrammarAST)root.getFirstChildWithType(ANTLRParser.OPTIONS);
-		if ( options!=null ) {
+		if ( options!=null && options.getChildCount() > 0 ) {
 			for (Object o : options.getChildren()) {
 				GrammarAST c = (GrammarAST)o;
 				if ( c.getType() == ANTLRParser.ASSIGN &&
