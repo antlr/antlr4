1f19cf79593b4028fc3c92eacdbb869201301876
Sam Harwell <sam@tunnelvisionlabs.com>
2013-04-20 17:59:40 -0500

    @SuppressWarnings("serial")

--- runtime/Java/src/org/antlr/v4/runtime/misc/JFileChooserConfirmOverwrite.java
+++ runtime/Java/src/org/antlr/v4/runtime/misc/JFileChooserConfirmOverwrite.java
@@ -38,6 +38,7 @@ import java.io.File;
  *
  * @author Sam Harwell
  */
+@SuppressWarnings("serial")
 public class JFileChooserConfirmOverwrite extends JFileChooser {
 
 	public JFileChooserConfirmOverwrite() {
