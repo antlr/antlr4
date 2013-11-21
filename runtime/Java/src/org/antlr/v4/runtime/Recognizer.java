599896cf1b88d91a3c551b60f605001dab305297
Terence Parr <parrt@antlr.org>
2013-11-21 09:02:53 -0800

    throw exception not null

--- runtime/Java/src/org/antlr/v4/runtime/Recognizer.java
+++ runtime/Java/src/org/antlr/v4/runtime/Recognizer.java
@@ -64,10 +64,12 @@ public abstract class Recognizer<Symbol, ATNInterpreter extends ATNSimulator> {
 	 *
 	 *  Provide a dummy return value here to support backward compatibility.
 	 *
-	 * For interpreters, we don't know their serialized ATN despite having
-	 * created the interpreter from it.
+	 *  For interpreters, we don't know their serialized ATN despite having
+	 *  created the interpreter from it.
 	 */
-	public String getSerializedATN() { return null; }
+	public String getSerializedATN() {
+		throw new UnsupportedOperationException("there is no serialized ATN");
+	}
 
 	/** For debugging and other purposes, might want the grammar name.
 	 *  Have ANTLR generate an implementation for this method.
