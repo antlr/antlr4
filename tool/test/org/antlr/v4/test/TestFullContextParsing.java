53ff4238e843fe450ea847c297caf16970d49cfb
Sam Harwell <sam@tunnelvisionlabs.com>
2014-02-08 10:40:47 -0600

    Fix unit tests following the full context DFA setting change

--- tool/test/org/antlr/v4/test/TestFullContextParsing.java
+++ tool/test/org/antlr/v4/test/TestFullContextParsing.java
@@ -63,10 +63,44 @@ public class TestFullContextParsing extends BaseTest {
 					 this.stderrDuringParse);
 	}
 
-	@Test public void testCtxSensitiveDFA() {
+	@Test public void testCtxSensitiveWithoutDFA() {
 		String grammar =
 			"grammar T;\n"+
 			"s @after {dumpDFA();}\n" +
+			"  : '$' a | '@' b ;\n" +
+			"a : e ID ;\n" +
+			"b : e INT ID ;\n" +
+			"e : INT | ;\n" +
+			"ID : 'a'..'z'+ ;\n"+
+			"INT : '0'..'9'+ ;\n"+
+			"WS : (' '|'\\t'|'\\n')+ -> skip ;\n";
+		String result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+								   "$ 34 abc", true);
+		String expecting =
+			"Decision 1:\n" +
+			"s0-INT->s1\n" +
+			"s1-ID->:s2=>1\n";
+		assertEquals(expecting, result);
+		assertEquals("line 1:5 reportAttemptingFullContext d=1 (e), input='34abc'\n" +
+					 "line 1:2 reportContextSensitivity d=1 (e), input='34'\n",
+					 this.stderrDuringParse);
+
+		result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+							"@ 34 abc", true);
+		expecting =
+			"Decision 1:\n" +
+			"s0-INT->s1\n" +
+			"s1-ID->:s2=>1\n";
+		assertEquals(expecting, result);
+		assertEquals("line 1:5 reportAttemptingFullContext d=1 (e), input='34abc'\n" +
+					 "line 1:5 reportContextSensitivity d=1 (e), input='34abc'\n",
+					 this.stderrDuringParse);
+	}
+
+	@Test public void testCtxSensitiveWithDFA() {
+		String grammar =
+			"grammar T;\n"+
+			"s @init{getInterpreter().enable_global_context_dfa = true;} @after {dumpDFA();}\n" +
 			"  : '$' a | '@' b ;\n" +
 			"a : e ID ;\n" +
 			"b : e INT ID ;\n" +
@@ -102,10 +136,35 @@ public class TestFullContextParsing extends BaseTest {
 					 this.stderrDuringParse);
 	}
 
-	@Test public void testCtxSensitiveDFATwoDiffInput() {
+	@Test public void testCtxSensitiveDFATwoDiffInputWithoutDFA() {
 		String grammar =
 			"grammar T;\n"+
 			"s @after {dumpDFA();}\n" +
+			"  : ('$' a | '@' b)+ ;\n" +
+			"a : e ID ;\n" +
+			"b : e INT ID ;\n" +
+			"e : INT | ;\n" +
+			"ID : 'a'..'z'+ ;\n"+
+			"INT : '0'..'9'+ ;\n"+
+			"WS : (' '|'\\t'|'\\n')+ -> skip ;\n";
+		String result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+								   "$ 34 abc @ 34 abc", true);
+		String expecting =
+			"Decision 2:\n" +
+			"s0-INT->s1\n" +
+			"s1-ID->:s2=>1\n";
+		assertEquals(expecting, result);
+		assertEquals("line 1:5 reportAttemptingFullContext d=2 (e), input='34abc'\n" +
+					 "line 1:2 reportContextSensitivity d=2 (e), input='34'\n" +
+					 "line 1:14 reportAttemptingFullContext d=2 (e), input='34abc'\n" +
+					 "line 1:14 reportContextSensitivity d=2 (e), input='34abc'\n",
+					 this.stderrDuringParse);
+	}
+
+	@Test public void testCtxSensitiveDFATwoDiffInputWithDFA() {
+		String grammar =
+			"grammar T;\n"+
+			"s @init{getInterpreter().enable_global_context_dfa = true;} @after {dumpDFA();}\n" +
 			"  : ('$' a | '@' b)+ ;\n" +
 			"a : e ID ;\n" +
 			"b : e INT ID ;\n" +
@@ -133,10 +192,34 @@ public class TestFullContextParsing extends BaseTest {
 	}
 
 	@Test
-	public void testSLLSeesEOFInLLGrammar() {
+	public void testSLLSeesEOFInLLGrammarWithoutDFA() {
 		String grammar =
 			"grammar T;\n"+
 			"s @after {dumpDFA();}\n" +
+			"  : a ;\n" +
+			"a : e ID ;\n" +
+			"b : e INT ID ;\n" +
+			"e : INT | ;\n" +
+			"ID : 'a'..'z'+ ;\n"+
+			"INT : '0'..'9'+ ;\n"+
+			"WS : (' '|'\\t'|'\\n')+ -> skip ;\n";
+		String result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+								   "34 abc", true);
+		String expecting =
+			"Decision 0:\n" +
+			"s0-INT->s1\n" +
+			"s1-ID->:s2=>1\n"; // Must point at accept state
+		assertEquals(expecting, result);
+		assertEquals("line 1:3 reportAttemptingFullContext d=0 (e), input='34abc'\n" +
+					 "line 1:0 reportContextSensitivity d=0 (e), input='34'\n",
+					 this.stderrDuringParse);
+	}
+
+	@Test
+	public void testSLLSeesEOFInLLGrammarWithDFA() {
+		String grammar =
+			"grammar T;\n"+
+			"s @init{getInterpreter().enable_global_context_dfa = true;} @after {dumpDFA();}\n" +
 			"  : a ;\n" +
 			"a : e ID ;\n" +
 			"b : e INT ID ;\n" +
@@ -158,11 +241,114 @@ public class TestFullContextParsing extends BaseTest {
 					 this.stderrDuringParse);
 	}
 
-	@Test public void testFullContextIF_THEN_ELSEParse() {
+	@Test public void testFullContextIF_THEN_ELSEParseWithoutDFA() {
 		String grammar =
 			"grammar T;\n"+
 			"s" +
 			"@init {_interp.setPredictionMode(PredictionMode.LL_EXACT_AMBIG_DETECTION);}\n" +
+			"@after {dumpDFA();}\n" +
+			"    : '{' stat* '}'" +
+			"    ;\n" +
+			"stat: 'if' ID 'then' stat ('else' ID)?\n" +
+			"    | 'return'\n" +
+			"    ;" +
+			"ID : 'a'..'z'+ ;\n"+
+			"WS : (' '|'\\t'|'\\n')+ -> skip ;\n";
+		String input = "{ if x then return }";
+		String result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+								   input, true);
+		String expecting =
+			"Decision 1:\n" +
+			"s0-'}'->:s1=>2\n";
+		assertEquals(expecting, result);
+		assertEquals(null, this.stderrDuringParse);
+
+		input = "{ if x then return else foo }";
+		result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+							input, true);
+		expecting =
+			"Decision 1:\n" +
+			"s0-'else'->:s1=>1\n";
+		assertEquals(expecting, result);
+		// Technically, this input sequence is not ambiguous because else
+		// uniquely predicts going into the optional subrule. else cannot
+		// be matched by exiting stat since that would only match '}' or
+		// the start of a stat. But, we are using the theory that
+		// SLL(1)=LL(1) and so we are avoiding full context parsing
+		// by declaring all else clause parsing to be ambiguous.
+		assertEquals("line 1:19 reportAttemptingFullContext d=1 (stat), input='else'\n" +
+					 "line 1:19 reportContextSensitivity d=1 (stat), input='else'\n",
+					 this.stderrDuringParse);
+
+		input =
+			"{ if x then if y then return else foo }";
+		result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+							input, true);
+		expecting =
+			"Decision 1:\n" +
+			"s0-'else'->:s1=>1\n" +
+			"s0-'}'->:s2=>2\n";
+		assertEquals(expecting, result);
+		assertEquals("line 1:29 reportAttemptingFullContext d=1 (stat), input='else'\n" +
+					 "line 1:38 reportAmbiguity d=1 (stat): ambigAlts={1, 2}, input='elsefoo}'\n",
+					 this.stderrDuringParse);
+
+		// should not be ambiguous because the second 'else bar' clearly
+		// indicates that the first else should match to the innermost if.
+		// LL_EXACT_AMBIG_DETECTION makes us keep going to resolve
+
+		input =
+			"{ if x then if y then return else foo else bar }";
+		result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+							input, true);
+		expecting =
+			"Decision 1:\n" +
+			"s0-'else'->:s1=>1\n";
+		assertEquals(expecting, result);
+		assertEquals("line 1:29 reportAttemptingFullContext d=1 (stat), input='else'\n" +
+					 "line 1:38 reportContextSensitivity d=1 (stat), input='elsefooelse'\n" +
+					 "line 1:38 reportAttemptingFullContext d=1 (stat), input='else'\n" +
+					 "line 1:38 reportContextSensitivity d=1 (stat), input='else'\n",
+					 this.stderrDuringParse);
+
+		input =
+			"{ if x then return else foo\n" +
+			"if x then if y then return else foo }";
+		result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+							input, true);
+		expecting =
+			"Decision 1:\n" +
+			"s0-'else'->:s1=>1\n" +
+			"s0-'}'->:s2=>2\n";
+		assertEquals(expecting, result);
+		assertEquals("line 1:19 reportAttemptingFullContext d=1 (stat), input='else'\n" +
+					 "line 1:19 reportContextSensitivity d=1 (stat), input='else'\n" +
+					 "line 2:27 reportAttemptingFullContext d=1 (stat), input='else'\n" +
+					 "line 2:36 reportAmbiguity d=1 (stat): ambigAlts={1, 2}, input='elsefoo}'\n",
+					 this.stderrDuringParse);
+
+		input =
+			"{ if x then return else foo\n" +
+			"if x then if y then return else foo }";
+		result = execParser("T.g4", grammar, "TParser", "TLexer", "s",
+							input, true);
+		expecting =
+				"Decision 1:\n" +
+				"s0-'else'->:s1=>1\n" +
+				"s0-'}'->:s2=>2\n";
+		assertEquals(expecting, result);
+		assertEquals("line 1:19 reportAttemptingFullContext d=1 (stat), input='else'\n" +
+					 "line 1:19 reportContextSensitivity d=1 (stat), input='else'\n" +
+					 "line 2:27 reportAttemptingFullContext d=1 (stat), input='else'\n" +
+					 "line 2:36 reportAmbiguity d=1 (stat): ambigAlts={1, 2}, input='elsefoo}'\n",
+					 this.stderrDuringParse);
+	}
+
+	@Test public void testFullContextIF_THEN_ELSEParseWithDFA() {
+		String grammar =
+			"grammar T;\n"+
+			"s" +
+			"@init {getInterpreter().enable_global_context_dfa = true; _interp.setPredictionMode(PredictionMode.LL_EXACT_AMBIG_DETECTION);}\n" +
 			"@after {dumpDFA();}\n" +
 			"    : '{' stat* '}'" +
 			"    ;\n" +
