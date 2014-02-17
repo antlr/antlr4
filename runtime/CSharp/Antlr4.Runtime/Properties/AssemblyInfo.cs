3ad255408705659329ce9bab413abfbb0bbb9091
Sam Harwell <sam@tunnelvisionlabs.com>
2014-02-16 21:54:39 -0600

    Updated assembly versions

--- runtime/CSharp/Antlr4.Runtime/Properties/AssemblyInfo.cs
+++ runtime/CSharp/Antlr4.Runtime/Properties/AssemblyInfo.cs
@@ -54,7 +54,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
 [assembly: AssemblyProduct("Antlr4.Runtime")]
-[assembly: AssemblyCopyright("Copyright © Sam Harwell 2013")]
+[assembly: AssemblyCopyright("Copyright © Sam Harwell 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -77,7 +77,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
 #if !NET_CF
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
+[assembly: AssemblyInformationalVersion("4.2.0.0-dev")]
 #endif
