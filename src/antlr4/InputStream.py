1b616c838ef95acc1639b5b5f9ba1cf95892b9ad
ericvergnaud <eric.vergnaud@wanadoo.fr>
2014-05-21 01:11:18 +0800

    AntLR 4 runtime for Python 2.7.6

---
+++ src/antlr4/InputStream.py
@@ -0,0 +1,133 @@
+# 
+#  [The "BSD license"]
+#   Copyright (c) 2012 Terence Parr
+#   Copyright (c) 2012 Sam Harwell
+#   Copyright (c) 2014 Eric Vergnaud
+#   All rights reserved.
+# 
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions
+#   are met:
+# 
+#   1. Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#   3. The name of the author may not be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+# 
+#   THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+#   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+#   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+#   IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+#   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+#   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+#   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+import unittest
+
+
+# 
+#  Vacuum all input from a string and then treat it like a buffer. 
+#
+from antlr4.Token import Token
+
+
+class InputStream (object):
+    
+    def __init__(self, data):
+        self.name = "<empty>"
+        self.strdata = unicode(data)
+        self._loadString()
+
+    def _loadString(self):    
+        self._index = 0
+        self.data = [ord(c) for c in self.strdata]
+        self._size = len(self.data)
+
+    @property
+    def index(self):
+        return self._index
+
+    @property
+    def size(self):
+        return self._size
+
+    # Reset the stream so that it's in the same state it was
+    #  when the object was created *except* the data array is not
+    #  touched.
+    #
+    def reset(self):
+        self._index = 0
+
+    def consume(self):
+        if self._index >= self._size:
+            assert self.LA(1) == Token.EOF
+            raise Exception("cannot consume EOF")
+        self._index += 1
+
+    def LA(self, offset):
+        if offset==0:
+            return 0 # undefined
+        if offset<0:
+            offset += 1 # e.g., translate LA(-1) to use offset=0
+        pos = self._index + offset - 1
+        if pos < 0 or pos >= self._size: # invalid
+            return Token.EOF
+        return self.data[pos]
+
+    def LT(self, offset):
+        return self.LA(offset)
+
+    # mark/release do nothing; we have entire buffer
+    def mark(self):
+        return -1
+
+    def release(self, marker):
+        pass
+
+    # consume() ahead until p==_index; can't just set p=_index as we must
+    # update line and column. If we seek backwards, just set p
+    #
+    def seek(self, _index):
+        if _index<=self._index:
+            self._index = _index # just jump; don't update stream state (line, ...)
+            return
+        # seek forward
+        self._index = min(_index, self._size)
+
+    def getText(self, start, stop):
+        if stop >= self._size:
+            stop = self._size-1
+        if start >= self._size:
+            return ""
+        else:
+            return self.strdata[start:stop+1]
+
+    def __str__(self):
+        return unicode(self)
+
+    def __unicode__(self):
+        return self.strdata
+
+
+class TestInputStream(unittest.TestCase):
+    
+    def testStream(self):
+        stream = InputStream("abcde")
+        self.assertEqual(0, stream.index)
+        self.assertEqual(5, stream.size)
+        self.assertEqual(ord("a"), stream.LA(1))
+        stream.consume()
+        self.assertEqual(1, stream.index)
+        stream.seek(5)
+        self.assertEqual(Token.EOF, stream.LA(1))
+        self.assertEqual("bcd", stream.getText(1, 3))
+        stream.reset()
+        self.assertEqual(0, stream.index)
+        
+        
---
+++ src/antlr4/tree/ParseTreeMatch.py
@@ -0,0 +1,145 @@
+#
+# [The "BSD license"]
+# Copyright (c) 2013 Terence Parr
+# Copyright (c) 2013 Sam Harwell
+# Copyright (c) 2014 Eric Vergnaud
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+
+#
+# Represents the result of matching a {@link ParseTree} against a tree pattern.
+#
+from io import StringIO
+
+
+class ParseTreeMatch(object):
+
+    #
+    # Constructs a new instance of {@link ParseTreeMatch} from the specified
+    # parse tree and pattern.
+    #
+    # @param tree The parse tree to match against the pattern.
+    # @param pattern The parse tree pattern.
+    # @param labels A mapping from label names to collections of
+    # {@link ParseTree} objects located by the tree pattern matching process.
+    # @param mismatchedNode The first node which failed to match the tree
+    # pattern during the matching process.
+    #
+    # @exception IllegalArgumentException if {@code tree} is {@code null}
+    # @exception IllegalArgumentException if {@code pattern} is {@code null}
+    # @exception IllegalArgumentException if {@code labels} is {@code null}
+    #
+    def __init__(self, tree, pattern, labels, mismatchedNode):
+        if tree is None:
+            raise Exception("tree cannot be null")
+        if pattern is None:
+            raise Exception("pattern cannot be null")
+        if labels is None:
+            raise Exception("labels cannot be null")
+        self.tree = tree
+        self.pattern = pattern
+        self.labels = labels
+        self.mismatchedNode = mismatchedNode
+
+    #
+    # Get the last node associated with a specific {@code label}.
+    #
+    # <p>For example, for pattern {@code <id:ID>}, {@code get("id")} returns the
+    # node matched for that {@code ID}. If more than one node
+    # matched the specified label, only the last is returned. If there is
+    # no node associated with the label, this returns {@code null}.</p>
+    #
+    # <p>Pattern tags like {@code <ID>} and {@code <expr>} without labels are
+    # considered to be labeled with {@code ID} and {@code expr}, respectively.</p>
+    #
+    # @param label The label to check.
+    #
+    # @return The last {@link ParseTree} to match a tag with the specified
+    # label, or {@code null} if no parse tree matched a tag with the label.
+    #
+    def get(self, label):
+        parseTrees = self.labels.get(label, None)
+        if parseTrees is None or len(parseTrees)==0:
+            return None
+        else:
+            return parseTrees[len(parseTrees)-1]
+
+    #
+    # Return all nodes matching a rule or token tag with the specified label.
+    #
+    # <p>If the {@code label} is the name of a parser rule or token in the
+    # grammar, the resulting list will contain both the parse trees matching
+    # rule or tags explicitly labeled with the label and the complete set of
+    # parse trees matching the labeled and unlabeled tags in the pattern for
+    # the parser rule or token. For example, if {@code label} is {@code "foo"},
+    # the result will contain <em>all</em> of the following.</p>
+    #
+    # <ul>
+    # <li>Parse tree nodes matching tags of the form {@code <foo:anyRuleName>} and
+    # {@code <foo:AnyTokenName>}.</li>
+    # <li>Parse tree nodes matching tags of the form {@code <anyLabel:foo>}.</li>
+    # <li>Parse tree nodes matching tags of the form {@code <foo>}.</li>
+    # </ul>
+    #
+    # @param label The label.
+    #
+    # @return A collection of all {@link ParseTree} nodes matching tags with
+    # the specified {@code label}. If no nodes matched the label, an empty list
+    # is returned.
+    #
+    def getAll(self, label):
+        nodes = self.labels.get(label, None)
+        if nodes is None:
+            return list()
+        else:
+            return nodes
+
+
+    #
+    # Gets a value indicating whether the match operation succeeded.
+    #
+    # @return {@code true} if the match operation succeeded; otherwise,
+    # {@code false}.
+    #
+    def succeeded(self):
+        return self.mismatchedNode is None
+
+    #
+    # {@inheritDoc}
+    #
+    def __str__(self):
+        return unicode(self)
+
+
+    def __unicode__(self):
+        with StringIO() as buf:
+            buf.write(u"Match ")
+            buf.write(u"succeeded" if self.succeeded() else "failed")
+            buf.write(u"; found ")
+            buf.write(unicode(len(self.labels)))
+            buf.write(u" labels")
+            return buf.getvalue()
