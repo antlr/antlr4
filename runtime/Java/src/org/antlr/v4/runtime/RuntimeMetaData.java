2a25ec2fa408972307b1ef10491ac3818b08ebc9
Sam Harwell <sam@tunnelvisionlabs.com>
2014-06-02 22:24:05 -0500

    Use tabs, add missing license header

--- runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
+++ runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
@@ -1,3 +1,33 @@
+/*
+ * [The "BSD license"]
+ *  Copyright (c) 2014 Terence Parr
+ *  Copyright (c) 2014 Sam Harwell
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.antlr.v4.runtime;
 
 /** Because targets can be updated at different times than the core tool, which includes this Java target,
@@ -18,38 +48,38 @@ package org.antlr.v4.runtime;
  *  as a message might not be seen if the parser were embedded in a server or something.
  */
 public class RuntimeMetaData {
-    public static class ANTLRVersionMismatchException extends RuntimeException {
-        public String generatedCodeVersion;
-        public String runtimeLibVersion;
+	public static class ANTLRVersionMismatchException extends RuntimeException {
+		public String generatedCodeVersion;
+		public String runtimeLibVersion;
 
-        public ANTLRVersionMismatchException(String message, String generatedCodeVersion, String runtimeLibVersion) {
-            super(message);
-            this.generatedCodeVersion = generatedCodeVersion;
-            this.runtimeLibVersion = runtimeLibVersion;
-        }
+		public ANTLRVersionMismatchException(String message, String generatedCodeVersion, String runtimeLibVersion) {
+			super(message);
+			this.generatedCodeVersion = generatedCodeVersion;
+			this.runtimeLibVersion = runtimeLibVersion;
+		}
 
-        @Override
-        public String getMessage() {
-            return super.getMessage()+": code version "+generatedCodeVersion+" != runtime version "+runtimeLibVersion;
-        }
-    }
+		@Override
+		public String getMessage() {
+			return super.getMessage()+": code version "+generatedCodeVersion+" != runtime version "+runtimeLibVersion;
+		}
+	}
 
-    /** Must match version of tool that generated recognizers */
-    public static final String VERSION = "4.3"; // use just "x.y" and don't include bug fix release number
+	/** Must match version of tool that generated recognizers */
+	public static final String VERSION = "4.3"; // use just "x.y" and don't include bug fix release number
 
-    /** As parser or lexer class is loaded, it checks that the version used to generate the code
-     *  is compatible with the runtime version. ANTLR tool generates recognizers with a hardcoded string created by
-     *  the tool during code gen. That version is passed to checkVersion().
-     */
-    public static void checkVersion(String toolVersion) {
-        // I believe that 4.2-generated parsers are compatible with the runtime for 4.3 so no exception in this case.
-        // Technically, we don't need this check because 4.2 it doesn't actually check the version. ;) I am just
-        // being explicit. Later we can always build a more sophisticated versioning check.
-        if ( (toolVersion.startsWith("4.2.") || toolVersion.equals("4.2")) ) {
-            return;
-        }
-        if ( !VERSION.equals(toolVersion) ) {
-            throw new ANTLRVersionMismatchException("ANTLR runtime and generated code versions disagree",toolVersion,VERSION);
-        }
-    }
+	/** As parser or lexer class is loaded, it checks that the version used to generate the code
+	 *  is compatible with the runtime version. ANTLR tool generates recognizers with a hardcoded string created by
+	 *  the tool during code gen. That version is passed to checkVersion().
+	 */
+	public static void checkVersion(String toolVersion) {
+		// I believe that 4.2-generated parsers are compatible with the runtime for 4.3 so no exception in this case.
+		// Technically, we don't need this check because 4.2 it doesn't actually check the version. ;) I am just
+		// being explicit. Later we can always build a more sophisticated versioning check.
+		if ( (toolVersion.startsWith("4.2.") || toolVersion.equals("4.2")) ) {
+			return;
+		}
+		if ( !VERSION.equals(toolVersion) ) {
+			throw new ANTLRVersionMismatchException("ANTLR runtime and generated code versions disagree",toolVersion,VERSION);
+		}
+	}
 }
