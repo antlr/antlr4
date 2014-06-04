d68ffad6d39eb7799d81a261c8fcfe52fdace0b7
ericvergnaud <eric.vergnaud@wanadoo.fr>
2014-06-05 01:50:36 +0800

    First running Lexer test

---
+++ src/antlr4/Utils.js
@@ -0,0 +1,40 @@
+String.prototype.hashCode = function(s) {
+	var hash = 0;
+	if (this.length === 0) {
+		return hash;
+	}
+	for (var i = 0; i < this.length; i++) {
+		var character = this.charCodeAt(i);
+		hash = ((hash << 5) - hash) + character;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
+};
+
+function Dict() {
+	return this;
+}
+
+function Set() {
+	this.data = {};
+	return this;
+}
+
+Set.prototype.add = function(value) {
+	var key = "hash_" + value.hashString().hashCode();
+	if(key in this.data) {
+		this.data[key].map(function(v) {
+			if (value.equals(v)) {
+				return v;
+			}
+		});
+		this.data[key].push(value);
+		return value;
+	} else {
+		this.data[key] = [ value ];
+		return value;
+	}
+};
+
+exports.Dict = Dict;
+exports.Set = Set;
