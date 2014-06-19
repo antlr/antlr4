7d88b0106a5c0196f9e89e7c6a52f15574a1044e
Sam Harwell <sam@tunnelvisionlabs.com>
2014-06-18 21:21:47 -0500

    [maven-release-plugin] prepare for next development iteration

--- runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
+++ runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
@@ -74,7 +74,7 @@ public class RuntimeMetaData {
 	 * libraries to include a literal reference to the version of the ANTLR 4
 	 * runtime library the code was compiled against.</p>
 	 */
-	public static final String VERSION = "4.3";
+	public static final String VERSION = "4.3.1-SNAPSHOT";
 
 	/**
 	 * This class provides detailed information about a mismatch between the
