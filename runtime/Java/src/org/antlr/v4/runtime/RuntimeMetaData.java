ba60548de0c771e38b49ae25eaa86f1d6ee764c1
ericvergnaud <eric.vergnaud@wanadoo.fr>
2014-06-23 22:42:01 +0800

    Version upgrade for Python targets

--- runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
+++ runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
@@ -74,7 +74,7 @@ public class RuntimeMetaData {
 	 * libraries to include a literal reference to the version of the ANTLR 4
 	 * runtime library the code was compiled against.</p>
 	 */
-	public static final String VERSION = "4.3.1-SNAPSHOT";
+	public static final String VERSION = "4.4.0";
 
 	/**
 	 * This class provides detailed information about a mismatch between the
