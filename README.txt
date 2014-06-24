489b48e3bc528b029a6aab5e69f75ea0764ee302
ericvergnaud <eric.vergnaud@wanadoo.fr>
2014-06-24 22:39:26 +0800

    PyPI version

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is the Python 3.4 runtime for AntLR.
+Visit the AntLR web site for more information:
+http://www.antlr.org
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='antlr4-python3-runtime',
-    version='4.2.0',
+    version='4.4.0',
     packages=['antlr4', 'antlr4.atn', 'antlr4.dfa', 'antlr4.tree', 'antlr4.error', 'antlr4.xpath'],
     package_dir={'': 'src'},
     url='http://www.antlr.org',
     license='BSD',
     author='Eric Vergnaud, Terence Parr, Sam Harwell',
     author_email='eric@test',
-    description='AntLR 4.2.0 runtime for Python 3.4.0'
+    description='AntLR 4.4.0 runtime for Python 3.4.0'
 )
