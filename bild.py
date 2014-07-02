c2154188b08056c59eb34bef5be8d03ef729d36d
Terence Parr <parrt@antlr.org>
2014-07-01 21:33:26 -0700

    makes a jar now

--- bild.py
+++ bild.py
@@ -4,9 +4,7 @@ sys.path.append(os.path.abspath("/Users/parrt/github/bild"))
 
 from bilder import *
 
-VERSION = 4.4
-
-#CLASSPATH = JARCACHE+"/*" +os.pathsep+ os.environ['CLASSPATH']
+VERSION = "4.4"
 
 def parsers():
     antlr3("tool/src/org/antlr/v4/parse", "gen", package="org.antlr.v4.parse")
@@ -14,23 +12,30 @@ def parsers():
     antlr4("runtime/Java/src/org/antlr/v4/runtime/tree/xpath", "gen", package="org.antlr.v4.runtime.tree.xpath")
 
 def compile():
-	require(parsers)
-	javac("src/java", "out")
-	javac("gen", "out")
+    require(parsers)
+    cp = uniformpath("out")+os.pathsep+\
+        os.path.join(JARCACHE,"antlr-3.5.1-complete.jar")+os.pathsep+\
+        "runtime/Java/lib/org.abego.treelayout.core.jar"
+    javac("runtime/JavaAnnotations/src/", "out", cp=cp)
+    javac("runtime/Java/src", "out", cp=cp)
+    javac("tool/src", "out", cp=cp)
+    javac("gen", "out", cp=cp)
 
 def mkjar():
-	require(compile)
-	copytree(src="resources", dst="out/resources")
-	manifest = """Version: %s
-Main-Class: org.foo.Blort
+    require(compile)
+    copytree(src="tool/resources", dst="out")
+    manifest = """Version: %s
+Main-Class: org.antlr.v4.Tool
 """ % VERSION
-	jar("dist/app.jar", srcdir="out", manifest=manifest)
+    # copy 3.5.1 in there
+    unjar("runtime/Java/lib/org.abego.treelayout.core.jar", trgdir="out")
+    jar("dist/antlr-"+VERSION+"-complete.jar", srcdir="out", manifest=manifest)
 
 def all():
-	mkjar()
+    mkjar()
 
 def clean():
-	rmdir("out")
-	rmdir("gen")
+    rmdir("out")
+    rmdir("gen")
 
-parsers()
+mkjar()
