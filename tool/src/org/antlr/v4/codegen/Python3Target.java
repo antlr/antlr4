cac6850f9761034983c11c7fd36501d1561c4500
Terence Parr <parrt@antlr.org>
2014-07-03 10:52:18 -0700

    factor common char encoding into main Target; version set to 4.4

--- tool/src/org/antlr/v4/codegen/Python3Target.java
+++ tool/src/org/antlr/v4/codegen/Python3Target.java
@@ -30,34 +30,38 @@
 
 package org.antlr.v4.codegen;
 
+import org.antlr.v4.tool.ast.GrammarAST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.StringRenderer;
+
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 /**
  *
  * @author Eric Vergnaud
  */
-public class Python3Target extends AbstractPythonTarget {
-
+public class Python3Target extends Target {
 	protected static final String[] python3Keywords = {
-		"abs", "all", "any", "apply", "as", 
-		"bin", "bool", "buffer", "bytearray", 
-		"callable", "chr", "classmethod", "coerce", "compile", "complex", 
-		"delattr", "dict", "dir", "divmod", 
+		"abs", "all", "any", "apply", "as",
+		"bin", "bool", "buffer", "bytearray",
+		"callable", "chr", "classmethod", "coerce", "compile", "complex",
+		"delattr", "dict", "dir", "divmod",
 		"enumerate", "eval", "execfile",
-		"file", "filter", "float", "format", "frozenset", 
-		"getattr", "globals", 
-		"hasattr", "hash", "help", "hex", 
-		"id", "input", "int", "intern", "isinstance", "issubclass", "iter", 
-		"len", "list", "locals", 
-		"map", "max", "min", "next", 
-		"memoryview", 
-		"object", "oct", "open", "ord", 
-		"pow", "print", "property", 
-		"range", "raw_input", "reduce", "reload", "repr", "reversed", "round", 
+		"file", "filter", "float", "format", "frozenset",
+		"getattr", "globals",
+		"hasattr", "hash", "help", "hex",
+		"id", "input", "int", "intern", "isinstance", "issubclass", "iter",
+		"len", "list", "locals",
+		"map", "max", "min", "next",
+		"memoryview",
+		"object", "oct", "open", "ord",
+		"pow", "print", "property",
+		"range", "raw_input", "reduce", "reload", "repr", "reversed", "round",
 		"set", "setattr", "slice", "sorted", "staticmethod", "str", "sum", "super",
-		"tuple", "type", 
+		"tuple", "type",
 		"unichr", "unicode",
 		"vars",
 		"with",
@@ -66,17 +70,58 @@ public class Python3Target extends AbstractPythonTarget {
 		"True", "False", "None"
 	};
 
+	public Python3Target(CodeGenerator gen) {
+		super(gen, "Python3");
+	}
+
+	@Override
+	public int getSerializedATNSegmentLimit() {
+		// set to something stupid to avoid segmentation
+		return 2 ^ 31;
+	}
+
+	@Override
+	protected boolean visibleGrammarSymbolCausesIssueInGeneratedCode(GrammarAST idNode) {
+		return getBadWords().contains(idNode.getText());
+	}
+
+	@Override
+	protected STGroup loadTemplates() {
+		STGroup result = super.loadTemplates();
+		result.registerRenderer(String.class, new PythonStringRenderer(), true);
+		return result;
+	}
+
+	protected static class PythonStringRenderer extends StringRenderer {
+
+		@Override
+		public String toString(Object o, String formatString, Locale locale) {
+			return super.toString(o, formatString, locale);
+		}
+	}
+
+	@Override
+	public boolean wantsBaseListener() {
+		return false;
+	}
+
+	@Override
+	public boolean wantsBaseVisitor() {
+		return false;
+	}
+
+	@Override
+	public boolean supportsOverloadedMethods() {
+		return false;
+	}
+
 	@Override
 	public String getVersion() {
-		return "4.4.0";
+		return "4.4";
 	}
 
 	/** Avoid grammar symbols in this set to prevent conflicts in gen'd code. */
 	protected final Set<String> badWords = new HashSet<String>();
-
-	public Python3Target(CodeGenerator gen) {
-		super(gen, "Python3");
-	}
 
 	public Set<String> getBadWords() {
 		if (badWords.isEmpty()) {
