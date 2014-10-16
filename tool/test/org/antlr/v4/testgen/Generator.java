3d04a72d5217860dabcff34a692696a26122369e
ericvergnaud <eric.vergnaud@wanadoo.fr>
2014-10-16 23:53:54 +0800

    progress

--- tool/test/org/antlr/v4/testgen/Generator.java
+++ tool/test/org/antlr/v4/testgen/Generator.java
@@ -115,7 +115,97 @@ public class Generator {
 		list.add(buildParserErrors());
 		list.add(buildParserExec());
 		list.add(buildParseTrees());
+		list.add(buildSemPredEvalLexer());
 		return list;		
+	}
+
+	private TestFile buildSemPredEvalLexer() throws Exception {
+		TestFile file = new TestFile("SemPredEvalLexer");
+		file.addLexerTest(input, "DisableRule", "L",
+				"enum abc", 
+				"[@0,0:3='enum',<2>,1:0]\n" +
+				"[@1,5:7='abc',<3>,1:5]\n" +
+				"[@2,8:7='<EOF>',<-1>,1:8]\n" +
+				"s0-' '->:s5=>4\n" +
+				"s0-'a'->:s6=>3\n" +
+				"s0-'e'->:s1=>3\n" +
+				":s1=>3-'n'->:s2=>3\n" +
+				":s2=>3-'u'->:s3=>3\n" +
+				":s6=>3-'b'->:s6=>3\n" +
+				":s6=>3-'c'->:s6=>3\n",
+				null);
+		file.addLexerTest(input, "IDvsEnum", "L",
+				"enum abc enum", 
+				"[@0,0:3='enum',<2>,1:0]\n" +
+				"[@1,5:7='abc',<2>,1:5]\n" +
+				"[@2,9:12='enum',<2>,1:9]\n" +
+				"[@3,13:12='<EOF>',<-1>,1:13]\n" +
+				"s0-' '->:s5=>3\n" +
+				"s0-'a'->:s4=>2\n" +
+				"s0-'e'->:s1=>2\n" +
+				":s1=>2-'n'->:s2=>2\n" +
+				":s2=>2-'u'->:s3=>2\n" +
+				":s4=>2-'b'->:s4=>2\n" +
+				":s4=>2-'c'->:s4=>2\n", // no 'm'-> transition...conflicts with pred
+				null); 
+		file.addLexerTest(input, "IDnotEnum", "L",
+				"enum abc enum", 
+				"[@0,0:3='enum',<2>,1:0]\n" +
+				"[@1,5:7='abc',<2>,1:5]\n" +
+				"[@2,9:12='enum',<2>,1:9]\n" +
+				"[@3,13:12='<EOF>',<-1>,1:13]\n" +
+				"s0-' '->:s2=>3\n", // no edges in DFA for enum/id. all paths lead to pred.
+				null);
+		file.addLexerTest(input, "EnumNotID", "L",
+				"enum abc enum", 
+				"[@0,0:3='enum',<1>,1:0]\n" +
+				"[@1,5:7='abc',<2>,1:5]\n" +
+				"[@2,9:12='enum',<1>,1:9]\n" +
+				"[@3,13:12='<EOF>',<-1>,1:13]\n" +
+				"s0-' '->:s3=>3\n", // no edges in DFA for enum/id. all paths lead to pred.
+				null);
+		file.addLexerTest(input, "Indent", "L",
+				"abc\n  def  \n", 
+				"INDENT\n" +                        // action output
+				"[@0,0:2='abc',<1>,1:0]\n" +		// ID
+				"[@1,3:3='\\n',<3>,1:3]\n" +  		// NL
+				"[@2,4:5='  ',<2>,2:0]\n" +			// INDENT
+				"[@3,6:8='def',<1>,2:2]\n" +		// ID
+				"[@4,9:10='  ',<4>,2:5]\n" +		// WS
+				"[@5,11:11='\\n',<3>,2:7]\n" +
+				"[@6,12:11='<EOF>',<-1>,3:8]\n" +
+				"s0-'\n" +
+				"'->:s2=>3\n" +
+				"s0-'a'->:s1=>1\n" +
+				"s0-'d'->:s1=>1\n" +
+				":s1=>1-'b'->:s1=>1\n" +
+				":s1=>1-'c'->:s1=>1\n" +
+				":s1=>1-'e'->:s1=>1\n" +
+				":s1=>1-'f'->:s1=>1\n",
+				null);
+		file.addLexerTest(input, "LexerInputPositionSensitivePredicates", "L",
+				"a cde\nabcde\n",
+				"a\n" +
+				"cde\n" +
+				"ab\n" +
+				"cde\n" +
+				"[@0,0:0='a',<1>,1:0]\n" +
+				"[@1,2:4='cde',<2>,1:2]\n" +
+				"[@2,6:7='ab',<1>,2:0]\n" +
+				"[@3,8:10='cde',<2>,2:2]\n" +
+				"[@4,12:11='<EOF>',<-1>,3:0]\n",
+				null);
+		file.addLexerTest(input, "PredicatedKeywords", "L",
+				"enum enu a",
+				"enum!\n" +
+				"ID enu\n" +
+				"ID a\n" +
+				"[@0,0:3='enum',<1>,1:0]\n" +
+				"[@1,5:7='enu',<2>,1:5]\n" +
+				"[@2,9:9='a',<2>,1:9]\n" +
+				"[@3,10:9='<EOF>',<-1>,1:10]\n",
+				null);
+		return file;
 	}
 
 	private TestFile buildParseTrees() throws Exception {
