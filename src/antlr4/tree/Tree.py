c32a849cc86c1253589d6bb58170fc70369d3394
ericvergnaud <eric.vergnaud@wanadoo.fr>
2014-11-23 14:53:51 +0800

    added visitor code

--- src/antlr4/tree/Tree.py
+++ src/antlr4/tree/Tree.py
@@ -55,9 +55,36 @@ class ErrorNode(TerminalNode):
     pass
 
 class ParseTreeVisitor(object):
-    pass
+    def visit(self, tree):
+        return tree.accept(self)
 
-ParserRuleContext = None
+    def visitChildren(self, node):
+        result = self.defaultResult()
+        n = node.getChildCount()
+        for i in range(n):
+            if not self.shouldVisitNextChild(node, result):
+                return
+
+            c = node.getChild(i)
+            childResult = c.accept(self)
+            result = self.aggregateResult(result, childResult)
+
+        return result
+
+    def visitTerminal(self, node):
+        return self.defaultResult()
+
+    def visitErrorNode(self, node):
+        return self.defaultResult()
+
+    def defaultResult(self):
+        return None
+
+    def aggregateResult(self, aggregate, nextResult):
+        return nextResult
+
+    def shouldVisitNextChild(self, node, currentResult):
+        return True
 
 class ParseTreeListener(object):
 
