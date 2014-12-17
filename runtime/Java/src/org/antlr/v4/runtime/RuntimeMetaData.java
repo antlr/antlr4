49a8029985c8b784b5eb1f2b569279681e85372b
Terence Parr <parrt@antlr.org>
2014-12-16 19:09:17 -0800

    allow 4.4-generated parsers to work with new 4.5 runtime.

--- runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
+++ runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
@@ -74,7 +74,7 @@ public class RuntimeMetaData {
 	 * libraries to include a literal reference to the version of the ANTLR 4
 	 * runtime library the code was compiled against.</p>
 	 */
-	public static final String VERSION = "4.4.1-dev";
+	public static final String VERSION = "4.5";
 
 	/**
 	 * This class provides detailed information about a mismatch between the
