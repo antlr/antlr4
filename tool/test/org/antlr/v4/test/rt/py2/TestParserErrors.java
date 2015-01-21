e40c4e074f2c70588a339c663f345613e08d2d61
Terence Parr <parrt@antlr.org>
2015-01-21 10:17:56 -0800

    Fix recovery error; Fixes https://github.com/antlr/antlr4/issues/795

--- tool/test/org/antlr/v4/test/rt/py2/TestParserErrors.java
+++ tool/test/org/antlr/v4/test/rt/py2/TestParserErrors.java
@@ -37,6 +37,17 @@ public class TestParserErrors extends BasePython2Test {
 
 	/* this file and method are generated, any edit will be overwritten by the next generation */
 	@Test
+	public void testSingleTokenDeletionConsumption() throws Exception {
+		String grammar = "grammar T;\n" +
+	                  "set: ('b'|'c') ;\n" +
+	                  "a: 'a' set 'd' {System.out.println($set.stop);} ;";
+		String found = execParser("T.g4", grammar, "TParser", "TLexer", "TListener", "TVisitor", "a", "aabd", false);
+		assertEquals("[@2,2:2='b',<1>,1:2]\n", found);
+		assertEquals("line 1:1 extraneous input 'a' expecting {'b', 'c'}\n", this.stderrDuringParse);
+	}
+
+	/* this file and method are generated, any edit will be overwritten by the next generation */
+	@Test
 	public void testSingleTokenInsertion() throws Exception {
 		String grammar = "grammar T;\n" +
 	                  "a : 'a' 'b' 'c' ;";
@@ -62,6 +73,17 @@ public class TestParserErrors extends BasePython2Test {
 	                  "a : 'a' ('b'|'c') 'd' ;";
 		String found = execParser("T.g4", grammar, "TParser", "TLexer", "TListener", "TVisitor", "a", "ad", false);
 		assertEquals("", found);
+		assertEquals("line 1:1 missing {'b', 'c'} at 'd'\n", this.stderrDuringParse);
+	}
+
+	/* this file and method are generated, any edit will be overwritten by the next generation */
+	@Test
+	public void testSingleSetInsertionConsumption() throws Exception {
+		String grammar = "grammar T;\n" +
+	                  "set: ('b'|'c') ;\n" +
+	                  "a: 'a' set 'd' {System.out.println($set.stop);} ;";
+		String found = execParser("T.g4", grammar, "TParser", "TLexer", "TListener", "TVisitor", "a", "ad", false);
+		assertEquals("[@0,0:0='a',<3>,1:0]\n", found);
 		assertEquals("line 1:1 missing {'b', 'c'} at 'd'\n", this.stderrDuringParse);
 	}
 
