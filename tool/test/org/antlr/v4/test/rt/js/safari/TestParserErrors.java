8219262d200e012cb874a0140fbe2181b6053b7c
Terence Parr <parrt@antlr.org>
2015-01-22 11:41:20 -0800

    freshen unit tests

--- tool/test/org/antlr/v4/test/rt/js/safari/TestParserErrors.java
+++ tool/test/org/antlr/v4/test/rt/js/safari/TestParserErrors.java
@@ -39,8 +39,8 @@ public class TestParserErrors extends BaseTest {
 	@Test
 	public void testSingleTokenDeletionConsumption() throws Exception {
 		String grammar = "grammar T;\n" +
-	                  "set: ('b'|'c') ;\n" +
-	                  "a: 'a' set 'd' {System.out.println($set.stop);} ;";
+	                  "myset: ('b'|'c') ;\n" +
+	                  "a: 'a' myset 'd' {document.getElementById('output').value += $myset.stop + '\\n';} ;";
 		String found = execParser("T.g4", grammar, "TParser", "TLexer", "TListener", "TVisitor", "a", "aabd", false);
 		assertEquals("[@2,2:2='b',<1>,1:2]\n", found);
 		assertEquals("line 1:1 extraneous input 'a' expecting {'b', 'c'}\n", this.stderrDuringParse);
@@ -80,8 +80,8 @@ public class TestParserErrors extends BaseTest {
 	@Test
 	public void testSingleSetInsertionConsumption() throws Exception {
 		String grammar = "grammar T;\n" +
-	                  "set: ('b'|'c') ;\n" +
-	                  "a: 'a' set 'd' {System.out.println($set.stop);} ;";
+	                  "myset: ('b'|'c') ;\n" +
+	                  "a: 'a' myset 'd' {document.getElementById('output').value += $myset.stop + '\\n';} ;";
 		String found = execParser("T.g4", grammar, "TParser", "TLexer", "TListener", "TVisitor", "a", "ad", false);
 		assertEquals("[@0,0:0='a',<3>,1:0]\n", found);
 		assertEquals("line 1:1 missing {'b', 'c'} at 'd'\n", this.stderrDuringParse);
