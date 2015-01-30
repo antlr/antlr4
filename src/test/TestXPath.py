75d6784278028e6c7323e0083d1963a515dfcb93
ericvergnaud <eric.vergnaud@wanadoo.fr>
2015-01-30 21:49:50 +0800

    region of Path lexer using latest antlr

---
+++ src/test/TestXPath.py
@@ -0,0 +1,17 @@
+__author__ = 'ericvergnaud'
+
+import unittest
+import time
+import uuid
+
+class TestXPath(unittest.TestCase):
+
+    def testLoadable(self):
+        from antlr4.xpath import XPath
+        self.assertTrue(True)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
