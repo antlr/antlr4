3bf1a16607e971ab8dc8839f5c594f588d97a3ba
ericvergnaud <eric.vergnaud@wanadoo.fr>
2015-03-18 23:06:36 +0800

    added tests for params and locals

---
+++ tool/test/org/antlr/v4/test/rt/py2/TestParams.java
@@ -0,0 +1,36 @@
+package org.antlr.v4.test.rt.py2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestParams extends BasePython2Test {
+
+	@Test
+	public void testParamNoType() throws Exception {
+		String grammar = "grammar T;\n" +
+	                  "a[rows]: 'a' {print(self._input.getText())} ;";
+		String found = execParser("T.g4", grammar, "TParser", "TLexer", "TListener", "TVisitor", "a(None)", "a", false);
+		assertEquals("a\n", found);
+		assertNull(this.stderrDuringParse);
+	}
+
+	@Test
+	public void testParamList() throws Exception {
+		String grammar = "grammar T;\n" +
+	                  "a[list rows]: 'a' {print(self._input.getText())} # r1 | 'A' {print(self._input.getText())} # r2;";
+		String found = execParser("T.g4", grammar, "TParser", "TLexer", "TListener", "TVisitor", "a(None)", "a", false);
+		assertEquals("a\n", found);
+		assertNull(this.stderrDuringParse);
+	}
+	
+	@Test
+	public void testParamInit() throws Exception {
+		String grammar = "grammar T;\n" +
+	                  "a locals[i=23]: 'a' {print(str($i))} ;";
+		String found = execParser("T.g4", grammar, "TParser", "TLexer", "TListener", "TVisitor", "a", "a", false);
+		assertEquals("23\n", found);
+		assertNull(this.stderrDuringParse);
+	}
+
+
+}
