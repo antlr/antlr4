8beec1828377a6c2d2b05445ca9b7c83fcc79a55
Jason van Zyl <jason@tesla.io>
2015-04-19 13:24:07 -0400

    Add Maven build
    Account for the following:
    - deploying snapshots to OSSRH
    - releasing to Maven Central
    - Shaded JAR including the treelayout dependency
    - OSGi manifest
    - remove Eclipse IDE metadata for clean import
    - remove IDEA IDE metadata for clean import

--- antlr4-maven-plugin/src/main/java/org/antlr/mojo/antlr4/Antlr4ErrorLog.java
+++ antlr4-maven-plugin/src/main/java/org/antlr/mojo/antlr4/Antlr4ErrorLog.java
@@ -29,7 +29,6 @@
 package org.antlr.mojo.antlr4;
 
 import org.antlr.v4.Tool;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.tool.ANTLRMessage;
 import org.antlr.v4.tool.ANTLRToolListener;
 import org.apache.maven.plugin.logging.Log;
