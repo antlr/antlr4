db076c7986ab91ed503012b99cd952ddd85ef0ba
Terence Parr <parrt@antlr.org>
2015-05-19 12:14:54 -0700

    rm test Eric says I can

--- tool/test/org/antlr/v4/test/rt/js/node/TestInfinite.java
+++ tool/test/org/antlr/v4/test/rt/js/node/TestInfinite.java
@@ -14,18 +14,4 @@ public class TestInfinite extends BaseTest {
 		System.setProperty("antlr-javascript-test-dir", "/Users/ericvergnaud/Development/antlr4/antlr/antlr4-javascript/src/test");
 		System.setProperty("antlr-javascript-erase-test-dir", "false");
 	}
-	
-	@Test
-	public void testLexer() throws Exception {
-		String textToParse = "() any text 1\n"
-				+ "\n"
-				+ "            any text 2\n"
-				+ "any text 3";
-		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("infinite/TestLexer.g4");
-		mkdir(tmpdir);
-		writeFile(tmpdir, "TestLexer.g4", input);
-		input.close();
-		String found = execLexer("TestLexer.g4", null, "TestLexer", textToParse, false);
-	}
-
 }
