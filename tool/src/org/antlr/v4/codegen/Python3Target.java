02f325db12c28462e3371926bade28af830cd8ee
Terence Parr <parrt@antlr.org>
2015-06-22 10:30:57 -0700

    set version

--- tool/src/org/antlr/v4/codegen/Python3Target.java
+++ tool/src/org/antlr/v4/codegen/Python3Target.java
@@ -117,7 +117,7 @@ public class Python3Target extends Target {
 
 	@Override
 	public String getVersion() {
-		return "4.5";
+		return "4.5.1";
 	}
 
 	/** Avoid grammar symbols in this set to prevent conflicts in gen'd code. */
