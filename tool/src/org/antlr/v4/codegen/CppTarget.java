095fe36b66f8b56085d7cdd5331a3e5c34fcde74
David Sisson <dsisson@google.com>
2015-06-27 10:09:01 -0700

    Updated list of reserved keywords in C++.

--- tool/src/org/antlr/v4/codegen/CppTarget.java
+++ tool/src/org/antlr/v4/codegen/CppTarget.java
@@ -46,14 +46,21 @@ import java.util.Set;
 public class CppTarget extends Target {
 
 	protected static final String[] cppKeywords = {
-		"abstract", "assert", "boolean", "break", "byte", "case", "catch",
-		"char", "class", "const", "continue", "default", "do", "double", "else",
-		"enum", "extends", "false", "final", "finally", "float", "for",
-		"if", "implements", "import", "instanceof", "int", "interface",
-		"long", "native", "new", "null", "package", "private", "protected",
-		"public", "return", "short", "static", "strictfp", "super", "switch",
-		"synchronized", "this", "throw", "throws", "transient", "true", "try",
-		"void", "volatile", "while"
+		"alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand",
+		"bitor", "bool", "break", "case", "catch", "char", "char16_t",
+		"char32_t", "class", "compl", "concept", "const", "constexpr",
+		"const_cast", "continue", "decltype", "default", "delete", "do",
+		"double", "dynamic_cast", "else", "enum", "explicit", "export",
+		"extern", "false", "float", "for", "friend", "goto", "if",
+		"inline", "int", "long", "mutable", "namespace", "new",
+                "noexcept", "not", "not_eq", "nullptr", "operator", "or",
+                "or_eq", "private", "protected", "public", "register",
+                "reinterpret_cast", "requires", "return", "short", "signed",
+                "sizeof", "static", "static_assert", "static_cast", "struct",
+                "switch", "template", "this", "thread_local", "throw", "true",
+                "try", "typedef", "typeid", "typename", "union", "unsigned",
+                "using", "virtual", "void", "volatile", "wchar_t", "while",
+                "xor", "xor_eq"
 	};
 
 	/** Avoid grammar symbols in this set to prevent conflicts in gen'd code. */
