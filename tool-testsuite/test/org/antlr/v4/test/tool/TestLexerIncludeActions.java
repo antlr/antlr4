85467877e12e592bf392405d1457b9f3b17c36cf
nttdatahenriksorensen <henrik.sorensen@nttdata.com>
2015-08-18 10:58:17 -0700

    JUnit for include action

---
+++ tool-testsuite/test/org/antlr/v4/test/tool/TestLexerIncludeActions.java
@@ -0,0 +1,51 @@
+package org.antlr.v4.test.tool;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import org.antlr.v4.test.runtime.java.BaseTest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestLexerIncludeActions extends BaseTest {
+	// ----- ACTIONS --------------------------------------------------------
+
+	@Test public void testActionPerformIncludeSourceFile() throws Exception {
+		// prepare test files
+		String fn[] = {"test_Z0.test","test_Z1.test"};
+		File f[] = {new File(fn[0]),new File(fn[1])};
+		FileWriter f0=new FileWriter(f[0]);
+		FileWriter f1=new FileWriter(f[1]);
+		f0.write("E F G Z2 L M");
+		f0.close();
+		f1.write("H I J K");
+		f1.close();
+
+		String grammar =
+			"lexer grammar L;\n"+
+			"I : 'A'..'Z' {} ;\n"+
+			"CP: 'Z' ('0'|'1') { performIncludeSourceFile( \"test_\"+getText()+\".test\" ); skip(); };\n" +
+			"WS : (' '|'\\n') -> skip ;";
+		String found = execLexer("L.g4", grammar, "L", "A B C D Z1 N O P");
+		
+		// clean up test files
+		f[0].delete();
+		f[1].delete();
+
+		String expecting =
+			"[@0,0:0='A',<1>,1:0]\n" +
+			"[@1,2:2='B',<1>,1:2]\n" +
+			"[@2,4:4='C',<1>,1:4]\n" +
+			"[@3,6:6='D',<1>,1:6]\n" +
+			"[@4,11:11='N',<1>,1:11]\n" +
+			"[@5,13:13='O',<1>,1:13]\n" +
+			"[@6,15:15='P',<1>,1:15]\n" +
+			"[@7,16:15='<EOF>',<-1>,1:16]\n"
+					;
+		assertEquals(expecting, found);
+
+		
+	}
+
+}
