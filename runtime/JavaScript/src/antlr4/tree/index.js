a6bc4181828bce93c5b842791f6a32b5c1a0b5e5
Peter Boyer <peter.b.boyer@gmail.com>
2015-12-23 15:05:29 -0600

    Fix incorrect export in Javascript runtime tree/index.js
    Fix for #1039

--- runtime/JavaScript/src/antlr4/tree/index.js
+++ runtime/JavaScript/src/antlr4/tree/index.js
@@ -1,5 +1,5 @@
 var Tree = require('./Tree');
-exports.Trees = require('./Tree').Trees;
+exports.Trees = require('./Trees').Trees;
 exports.RuleNode = Tree.RuleNode;
 exports.ParseTreeListener = Tree.ParseTreeListener;
 exports.ParseTreeVisitor = Tree.ParseTreeVisitor;
