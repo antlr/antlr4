8954c1a072cb69fbea1d049862141a5123affa0d
Peter Boyer <peter.b.boyer@gmail.com>
2016-01-12 09:43:03 -0500

    Delete testing code

--- pb/TestLeftRecursion-1452198850336/node_modules/antlr4/dfa/index.js
+++
@@ -1,4 +0,0 @@
-exports.DFA = require('./DFA').DFA;
-exports.DFASerializer = require('./DFASerializer').DFASerializer;
-exports.LexerDFASerializer = require('./DFASerializer').LexerDFASerializer;
-exports.PredPrediction = require('./DFAState').PredPrediction;
--- pb/TestLeftRecursion-1452198850336/node_modules/antlr4/tree/index.js
+++
@@ -1,6 +0,0 @@
-var Tree = require('./Tree');
-exports.Trees = require('./Tree').Trees;
-exports.RuleNode = Tree.RuleNode;
-exports.ParseTreeListener = Tree.ParseTreeListener;
-exports.ParseTreeVisitor = Tree.ParseTreeVisitor;
-exports.ParseTreeWalker = Tree.ParseTreeWalker;
