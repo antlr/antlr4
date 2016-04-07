467db4ebda7311698837487ad4f2332ccb5527e9
David Tymon <david.tymon@gmail.com>
2016-04-07 15:00:39 +1000

    If an instance of a class deriving from ParserRuleContext is created with an invokingState of 0 then the constructor will incorrectly change this to null due to the falsey nature of 0.
    A similar problem exists in the base class of ParserRuleContext,
    that is RuleContext, where it will change an invokingState of 0
    to -1. Using the Java source as a reference, this appears to be a
    bug. The invokingState should be set to -1 when the parent is null.
    
    It looks like the code is trying to watch for an invokingState of
    null or undefined. However, I think this is unnecessary since the
    rule context classes generated from the grammar already handle
    these cases, for example:
    
    function ContentContext(parser, parent, invokingState) {
            if(parent===undefined) {
                parent = null;
            }
            if(invokingState===undefined || invokingState===null) {
                    invokingState = -1;
            }

--- runtime/JavaScript/src/antlr4/RuleContext.js
+++ runtime/JavaScript/src/antlr4/RuleContext.js
@@ -56,11 +56,11 @@ var INVALID_ALT_NUMBER = require('./atn/ATN').INVALID_ALT_NUMBER;
 function RuleContext(parent, invokingState) {
 	RuleNode.call(this);
 	// What context invoked this rule?
-	this.parentCtx = parent || null;
+	this.parentCtx = parent;
 	// What state invoked the rule associated with this context?
 	// The "return address" is the followState of invokingState
 	// If parent is null, this should be -1.
-	this.invokingState = invokingState || -1;
+	this.invokingState = (parent == null) ? -1 : invokingState;
 	return this;
 }
 
