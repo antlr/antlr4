a127f34a74cb6ecd57eb2a4bbf08035dfc4e3a8b
Mike Lischke <mike.lischke@oracle.com>
2016-06-22 10:56:38 +0200

    Changed Win deployment script to produce 2 smaller zips instead of a big one.

--- runtime/Cpp/runtime/deploy-windows.cmd
+++ runtime/Cpp/runtime/deploy-windows.cmd
@@ -5,7 +5,11 @@ if exist bin rmdir /S /Q bin
 if exist obj rmdir /S /Q obj
 if exist lib rmdir /S /Q lib
 if exist antlr4-runtime rmdir /S /Q antlr4-runtime
-if exist antlr4-cpp-runtime-windows.zip erase antlr4-cpp-runtime-windows.zip
+if exist antlr4-cpp-runtime-vs2013.zip erase antlr4-cpp-runtime-vs2013.zip
+if exist antlr4-cpp-runtime-vs2015.zip erase antlr4-cpp-runtime-vs2015.zip
+
+rem Headers
+xcopy src\*.h antlr4-runtime\ /s
 
 rem Binaries
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat" (
@@ -15,6 +19,17 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDe
   msbuild antlr4cpp-vs2013.vcxproj /p:configuration="Release Static" /p:platform=Win32
   msbuild antlr4cpp-vs2013.vcxproj /p:configuration="Release DLL" /p:platform=x64
   msbuild antlr4cpp-vs2013.vcxproj /p:configuration="Release Static" /p:platform=x64
+  
+  7z a antlr4-cpp-runtime-vs2013.zip antlr4-runtime
+  xcopy bin\*.dll lib\ /s
+  xcopy bin\*.lib lib\ /s
+  7z a antlr4-cpp-runtime-vs2013.zip lib
+  
+  rmdir /S /Q lib
+  rmdir /S /Q bin
+  rmdir /S /Q obj
+  
+  rem if exist antlr4-cpp-runtime-vs2013.zip copy antlr4-cpp-runtime-vs2013.zip ~/antlr/sites/website-antlr4/download
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" (
@@ -24,23 +39,19 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDe
   msbuild antlr4cpp-vs2015.vcxproj /p:configuration="Release Static" /p:platform=Win32
   msbuild antlr4cpp-vs2015.vcxproj /p:configuration="Release DLL" /p:platform=x64
   msbuild antlr4cpp-vs2015.vcxproj /p:configuration="Release Static" /p:platform=x64
+  
+  7z a antlr4-cpp-runtime-vs2015.zip antlr4-runtime
+  xcopy bin\*.dll lib\ /s
+  xcopy bin\*.lib lib\ /s
+  7z a antlr4-cpp-runtime-vs2015.zip lib
+  
+  rmdir /S /Q lib
+  rmdir /S /Q bin
+  rmdir /S /Q obj
+  
+  rem if exist antlr4-cpp-runtime-vs2015.zip copy antlr4-cpp-runtime-vs2015.zip ~/antlr/sites/website-antlr4/download
 )
 
-rem Headers
-xcopy src\*.h antlr4-runtime\ /s
-
-rem Zip up and clean up
-7z a antlr4-cpp-runtime-windows.zip antlr4-runtime
-xcopy bin\*.dll lib\ /s
-xcopy bin\*.lib lib\ /s
-7z a antlr4-cpp-runtime-windows.zip lib
-
 rmdir /S /Q antlr4-runtime
-rmdir /S /Q lib
-rmdir /S /Q bin
-rmdir /S /Q obj
-
-rem Deploy
-rem copy antlr4-cpp-runtime-windows.zip ~/antlr/sites/website-antlr4/download
 
 :end
