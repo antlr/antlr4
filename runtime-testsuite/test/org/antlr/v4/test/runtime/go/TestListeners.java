edbf3225efbcffe9f16f28cbb42c5d95a5de9996
Will Faught <will.faught@gmail.com>
2016-07-06 10:18:15 -0700

    Fix LRWithLabelsListener to call EList.String

--- runtime-testsuite/test/org/antlr/v4/test/runtime/go/TestListeners.java
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/go/TestListeners.java
@@ -120,7 +120,7 @@ public class TestListeners extends BaseTest {
 	@Test
 	public void testLRWithLabels() throws Exception {
 		mkdir(parserpkgdir);
-		StringBuilder grammarBuilder = new StringBuilder(709);
+		StringBuilder grammarBuilder = new StringBuilder(726);
 		grammarBuilder.append("grammar T;\n");
 		grammarBuilder.append("@parser::header {\n");
 		grammarBuilder.append("}\n");
@@ -135,7 +135,7 @@ public class TestListeners extends BaseTest {
 		grammarBuilder.append("}\n");
 		grammarBuilder.append("\n");
 		grammarBuilder.append("func (*LeafListener) ExitCall(ctx *CallContext) {\n");
-		grammarBuilder.append("	fmt.Printf(\"%s %s\", ctx.E().GetStart().GetText(), ctx.EList())\n");
+		grammarBuilder.append("	fmt.Printf(\"%s %s\", ctx.E().GetStart().GetText(), ctx.EList().String(nil, nil))\n");
 		grammarBuilder.append("}\n");
 		grammarBuilder.append("\n");
 		grammarBuilder.append("func (*LeafListener) ExitInt(ctx *IntContext) {\n");
