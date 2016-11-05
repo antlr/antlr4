0799e838bbb9169d609fa04667a51ad6ae40a40f
parrt <parrt@cs.usfca.edu>
2016-11-05 09:32:04 -0700

    weird. missed a few files. these are to simplify get filename functionality.

--- tool/src/org/antlr/v4/codegen/target/GoTarget.java
+++ tool/src/org/antlr/v4/codegen/target/GoTarget.java
@@ -2,6 +2,7 @@ package org.antlr.v4.codegen.target;
 
 import org.antlr.v4.codegen.CodeGenerator;
 import org.antlr.v4.codegen.Target;
+import org.antlr.v4.parse.ANTLRParser;
 import org.antlr.v4.tool.Grammar;
 import org.antlr.v4.tool.ast.GrammarAST;
 import org.stringtemplate.v4.ST;
@@ -132,6 +133,65 @@ public class GoTarget extends Target {
 		STGroup result = super.loadTemplates();
 		result.registerRenderer(String.class, new JavaStringRenderer(), true);
 		return result;
+	}
+
+	public String getRecognizerFileName(boolean header) {
+		CodeGenerator gen = getCodeGenerator();
+		Grammar g = gen.g;
+		assert g!=null;
+		String name;
+		switch ( g.getType()) {
+			case ANTLRParser.PARSER:
+				name = g.name.endsWith("Parser") ? g.name.substring(0, g.name.length()-6) : g.name;
+				return name.toLowerCase()+"_parser.go";
+			case ANTLRParser.LEXER:
+				name = g.name.endsWith("Lexer") ? g.name.substring(0, g.name.length()-5) : g.name; // trim off "lexer"
+				return name.toLowerCase()+"_lexer.go";
+			case ANTLRParser.COMBINED:
+				return g.name.toLowerCase()+"_parser.go";
+			default :
+				return "INVALID_FILE_NAME";
+		}
+	}
+
+	/** A given grammar T, return the listener name such as
+	 *  TListener.java, if we're using the Java target.
+ 	 */
+	public String getListenerFileName(boolean header) {
+		CodeGenerator gen = getCodeGenerator();
+		Grammar g = gen.g;
+		assert g.name != null;
+		return g.name.toLowerCase()+"_listener.go";
+	}
+
+	/** A given grammar T, return the visitor name such as
+	 *  TVisitor.java, if we're using the Java target.
+ 	 */
+	public String getVisitorFileName(boolean header) {
+		CodeGenerator gen = getCodeGenerator();
+		Grammar g = gen.g;
+		assert g.name != null;
+		return g.name.toLowerCase()+"_visitor.go";
+	}
+
+	/** A given grammar T, return a blank listener implementation
+	 *  such as TBaseListener.java, if we're using the Java target.
+ 	 */
+	public String getBaseListenerFileName(boolean header) {
+		CodeGenerator gen = getCodeGenerator();
+		Grammar g = gen.g;
+		assert g.name != null;
+		return g.name.toLowerCase()+"_base_listener.go";
+	}
+
+	/** A given grammar T, return a blank listener implementation
+	 *  such as TBaseListener.java, if we're using the Java target.
+ 	 */
+	public String getBaseVisitorFileName(boolean header) {
+		CodeGenerator gen = getCodeGenerator();
+		Grammar g = gen.g;
+		assert g.name != null;
+		return g.name.toLowerCase()+"_base_visitor.go";
 	}
 
 	protected static class JavaStringRenderer extends StringRenderer {
