a3d37a082ed3339a37ed274addc4ceae0a838a96
Ewan Mellor <commits@ewanmellor.org>
2016-11-06 23:47:15 -0800

    Work around apparent rvm bug that is in Travis's Xcode image.

--- travis/before_install-macos.sh
+++ travis/before_install-macos.sh
@@ -7,4 +7,9 @@ thisdir=$(dirname "$0")
 brew update
 brew install mono python3
 
+# Work around apparent rvm bug that is in Travis's Xcode image.
+# https://github.com/direnv/direnv/issues/210
+# https://github.com/travis-ci/travis-ci/issues/6307
+shell_session_update() { :; }
+
 "$thisdir/before_install-common.sh"
