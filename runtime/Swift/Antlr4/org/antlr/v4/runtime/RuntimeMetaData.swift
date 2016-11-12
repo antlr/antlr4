12be972de8dd671a98979f44af845a4b3057b4e7
janyou <janyou.antlr@outlook.com>
2016-11-12 15:15:52 +0800

    Update RuntimeMetaData.swift

--- runtime/Swift/Antlr4/org/antlr/v4/runtime/RuntimeMetaData.swift
+++ runtime/Swift/Antlr4/org/antlr/v4/runtime/RuntimeMetaData.swift
@@ -92,7 +92,7 @@ public class RuntimeMetaData {
      * omitted.</li>
      * </ul>
      */
-    public static let VERSION: String = "4.5.1"
+    public static let VERSION: String = "4.5.3"
 
     /**
      * Gets the currently executing version of the ANTLR 4 runtime library.
