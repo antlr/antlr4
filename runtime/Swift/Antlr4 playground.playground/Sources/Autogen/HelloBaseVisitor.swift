b0c38243578c540893efc4372e2381a8f32e0094
Ewan Mellor <commits@ewanmellor.org>
2016-12-01 01:19:52 -0800

    Create a new Xcode playground for Swift.
    This takes the example from
    https://github.com/janyou/Antlr-Swift-Runtime/tree/master/Test and packages
    it into an Xcode playground.  This allows the user to interact with the
    grammar directly inside Xcode, and serves as a live demo of the Swift support.
    
    Update the docs to match.

---
+++ runtime/Swift/Antlr4 playground.playground/Sources/Autogen/HelloBaseVisitor.swift
@@ -0,0 +1,20 @@
+// Generated from Hello.g4 by ANTLR 4.6
+import Antlr4
+
+/**
+ * This class provides an empty implementation of {@link HelloVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+open class HelloBaseVisitor<T>: AbstractParseTreeVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	open func visitR(_ ctx: HelloParser.RContext) -> T? { return visitChildren(ctx) }
+}
