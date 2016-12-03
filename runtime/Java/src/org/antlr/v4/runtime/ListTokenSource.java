891915de846d39c17b8357a17a7a8626a2869dfc
parrt <parrt@cs.usfca.edu>
2016-12-03 11:34:30 -0800

    Update copyright on java, xml files. Make BSD license conform to standard.

--- runtime/Java/src/org/antlr/v4/runtime/ListTokenSource.java
+++ runtime/Java/src/org/antlr/v4/runtime/ListTokenSource.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2012 The ANTLR Project Contributors. All rights reserved.
+ * Use is of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
+
 package org.antlr.v4.runtime;
 
 import org.antlr.v4.runtime.misc.Pair;
