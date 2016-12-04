1b1a9862c4da857737c8a06e3fd3f6ceb6b9534c
parrt <parrt@cs.usfca.edu>
2016-12-04 11:42:22 -0800

    contributors->authors

--- runtime/Python3/src/antlr4/dfa/DFA.py
+++ runtime/Python3/src/antlr4/dfa/DFA.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2012 The ANTLR Project Contributors. All rights reserved.
+# Copyright (c) 2012 The ANTLR Project Authors. All rights reserved.
 # Use of this file is governed by the BSD 3-clause license that
 # can be found in the LICENSE.txt file in the project root.
 from antlr4.atn.ATNState import StarLoopEntryState
