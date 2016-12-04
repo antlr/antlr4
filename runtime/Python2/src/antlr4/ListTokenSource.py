76d421a89ec458e16e562b219266cdb52f2050d5
parrt <parrt@cs.usfca.edu>
2016-12-04 10:39:38 -0800

    add copyrights on Python2

--- runtime/Python2/src/antlr4/ListTokenSource.py
+++ runtime/Python2/src/antlr4/ListTokenSource.py
@@ -1,4 +1,10 @@
 #
+# Copyright (c) 2012 The ANTLR Project Contributors. All rights reserved.
+# Use is of this file is governed by the BSD 3-clause license that
+# can be found in the LICENSE.txt file in the project root.
+#
+
+#
 # Provides an implementation of {@link TokenSource} as a wrapper around a list
 # of {@link Token} objects.
 #
