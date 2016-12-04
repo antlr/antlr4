03925a27539d1969c6404a0d22cb817b68d85b38
parrt <parrt@cs.usfca.edu>
2016-12-04 11:19:27 -0800

    add copyrights on go

--- runtime/Go/antlr/tree.go
+++ runtime/Go/antlr/tree.go
@@ -1,3 +1,7 @@
+/* Copyright (c) 2012 The ANTLR Project Contributors. All rights reserved.
+ * Use is of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
 package antlr
 
 // The basic notion of a tree has a parent, a payload, and a list of children.
