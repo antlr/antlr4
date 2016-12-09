53918ab41e78632e80bda8aadff5926a9c7ad4fd
parrt <parrt@cs.usfca.edu>
2016-12-09 10:19:31 -0800

    add unit test for #1298

--- runtime-testsuite/test/org/antlr/v4/test/runtime/descriptors/ParseTreesDescriptors.java
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/descriptors/ParseTreesDescriptors.java
@@ -74,6 +74,43 @@ public class ParseTreesDescriptors {
 
 	}
 
+	public static class ExtraTokensShouldBeInTree extends BaseParserTestDescriptor {
+		public String input = "${ ? a ?}";
+		public String output = "(s ${ ? (v a) ? })\n";
+		public String errors =
+			"line 1:3 extraneous input '?' expecting {'a', 'b'}\n"+
+			"line 1:7 extraneous input '?' expecting '}'\n";
+		public String startRule = "s";
+		public String grammarName = "T";
+
+		/**
+		 grammar T;
+
+		 s
+		 @init {
+		 <BuildParseTrees()>
+		 }
+		 @after {
+		 <ToStringTree("$ctx"):writeln()>
+		 }
+   		   : '${' v '}'
+		   ;
+
+		 v : A #altA
+		   | B #altB
+		   ;
+
+		 A : 'a' ;
+		 B : 'b' ;
+
+		 WHITESPACE : [ \n\t\r]+ -> channel(HIDDEN) ;
+
+		 ERROR : . ;
+		 */
+		@CommentHasStringValue
+		public String grammar;
+	}
+
 	public static class NoViableAlt extends BaseParserTestDescriptor {
 		public String input = "z";
 		public String output = "(a z)\n";
