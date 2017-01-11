a053d908fb9559fd607f1d3223275ef2443ec4e8
robert <robert@semmle.com>
2017-01-11 15:28:59 +0000

    fix missing variable declarations

--- runtime/JavaScript/src/antlr4/atn/ParserATNSimulator.js
+++ runtime/JavaScript/src/antlr4/atn/ParserATNSimulator.js
@@ -1444,7 +1444,7 @@ ParserATNSimulator.prototype.precedenceTransition = function(config, pt,  collec
                 c = new ATNConfig({state:pt.target}, config); // no pred context
             }
         } else {
-            newSemCtx = SemanticContext.andContext(config.semanticContext, pt.getPredicate());
+            var newSemCtx = SemanticContext.andContext(config.semanticContext, pt.getPredicate());
             c = new ATNConfig({state:pt.target, semanticContext:newSemCtx}, config);
         }
     } else {
