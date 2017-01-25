09d45f5d6e10d8959393045f2543842afe3f4331
Dong Xie <xied75@gmail.com>
2017-01-25 00:24:36 +0000

    [csharp] update travis for dotnet

---
+++ .travis/before-install-linux-dotnet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+sudo apt-get update
+
+sudo apt-get install dotnet-dev-1.0.0-preview2.1-003177
+
