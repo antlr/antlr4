d9d4d8c6c4756f1d59c7dfbea078cdd2645ec5dd
Ben Hamilton <beng@fb.com>
2017-01-30 08:49:05 -0800

    Restore Python3 FileStream to ascii/strict

--- runtime/Python3/src/antlr4/FileStream.py
+++ runtime/Python3/src/antlr4/FileStream.py
@@ -16,11 +16,11 @@ from antlr4.InputStream import InputStream
 
 class FileStream(InputStream):
 
-    def __init__(self, fileName:str, encoding:str='utf-8', errors:str='replace'):
+    def __init__(self, fileName:str, encoding:str='ascii', errors:str='strict'):
         super().__init__(self.readDataFrom(fileName, encoding, errors))
         self.fileName = fileName
 
-    def readDataFrom(self, fileName:str, encoding:str, errors:str='replace'):
+    def readDataFrom(self, fileName:str, encoding:str, errors:str='strict'):
         # read binary to avoid line ending conversion
         with open(fileName, 'rb') as file:
             bytes = file.read()
