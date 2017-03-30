e6a9adf27670c1cf51734dccffe0f7b7898f6e25
parrt <parrt@cs.usfca.edu>
2017-03-30 11:06:44 -0700

    update 4.6 -> 4.7 in files

--- runtime/Python3/src/antlr4/xpath/XPath.py
+++ runtime/Python3/src/antlr4/xpath/XPath.py
@@ -119,7 +119,7 @@ class XPathLexer(Lexer):
 
     def __init__(self, input=None):
         super().__init__(input)
-        self.checkVersion("4.6")
+        self.checkVersion("4.7")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
