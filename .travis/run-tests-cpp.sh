0e573fb0a5d8e5a3ce8a01837dfa313ad4664d00
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-06-27 00:50:49 +0800

    categorise tests to fix Travis timeout

--- .travis/run-tests-cpp.sh
+++ .travis/run-tests-cpp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mvn -q -Dtest=cpp.* test # timeout due to no output for 10 min on travis if in parallel
+if [ $GROUP == "LEXER" ]; then GROUPS=-Dgroups="org.antlr.v4.test.runtime.category.LexerTests"
+elif [ $GROUP == "PARSER" ]; then GROUPS=-Dgroups="org.antlr.v4.test.runtime.category.ParserTests"
+elif [ $GROUP == "RECURSION" ]; then GROUPS=-Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests"
+else GROUPS=
+fi
+
+mvn -q $GROUPS -Dtest=cpp.* test # timeout due to no output for 10 min on travis if in parallel
--- .travis/run-tests-swift.sh
+++ .travis/run-tests-swift.sh
@@ -17,4 +17,10 @@ fi
 swift --version
 swift build --version
 
-mvn -q -Dtest=swift.* test
+if [ $GROUP == "LEXER" ]; then GROUPS=-Dgroups="org.antlr.v4.test.runtime.category.LexerTests"
+elif [ $GROUP == "PARSER" ]; then GROUPS=-Dgroups="org.antlr.v4.test.runtime.category.ParserTests"
+elif [ $GROUP == "RECURSION" ]; then GROUPS=-Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests"
+else GROUPS=
+fi
+
+mvn -q $GROUPS -Dtest=swift.* test
