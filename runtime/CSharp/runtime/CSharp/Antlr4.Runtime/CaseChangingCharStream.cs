c941919d3d6c8e0e650ff1402c149f5b6c90d109
Sebastian Lang <admin@sebastian-lang.eu>
2017-12-01 15:09:14 +0100

    Add CaseChangingCharStream that converts symbols to upper or lower case

---
+++ runtime/CSharp/runtime/CSharp/Antlr4.Runtime/CaseChangingCharStream.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Antlr4.Runtime.Misc;
+
+namespace Antlr4.Runtime
+{
+    public class CaseChangingCharStream : ICharStream
+    {
+        private ICharStream stream;
+        private bool upper;
+
+        public CaseChangingCharStream(ICharStream stream, bool upper)
+        {
+            this.stream = stream;
+            this.upper = upper;
+        }
+
+        public int Index => stream.Index;
+
+        public int Size => stream.Size;
+
+        public string SourceName => stream.SourceName;
+
+        public void Consume() => stream.Consume();
+
+        [return: NotNull]
+        public string GetText(Interval interval) => stream.GetText(interval);
+
+        public int LA(int i)
+        {
+            int c = stream.LA(i);
+
+            if (c <= 0) return c;
+
+            var o = Convert.ToChar(c);
+
+            if (upper) return Convert.ToInt32(char.ToUpper(o));
+
+            return Convert.ToInt32(char.ToLower(o));
+        }
+
+        public int Mark() => stream.Mark();
+
+        public void Release(int marker) => stream.Release(marker);
+
+        public void Seek(int index) => stream.Seek(index);
+    }
+}
