2a3530f513346f497fae13d603d162d1bc829c32
Alex Mykyta <amykyta3@users.noreply.github.com>
2018-11-15 20:05:30 -0800

    Fix install dependency for python3.4
    Add install dependency for "typing" backport if using anything older than Python3.5

--- runtime/Python3/setup.py
+++ runtime/Python3/setup.py
@@ -5,6 +5,9 @@ setup(
     version='4.7.1',
     packages=['antlr4', 'antlr4.atn', 'antlr4.dfa', 'antlr4.tree', 'antlr4.error', 'antlr4.xpath'],
     package_dir={'': 'src'},
+    install_requires=[
+        "typing ; python_version<'3.5'",
+    ],
     url='http://www.antlr.org',
     license='BSD',
     author='Eric Vergnaud, Terence Parr, Sam Harwell',
