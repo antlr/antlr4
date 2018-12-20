2d284002344faf6ccf2f145eade4b793c6c193b3
WalterCouto <walter_couto@hotmail.com>
2018-12-20 11:02:35 -0500

    C++ avoid warning in visual studio build due to clang specific directive
    The revently added clang directive cause a new warning for Visual Studio build. Need to include the new code with a directive checking for visual studio

--- runtime/Cpp/runtime/src/support/CPPUtils.cpp
+++ runtime/Cpp/runtime/src/support/CPPUtils.cpp
@@ -50,8 +50,10 @@ namespace antlrcpp {
             break;
           }
           // else fall through
+#ifndef _MSC_VER
 #if __has_cpp_attribute(clang::fallthrough)
           [[clang::fallthrough]];
+#endif
 #endif
 
         default:
