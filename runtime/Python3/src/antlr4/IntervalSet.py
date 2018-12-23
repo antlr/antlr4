03fe805ac71e978f16d49ba2f5098b82f1774f50
You Kaichao <youkaichao@gmail.com>
2018-12-23 19:59:15 +0800

    bugfix: change xrange to list(range) for python3

--- runtime/Python3/src/antlr4/IntervalSet.py
+++ runtime/Python3/src/antlr4/IntervalSet.py
@@ -13,7 +13,7 @@ class Interval(object):
     def __init__(self, start, stop):
         self.start = start
         self.stop = stop
-        self.range = xrange(start, stop)
+        self.range = list(range(start, stop))
 
     def __contains__(self, item):
         return item in self.range
