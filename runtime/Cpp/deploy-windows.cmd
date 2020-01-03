c487b1a58012247ff5490fc1d4689d5de361cdf3
Mike Lischke <mike.lischke@oracle.com>
2020-01-03 09:01:13 +0100

    Create pre-built binaries for VS 2017/19 instead of older VS versions

--- runtime/Cpp/deploy-windows.cmd
+++ runtime/Cpp/deploy-windows.cmd
@@ -5,52 +5,51 @@ if exist bin rmdir /S /Q runtime\bin
 if exist obj rmdir /S /Q runtime\obj
 if exist lib rmdir /S /Q lib
 if exist antlr4-runtime rmdir /S /Q antlr4-runtime
-if exist antlr4-cpp-runtime-vs2013.zip erase antlr4-cpp-runtime-vs2013.zip
-if exist antlr4-cpp-runtime-vs2015.zip erase antlr4-cpp-runtime-vs2015.zip
+if exist antlr4-cpp-runtime-vs2017.zip erase antlr4-cpp-runtime-vs2017.zip
+if exist antlr4-cpp-runtime-vs2019.zip erase antlr4-cpp-runtime-vs2019.zip
 
 rem Headers
 xcopy runtime\src\*.h antlr4-runtime\ /s
 
 rem Binaries
-rem VS 2013 disabled by default. Change the X to a C to enable it.
-if exist "X:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat" (
-  call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 15.0\Common7\Tools\VsDevCmd.bat" (
+  call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\Common7\Tools\VsDevCmd.bat"
 
   pushd runtime
-  msbuild antlr4cpp-vs2013.vcxproj /p:configuration="Release DLL" /p:platform=Win32
-  msbuild antlr4cpp-vs2013.vcxproj /p:configuration="Release DLL" /p:platform=x64
+  msbuild antlr4cpp-vs2017.vcxproj /p:configuration="Release DLL" /p:platform=Win32
+  msbuild antlr4cpp-vs2017.vcxproj /p:configuration="Release DLL" /p:platform=x64
   popd
   
-  7z a antlr4-cpp-runtime-vs2013.zip antlr4-runtime
+  7z a antlr4-cpp-runtime-vs2017.zip antlr4-runtime
   xcopy runtime\bin\*.dll lib\ /s
   xcopy runtime\bin\*.lib lib\ /s
-  7z a antlr4-cpp-runtime-vs2013.zip lib
+  7z a antlr4-cpp-runtime-vs2017.zip lib
   
   rmdir /S /Q lib
   rmdir /S /Q runtime\bin
   rmdir /S /Q runtime\obj
   
-  rem if exist antlr4-cpp-runtime-vs2013.zip copy antlr4-cpp-runtime-vs2013.zip ~/antlr/sites/website-antlr4/download
+  rem if exist antlr4-cpp-runtime-vs2017.zip copy antlr4-cpp-runtime-vs2017.zip ~/antlr/sites/website-antlr4/download
 )
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" (
-  call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 16.0\Common7\Tools\VsDevCmd.bat" (
+  call "C:\Program Files (x86)\Microsoft Visual Studio 16.0\Common7\Tools\VsDevCmd.bat"
 
   pushd runtime
-  msbuild antlr4cpp-vs2015.vcxproj /p:configuration="Release DLL" /p:platform=Win32
-  msbuild antlr4cpp-vs2015.vcxproj /p:configuration="Release DLL" /p:platform=x64
+  msbuild antlr4cpp-vs2019.vcxproj /p:configuration="Release DLL" /p:platform=Win32
+  msbuild antlr4cpp-vs2019.vcxproj /p:configuration="Release DLL" /p:platform=x64
   popd
   
-  7z a antlr4-cpp-runtime-vs2015.zip antlr4-runtime
+  7z a antlr4-cpp-runtime-vs2019.zip antlr4-runtime
   xcopy runtime\bin\*.dll lib\ /s
   xcopy runtime\bin\*.lib lib\ /s
-  7z a antlr4-cpp-runtime-vs2015.zip lib
+  7z a antlr4-cpp-runtime-vs2019.zip lib
   
   rmdir /S /Q lib
   rmdir /S /Q runtime\bin
   rmdir /S /Q runtime\obj
   
-  rem if exist antlr4-cpp-runtime-vs2015.zip copy antlr4-cpp-runtime-vs2015.zip ~/antlr/sites/website-antlr4/download
+  rem if exist antlr4-cpp-runtime-vs2019.zip copy antlr4-cpp-runtime-vs2019.zip ~/antlr/sites/website-antlr4/download
 )
 
 rmdir /S /Q antlr4-runtime
