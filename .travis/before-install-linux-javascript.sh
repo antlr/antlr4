18f5f3cd27661e5f549abf7eae5d120fcceb2ce5
Camilo Roca <camilo.roca@cobi.bike>
2020-02-03 20:31:57 +0100

    fix: bump nodejs test version to support classes

--- .travis/before-install-linux-javascript.sh
+++ .travis/before-install-linux-javascript.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 sudo apt-get update -qq
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -qq nodejs
 node --version
