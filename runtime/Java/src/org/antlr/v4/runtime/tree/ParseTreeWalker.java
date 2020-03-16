d832f90a70906fa5f9a624c0410a88f75d4c60cc
Song Yang <songyang1218@gmail.com>
2020-03-16 00:16:03 -0400

    Correct doc on exitRule

--- runtime/Java/src/org/antlr/v4/runtime/tree/ParseTreeWalker.java
+++ runtime/Java/src/org/antlr/v4/runtime/tree/ParseTreeWalker.java
@@ -53,7 +53,7 @@ public class ParseTreeWalker {
 
 	/**
 	 * Exits a grammar rule by first triggering the event specific to the given parse tree node
-	 * then by triggering the generic event {@link ParseTreeListener#enterEveryRule}
+	 * then by triggering the generic event {@link ParseTreeListener#exitEveryRule}
 	 * @param listener The listener responding to the trigger events
 	 * @param r The grammar rule containing the rule context
 	 */
