d5e2e4b4f3c3f1f759bc608d98d1c6e41acfe75c
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-10-04 00:01:56 +0800

    try fix travis

--- .travis/before-install-linux-javascript.sh
+++ .travis/before-install-linux-javascript.sh
@@ -6,5 +6,6 @@ sudo apt-get update -qq
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -qq nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | sudo -E bash -
 sudo nvm use 14
 node --version
