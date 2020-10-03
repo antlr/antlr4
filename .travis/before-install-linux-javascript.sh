292811ce81103af7ccb19c961fa4cca15cdc6a4a
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-10-04 00:19:54 +0800

    try fix travis

--- .travis/before-install-linux-javascript.sh
+++ .travis/before-install-linux-javascript.sh
@@ -4,10 +4,15 @@ set -euo pipefail
 
 sudo apt-get update -qq
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -qq nodejs
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | sudo -E bash -
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-sudo nvm use 14
+## download and install nodejs
+#curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+#sudo apt-get install -qq nodejs
+#
+## download and install nvm
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | sudo -E bash -
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#
+## use v14 and check
+#sudo nvm use 14
 node --version
