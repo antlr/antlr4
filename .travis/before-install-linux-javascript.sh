90e78a9cb562c523bac1c86e3208f6691f32e1d3
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-10-03 23:41:29 +0800

    try fix travis

--- .travis/before-install-linux-javascript.sh
+++ .travis/before-install-linux-javascript.sh
@@ -3,8 +3,5 @@
 set -euo pipefail
 
 sudo apt-get update -qq
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -qq nodejs
-sudo npm install -g n
-n 14
+sudo n 14
 node --version
