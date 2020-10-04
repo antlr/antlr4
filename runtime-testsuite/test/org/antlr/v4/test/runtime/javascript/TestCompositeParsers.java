afa772e4fa659922d800fd68c97afe2c8eb29c4a
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-10-04 12:59:53 +0800

    reenable tests

--- runtime-testsuite/test/org/antlr/v4/test/runtime/javascript/TestCompositeParsers.java
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/javascript/TestCompositeParsers.java
@@ -9,11 +9,9 @@ package org.antlr.v4.test.runtime.javascript;
 import org.antlr.v4.test.runtime.BaseRuntimeTest;
 import org.antlr.v4.test.runtime.RuntimeTestDescriptor;
 import org.antlr.v4.test.runtime.descriptors.CompositeParsersDescriptors;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-@Ignore
 @RunWith(Parameterized.class)
 public class TestCompositeParsers extends BaseRuntimeTest {
 	public TestCompositeParsers(RuntimeTestDescriptor descriptor) {
