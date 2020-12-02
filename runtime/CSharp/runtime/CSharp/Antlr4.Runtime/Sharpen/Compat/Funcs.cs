4884a0fecb19687b8d073bbb3ca7e4757079c4b4
Ivan Kochurkin <kvanttt@gmail.com>
2020-12-02 17:51:54 +0300

    Drop support of outdated net35, remove mono and vs2013 .csproj and .sln files

--- runtime/CSharp/runtime/CSharp/Antlr4.Runtime/Sharpen/Compat/Funcs.cs
+++ runtime/CSharp/runtime/CSharp/Antlr4.Runtime/Sharpen/Compat/Funcs.cs
@@ -13,10 +13,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR TArg PARTICULAR PURPOSE AND
@@ -25,17 +25,11 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 //
 
-#if !NET35PLUS
-
-using System.Runtime.CompilerServices;
-
 namespace Antlr4.Runtime.Sharpen {
 
 	public delegate TResult Func<TResult> ();
-	
+
 	public delegate TResult Func<T, TResult> (T arg);
-	
+
 	public delegate TResult Func<in T1, in T2, out TResult> (T1 arg1, T2 arg2);
 }
-
-#endif
