5eedc1e65a2967891921eec2310cdb64490fe7f2
David Phillips <david@acz.org>
2020-12-03 15:00:29 -0800

    Java: add newline to version mismatch messages

--- runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
+++ runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
@@ -157,11 +157,11 @@ public class RuntimeMetaData {
 			!getMajorMinorVersion(runtimeVersion).equals(getMajorMinorVersion(compileTimeVersion));
 
 		if ( runtimeConflictsWithGeneratingTool ) {
-			System.err.printf("ANTLR Tool version %s used for code generation does not match the current runtime version %s",
+			System.err.printf("ANTLR Tool version %s used for code generation does not match the current runtime version %s%n",
 							  generatingToolVersion, runtimeVersion);
 		}
 		if ( runtimeConflictsWithCompileTimeTool ) {
-			System.err.printf("ANTLR Runtime version %s used for parser compilation does not match the current runtime version %s",
+			System.err.printf("ANTLR Runtime version %s used for parser compilation does not match the current runtime version %s%n",
 							  compileTimeVersion, runtimeVersion);
 		}
 	}
