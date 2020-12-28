925c0a6b8baf38d5419f07a974a9f73dfc292753
Ivan Kochurkin <kvanttt@gmail.com>
2020-12-28 16:06:19 +0300

    Fix CI scripts; use dotnet everywhere for testing; remove before-install-linux-csharp.sh, run-tests-csharp.sh

--- .travis/before-install-linux-dotnet.sh
+++ .travis/before-install-linux-dotnet.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-# install dotnet
-sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-sudo apt-get update
-sudo apt-get --allow-unauthenticated install dotnet-dev-1.0.4
-
+wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
