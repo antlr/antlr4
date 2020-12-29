c12821d15f0e0a5d4abd3b2c44291ed2c54ccdf6
Ivan Kochurkin <kvanttt@gmail.com>
2020-12-29 15:22:31 +0300

    Add FRAGMENT (-3) constant to Token

--- runtime/Java/src/org/antlr/v4/runtime/Token.java
+++ runtime/Java/src/org/antlr/v4/runtime/Token.java
@@ -13,23 +13,28 @@ package org.antlr.v4.runtime;
 public interface Token {
 	public static final int INVALID_TYPE = 0;
 
-    /** During lookahead operations, this "token" signifies we hit rule end ATN state
-     *  and did not follow it despite needing to.
-     */
-    public static final int EPSILON = -2;
+	/**
+	 * During lookahead operations, this "token" signifies we hit rule end ATN state
+	 * and did not follow it despite needing to.
+	 */
+	public static final int EPSILON = -2;
+
+	public static final int FRAGMENT = -3;
 
 	public static final int MIN_USER_TOKEN_TYPE = 1;
 
-    public static final int EOF = IntStream.EOF;
+	public static final int EOF = IntStream.EOF;
 
-	/** All tokens go to the parser (unless skip() is called in that rule)
-	 *  on a particular "channel".  The parser tunes to a particular channel
-	 *  so that whitespace etc... can go to the parser on a "hidden" channel.
+	/**
+	 * All tokens go to the parser (unless skip() is called in that rule)
+	 * on a particular "channel".  The parser tunes to a particular channel
+	 * so that whitespace etc... can go to the parser on a "hidden" channel.
 	 */
 	public static final int DEFAULT_CHANNEL = 0;
 
-	/** Anything on different channel than DEFAULT_CHANNEL is not parsed
-	 *  by parser.
+	/**
+	 * Anything on different channel than DEFAULT_CHANNEL is not parsed
+	 * by parser.
 	 */
 	public static final int HIDDEN_CHANNEL = 1;
 
@@ -51,45 +56,54 @@ public interface Token {
 	 */
 	String getText();
 
-	/** Get the token type of the token */
+	/**
+	 * Get the token type of the token
+	 */
 	int getType();
 
-	/** The line number on which the 1st character of this token was matched,
-	 *  line=1..n
+	/**
+	 * The line number on which the 1st character of this token was matched,
+	 * line=1..n
 	 */
 	int getLine();
 
-	/** The index of the first character of this token relative to the
-	 *  beginning of the line at which it occurs, 0..n-1
+	/**
+	 * The index of the first character of this token relative to the
+	 * beginning of the line at which it occurs, 0..n-1
 	 */
 	int getCharPositionInLine();
 
-	/** Return the channel this token. Each token can arrive at the parser
-	 *  on a different channel, but the parser only "tunes" to a single channel.
-	 *  The parser ignores everything not on DEFAULT_CHANNEL.
+	/**
+	 * Return the channel this token. Each token can arrive at the parser
+	 * on a different channel, but the parser only "tunes" to a single channel.
+	 * The parser ignores everything not on DEFAULT_CHANNEL.
 	 */
 	int getChannel();
 
-	/** An index from 0..n-1 of the token object in the input stream.
-	 *  This must be valid in order to print token streams and
-	 *  use TokenRewriteStream.
-	 *
-	 *  Return -1 to indicate that this token was conjured up since
-	 *  it doesn't have a valid index.
+	/**
+	 * An index from 0..n-1 of the token object in the input stream.
+	 * This must be valid in order to print token streams and
+	 * use TokenRewriteStream.
+	 * <p>
+	 * Return -1 to indicate that this token was conjured up since
+	 * it doesn't have a valid index.
 	 */
 	int getTokenIndex();
 
-	/** The starting character index of the token
-	 *  This method is optional; return -1 if not implemented.
+	/**
+	 * The starting character index of the token
+	 * This method is optional; return -1 if not implemented.
 	 */
 	int getStartIndex();
 
-	/** The last character index of the token.
-	 *  This method is optional; return -1 if not implemented.
+	/**
+	 * The last character index of the token.
+	 * This method is optional; return -1 if not implemented.
 	 */
 	int getStopIndex();
 
-	/** Gets the {@link TokenSource} which created this token.
+	/**
+	 * Gets the {@link TokenSource} which created this token.
 	 */
 	TokenSource getTokenSource();
 
