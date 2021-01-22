c11a1dff9e3002d294dbf57d9462f5a030a58595
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-22 10:14:53 +0800

    let's see if maven is the culprit

--- .github/scripts/run-tests-swift.sh
+++ .github/scripts/run-tests-swift.sh
@@ -36,13 +36,13 @@ if [ $rc == 0 ]; then
   # run java tests
   cd runtime-testsuite/
   if [ $GROUP == "LEXER" ]; then
-      arch -x86_64 mvn -e -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest="swift.*" test
+      mvn -e -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest="swift.TestSemPredEvalLexer" test
   elif [ $GROUP == "PARSER" ]; then
-      arch -x86_64 mvn -e -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTests" -Dtest="swift.*" test
+      mvn -e -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTests" -Dtest="swift.*" test
   elif [ $GROUP == "RECURSION" ]; then
-      arch -x86_64 mvn -e -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest="swift.*" test
+      mvn -e -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest="swift.*" test
   else
-      arch -x86_64 mvn -e -q -Dtest=swift.* test
+      mvn -e -q -Dtest=swift.* test
   fi
   rc=$?
   cat target/surefire-reports/*.dumpstream || true
