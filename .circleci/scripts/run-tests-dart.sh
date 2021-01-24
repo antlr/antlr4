be3c439708c9869ac22e90ce01a60b82cbad681e
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 02:14:36 +0800

    update config

--- .circleci/scripts/run-tests-dart.sh
+++ .circleci/scripts/run-tests-dart.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
+dart --version
+
 pushd runtime-testsuite
   echo "running maven tests..."
-  mvn -q -Dparallel=classes -DthreadCount=4 -Dtest=dart.* test
+#  mvn -q -Dparallel=classes -DthreadCount=4 -Dtest=dart.* test
+  mvn -q -Dtest=dart.* test
 popd
