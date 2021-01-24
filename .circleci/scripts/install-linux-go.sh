dacdf5151e908416cf488b4e746ca35407c6b36d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 02:59:31 +0800

    update config

--- .circleci/scripts/install-linux-go.sh
+++ .circleci/scripts/install-linux-go.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
+echo "installing gimme..."
+sudo apt-get update
+sudo apt-get install gimme
+echo "installing go..."
 eval "$(sudo gimme 1.7.3)"
 ( go version ; go env ) || true
+echo "done installing go"
