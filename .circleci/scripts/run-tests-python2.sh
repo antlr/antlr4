6f83e43543a82a0340e31f42d2c2b9ed55c89b66
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 00:49:51 +0800

    update config

--- .circleci/scripts/run-tests-python2.sh
+++ .circleci/scripts/run-tests-python2.sh
@@ -8,10 +8,10 @@ echo "done updating"
 echo "installing python..."
 sudo apt-get install python2
 echo "done installing python"
-python --version
+python2 --version
 
 mvn -q -Dparallel=methods -DthreadCount=4 -Dtest=python2.* test
 
 cd ../runtime/Python2/tests
 
-python run.py
+python2 run.py
