2a12101f55574240642e37e689264fb6396af3cc
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 14:22:02 +0800

    update config

--- .circleci/scripts/install-linux-javascript.sh
+++ .circleci/scripts/install-linux-javascript.sh
@@ -3,5 +3,16 @@
 set -euo pipefail
 
 # use v14 and check
+echo "installing nodejs..."
+sudo apt update
+sudo apt install nodejs 14
+sudo apt install npm
 echo node version: $(node --version)
+echo "done installing nodejs..."
 
+echo "installing javascript runtime..."
+pushd runtime/JavaScript
+  npm install
+  npm link
+popd
+echo "done installing javascript runtime"
--- .circleci/scripts/run-tests-javascript.sh
+++ .circleci/scripts/run-tests-javascript.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-cd ../runtime/JavaScript
-npm install
-npm link
-cd ../../runtime-testsuite
-mvn -q -Dparallel=methods -DthreadCount=1 -Dtest=javascript.* test
+pushd
+  cd runtime-testsuite
+  mvn -q -Dparallel=methods -DthreadCount=1 -Dtest=javascript.* test
+popd
