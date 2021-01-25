971fc6076eba6be1db138bf35ec273add25f808f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 15:40:03 +0800

    update config

--- .circleci/scripts/run-tests-dotnet.sh
+++ .circleci/scripts/run-tests-dotnet.sh
@@ -2,12 +2,15 @@
 
 set -euo pipefail
 
-if [ $GROUP == "LEXER" ]; then
-    mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dparallel=classes -DthreadCount=4 -Dtest=csharp.TestCompositeLexers test
-elif [ $GROUP == "PARSER" ]; then
-    mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTests" -Dparallel=classes -DthreadCount=4 -Dtest=csharp.* test
-elif [ $GROUP == "RECURSION" ]; then
-    mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dparallel=classes -DthreadCount=4 -Dtest=csharp.* test
-else
-    mvn -q -Dparallel=methods -DthreadCount=4 -Dtest=csharp.* test
-fi
+pushd runtime-testsuite/
+  echo "running maven tests..."
+  if [ $GROUP == "LEXER" ]; then
+      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dparallel=classes -DthreadCount=4 -Dtest=csharp.TestCompositeLexers test
+  elif [ $GROUP == "PARSER" ]; then
+      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTests" -Dparallel=classes -DthreadCount=4 -Dtest=csharp.* test
+  elif [ $GROUP == "RECURSION" ]; then
+      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dparallel=classes -DthreadCount=4 -Dtest=csharp.* test
+  else
+      mvn -q -Dparallel=methods -DthreadCount=4 -Dtest=csharp.* test
+  fi
+popd
