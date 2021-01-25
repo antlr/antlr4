4c8bd2783dc50f6309c8789c37eace1dfed3bf9d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 13:28:37 +0800

    update config

--- .circleci/scripts/install-linux-go.sh
+++ .circleci/scripts/install-linux-go.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 echo "installing go runtime..."
 echo "fetching gimme..."
-curl -sL -o /usr/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
-chmod +x /usr/bin/gimme
+mkdir ~/bin
+export PATH="$PATH:~/bin"
+curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x ~/bin/gimme
 echo "installing go using gimme..."
-eval "$(sudo /usr/bin/gimme 1.7.3)"
+eval "$(sudo gimme 1.7.3)"
 echo "done installing go using gimme..."
 go version
 go env
