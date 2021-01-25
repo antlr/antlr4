a4f099561e2d5096c9cab5e6b6c1ecfa700d0b1e
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 21:14:07 +0800

    update config

--- .circleci/scripts/install-linux-swift.sh
+++ .circleci/scripts/install-linux-swift.sh
@@ -3,40 +3,35 @@ set -euo pipefail
 echo "installing swift SDK..."
 # see https://tecadmin.net/install-swift-ubuntu-1604-xenial/
 sudo apt-get update -y
-# sudo apt-get install clang libicu-dev
-# sudo apt-get install libpython2.7 libpython2.7-dev
+sudo apt-get install clang libicu-dev
+sudo apt-get install libpython2.7 libpython2.7-dev
 
-#export SWIFT_VERSION=swift-5.3.2
-#echo "installing gpg key..."
-#wget -q -O - https://swift.org/keys/all-keys.asc | sudo gpg --import -
-#echo "downloading SDK gpg key..."
-#SWIFT_SDK=https://swift.org/builds/$SWIFT_VERSION-release/ubuntu1604/$SWIFT_VERSION-RELEASE/$SWIFT_VERSION-RELEASE-ubuntu16.04.tar.gz
-#echo $SWIFT_SDK
-#wget -q $SWIFT_SDK
-#sudo tar xzf $SWIFT_VERSION-RELEASE-ubuntu16.04.tar.gz
-#mv $SWIFT_VERSION-RELEASE-ubuntu16.04 $PWD/swift
+export SWIFT_VERSION=swift-5.3.2
+echo "installing gpg key..."
+wget -q -O - https://swift.org/keys/all-keys.asc | sudo gpg --import -
+echo "downloading SDK gpg key..."
+SWIFT_SDK=https://swift.org/builds/$SWIFT_VERSION-release/ubuntu1604/$SWIFT_VERSION-RELEASE/$SWIFT_VERSION-RELEASE-ubuntu16.04.tar.gz
+echo $SWIFT_SDK
+wget -q $SWIFT_SDK
+sudo tar xzf $SWIFT_VERSION-RELEASE-ubuntu16.04.tar.gz
+mv $SWIFT_VERSION-RELEASE-ubuntu16.04 $PWD/swift
 
-# export SWIFT_HOME=$PWD/swift/$SWIFT_VERSION-RELEASE-ubuntu16.04/usr/bin/
-# export PATH=$PWD/swift/usr/bin:$PATH
+export SWIFT_HOME=$PWD/swift/$SWIFT_VERSION-RELEASE-ubuntu16.04/usr/bin/
+export PATH=$PWD/swift/usr/bin:$PATH
 
 # This would fix a know linker issue mentioned in: # https://bugs.swift.org/browse/SR-2299
-# sudo ln -sf ld.gold /usr/bin/ld
+sudo ln -sf ld.gold /usr/bin/ld
 # This would fix missing libtinfo.so.5
-# sudo apt install libncurses5
+sudo apt install libncurses5
 # This would fix missing CURL_OPENSSL_3
 mkdir ~/libcurl3
 cd ~/libcurl3
 wget http://archive.ubuntu.com/ubuntu/pool/main/c/curl/libcurl3_7.47.0-1ubuntu2_amd64.deb
 ar x libcurl3* data.tar.xz
 tar xf data.tar.xz
-ls -all ~/libcurl3/usr/lib/x86_64-linux-gnu/
 sudo cp -L ~/libcurl3/usr/lib/x86_64-linux-gnu/libcurl.so.4 /usr/lib/libcurl.so.3
-# sudo ln -sf /usr/lib/ /usr/bin/libcurl.so.4.4.0
 cd ..
 sudo rm -rf ~/libcurl3
-
-echo "ls /usr/lib/"
-ls -all /usr/lib/ | grep libcurl
 
 echo "done installing swift SDK..."
 
