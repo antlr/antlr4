261b9ecebc8744342b251aea3c1403e0e9a50150
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 21:04:04 +0800

    update config

--- .circleci/scripts/install-linux-swift.sh
+++ .circleci/scripts/install-linux-swift.sh
@@ -30,7 +30,8 @@ wget http://archive.ubuntu.com/ubuntu/pool/main/c/curl/libcurl3_7.47.0-1ubuntu2_
 ar x libcurl3* data.tar.xz
 tar xf data.tar.xz
 ls -all ~/libcurl3/usr/lib/x86_64-linux-gnu/
-sudo cp -L ~/libcurl3/usr/lib/x86_64-linux-gnu/libcurl.so.4 /usr/lib/libcurl.so.3
+sudo cp -L ~/libcurl3/usr/lib/x86_64-linux-gnu/libcurl.so.4.4.0 /usr/lib/
+sudo ln -sf /usr/lib/ /usr/bin/libcurl.so.4.4.0
 cd ..
 sudo rm -rf ~/libcurl3
 
