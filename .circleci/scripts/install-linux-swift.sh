31bf193f6ac42269c198c390fc5b24b1b940e794
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 20:56:34 +0800

    update config

--- .circleci/scripts/install-linux-swift.sh
+++ .circleci/scripts/install-linux-swift.sh
@@ -25,13 +25,14 @@ sudo apt-get update -y
 # sudo apt install libncurses5
 # This would fix missing CURL_OPENSSL_3
 mkdir ~/libcurl3
-pushd ~/libcurl3
-  wget http://archive.ubuntu.com/ubuntu/pool/main/c/curl/libcurl3_7.47.0-1ubuntu2_amd64.deb
-  ar x libcurl3* data.tar.xz
-  sudo tar xf data.tar.xz
-  ls -all
-  sudo cp -L ~/libcurl3/usr/lib/x86_64-linux-gnu/libcurl.so.4 /usr/lib/libcurl.so.3
-popd
+cd ~/libcurl3
+wget http://archive.ubuntu.com/ubuntu/pool/main/c/curl/libcurl3_7.47.0-1ubuntu2_amd64.deb
+ar x libcurl3* data.tar.xz
+ls -all
+tar xf data.tar.xz
+ls -all
+sudo cp -L ~/libcurl3/usr/lib/x86_64-linux-gnu/libcurl.so.4 /usr/lib/libcurl.so.3
+cd ..
 sudo rm -rf ~/libcurl3
 
 ls -all /usr/lib/x86_64-linux-gnu/ | grep libcurl
