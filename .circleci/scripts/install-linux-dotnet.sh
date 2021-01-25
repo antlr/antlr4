abe1bc9a0189f426e47593d08ea827e17ed717ae
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-25 15:33:50 +0800

    update config

--- .circleci/scripts/install-linux-dotnet.sh
+++ .circleci/scripts/install-linux-dotnet.sh
@@ -2,9 +2,18 @@
 
 set -euo pipefail
 
+"installing .Net SDK..."
 wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
   sudo apt-get install -y dotnet-sdk-3.1
+export PATH=$PATH:~/.dotnet
+"done installing .Net SDK"
+
+# we need to build the runtime before test run, since we used "--no-dependencies"
+# when we call dotnet cli for restore and build, in order to speed up
+"building runtime..."
+dotnet build -c Release -f netstandard2.0 runtime/CSharp/Antlr4.csproj
+"done building runtime"
--- .circleci/scripts/run-tests-dotnet.sh
+++ .circleci/scripts/run-tests-dotnet.sh
@@ -2,15 +2,6 @@
 
 set -euo pipefail
 
-export PATH=$PATH:/Users/travis/.dotnet
-
-# we need to build the runtime before test run, since we used "--no-dependencies"
-# when we call dotnet cli for restore and build, in order to speed up
-
-dotnet build -c Release -f netstandard2.0 ../runtime/CSharp/Antlr4.csproj
-
-# call test
-
 if [ $GROUP == "LEXER" ]; then
     mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dparallel=classes -DthreadCount=4 -Dtest=csharp.TestCompositeLexers test
 elif [ $GROUP == "PARSER" ]; then
