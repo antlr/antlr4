fdc75404d4629602c9df1651eea5fb88bb472b66
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-26 10:35:01 +0800

    update config

--- .circleci/scripts/run-tests-cpp.sh
+++ .circleci/scripts/run-tests-cpp.sh
@@ -2,13 +2,16 @@
 
 set -euo pipefail
 
-if [ $GROUP == "LEXER" ]; then
-    mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=cpp.* test 
-elif [ $GROUP == "PARSER" ]; then
-    mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTests" -Dtest=cpp.* test 
-elif [ $GROUP == "RECURSION" ]; then
-    mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=cpp.* test 
-else
-    mvn -q -Dtest=cpp.* test 
-fi
+pushd runtime-testsuite
+  echo "running maven tests..."
+  if [ $GROUP == "LEXER" ]; then
+      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=cpp.TestCompositeLexer test
+  elif [ $GROUP == "PARSER" ]; then
+      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTests" -Dtest=cpp.* test
+  elif [ $GROUP == "RECURSION" ]; then
+      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=cpp.* test
+  else
+      mvn -q -Dtest=cpp.* test
+  fi
+popd
 
