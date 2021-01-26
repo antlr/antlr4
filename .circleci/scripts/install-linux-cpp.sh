77876cbd1162b13a8ca6c572a175b6c00c51fa0c
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-26 11:14:24 +0800

    update config

--- .circleci/scripts/install-linux-cpp.sh
+++ .circleci/scripts/install-linux-cpp.sh
@@ -7,6 +7,24 @@ echo "installing cpp SDK..."
 sudo apt-get update -y
 sudo apt-get install clang
 
-echo "done installing cpp SDK..."
+echo "done installing cpp SDK"
 
 clang++ --version
+
+echo "building cpp runtime..."
+
+pushd "runtime/Cpp/"
+  rc=0
+  if [ $rc == 0 ]; then
+    cmake . -DCMAKE_BUILD_TYPE=release
+    rc=$?
+  fi
+  if [ $rc == 0 ]; then
+    make -j 8
+    rc=$?
+  fi
+popd
+
+
+echo "done building cpp runtime"
+
