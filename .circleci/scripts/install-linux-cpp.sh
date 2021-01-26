a24317b4fa790146ce25aac949700ff7bb9b2039
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-26 11:34:58 +0800

    update config

--- .circleci/scripts/install-linux-cpp.sh
+++ .circleci/scripts/install-linux-cpp.sh
@@ -7,10 +7,12 @@ echo "installing cpp SDK..."
 sudo apt-get update -y
 sudo apt-get install -y clang
 sudo apt-get install -y cmake
+sudo apt-get install -y pkg-config
 
 echo "done installing cpp SDK"
 
 clang++ --version
+cmake --version
 
 echo "building cpp runtime..."
 
