4716a897e39c2138076ef342c831b6b8afa91398
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2021-01-31 21:38:45 -0800

    Enable development and production bundling with Webpack

--- runtime/JavaScript/webpack.config.js
+++ runtime/JavaScript/webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
 
-module.exports = {
-    mode: "production",
-    entry: './src/antlr4/index.js',
-    output: {
-        filename: 'antlr4.js',
-        path: path.resolve(__dirname, 'dist'),
-        // the name of the exported antlr4
-        library: "antlr4",
-        libraryTarget: 'window'
-    },
-    node: {
-        module: "empty",
-        net: "empty",
-        fs: "empty"
-    },
-    target: "web",
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-            }
-        }]
-    }
-}
+module.exports = (env, argv) => {
+    return {
+        entry: './src/antlr4/index.js',
+        output: {
+            filename:
+                argv.mode === 'production' ? 'antlr4.min.js' : 'antlr4.js',
+            path: path.resolve(__dirname, 'dist'),
+            // the name of the exported antlr4
+            library: 'antlr4',
+            libraryTarget: 'umd',
+        },
+        node: {
+            module: 'empty',
+            net: 'empty',
+            fs: 'empty',
+        },
+        target: 'web',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    },
+                },
+            ],
+        },
+    };
+};
