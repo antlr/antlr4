07a55988621cd633949ef6605201a2034d4d8729
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-02-14 14:54:59 +0800

    fix CI config

--- .circleci/scripts/run-tests-javascript.sh
+++ .circleci/scripts/run-tests-javascript.sh
@@ -4,11 +4,15 @@ set -euo pipefail
 
 declare -i RESULT=0
 
-pushd runtime-testsuite
+pushd runtime/JavaScript
 
   echo "running jest tests..."
   yarn test
   RESULT+=$?
+
+popd
+
+pushd runtime-testsuite
 
   echo "running maven tests..."
   mvn -q -Dtest=javascript.* test
