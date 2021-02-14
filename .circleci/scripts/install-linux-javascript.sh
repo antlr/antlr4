f6f982c9da4c5f14870848828acefe32579cf264
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-02-14 14:11:03 +0800

    run jest tests in CI

--- .circleci/scripts/install-linux-javascript.sh
+++ .circleci/scripts/install-linux-javascript.sh
@@ -7,7 +7,11 @@ echo "installing nodejs..."
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 echo node version: $(node --version)
-echo "done installing nodejs..."
+echo "done installing nodejs"
+
+echo "installing yarn..."
+sudo npm install -g yarn@v1.22.10
+echo "done installing yarn"
 
 echo "packaging javascript runtime..."
 pushd runtime/JavaScript
--- .circleci/scripts/run-tests-javascript.sh
+++ .circleci/scripts/run-tests-javascript.sh
@@ -2,7 +2,18 @@
 
 set -euo pipefail
 
+declare -i RESULT=0
+
 pushd runtime-testsuite
+
+  echo "running jest tests..."
+  yarn test
+  RESULT+=$?
+
   echo "running maven tests..."
   mvn -q -Dtest=javascript.* test
+  RESULT+=$?
+
 popd
+
+exit $RESULT
