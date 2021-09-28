7bd1d1676186bbbae5cf3d503d3616bf8b29b005
Martin van Wingerden <martin@martinvw.nl>
2021-09-28 08:50:58 +0200

    Added on more part under the same mutex
    Signed-off-by: Martin van Wingerden <martin@martinvw.nl>

--- runtime/Swift/Sources/Antlr4/dfa/DFA.swift
+++ runtime/Swift/Sources/Antlr4/dfa/DFA.swift
@@ -26,11 +26,6 @@ public class DFA: CustomStringConvertible {
     /// 
     private let precedenceDfa: Bool
     
-    /// 
-    /// mutex for DFAState changes.
-    /// 
-    internal private(set) var dfaStateMutex = Mutex()
-    
     ///
     /// mutex for states changes.
     ///
@@ -118,7 +113,7 @@ public class DFA: CustomStringConvertible {
 
         // synchronization on s0 here is ok. when the DFA is turned into a
         // precedence DFA, s0 will be initialized once and not updated again
-        dfaStateMutex.synchronized {
+        s0.mutex.synchronized {
             // s0.edges is never null for a precedence DFA
             if precedence >= edges.count {
                 let increase = [DFAState?](repeating: nil, count: (precedence + 1 - edges.count))
