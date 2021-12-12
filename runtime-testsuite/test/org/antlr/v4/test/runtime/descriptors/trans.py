04d9c70092433f904b37fd45c1ba2f992c6af504
parrt <parrt@cs.usfca.edu>
2021-12-12 12:17:33 -0800

    translator seems to work

--- runtime-testsuite/test/org/antlr/v4/test/runtime/descriptors/trans.py
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/descriptors/trans.py
@@ -5,35 +5,48 @@ import re
 import os
 import glob
 
+output_dir = "/tmp/output/"
+output_dir = "."
+
 pattern = r"/\*\*(.+?)\*/\s+@CommentHasStringValue\s+(public String \w+);"
 
 def update(filename, java):
-    matches = re.finditer(pattern, java, re.DOTALL)
-
-    for matchNum, match in enumerate(matches, start=1):
-        span = match.span()
-    #     print("span", span)
-    #     print("COMMENT")#,match.group(1))
+    # skip copyright comment
+    package_idx = java.index("package org.antlr.v4.test.runtime.descriptors")
+    java_ = java[package_idx:]
+    # Match one at a time so we can replace and not track altered indexes
+    match = re.search(pattern, java_, re.DOTALL)
+    while match is not None:
+        # for matchNum, match in enumerate(matches, start=1):
+        a,b = match.span()
+        # print("span", a, b, java_[a:b])
+        #     print("COMMENT")#,match.group(1))
         span = match.span(1)
-        string = java[span[0]:span[1]].lstrip('\n')
+        string = java_[span[0]:span[1]].lstrip('\n')
         string = string.rstrip()
-    #     print("span", span, ":", f"!{string}!")
-    #     print("VAR")#,match.group(2))
+        #     print("span", span, ":", f"!{string}!")
+        #     print("VAR")#,match.group(2))
         span = match.span(2)
-        field = java[span[0]:span[1]]
-    #     print("span", span, ":", field)
+        field = java_[span[0]:span[1]]
+        #     print("span", span, ":", field)
 
-        print(f'''
-{field} = """
+        repl = f'''{field} = """
 {string}
-""";
-        ''')
+""";'''
+        java_ = java_[0:a] + repl + java_[b:]
+        match = re.search(pattern, java_, re.DOTALL)
+    # print(java[0:package_idx] + java_)
+    # print("-----------------------")
+    return java[0:package_idx] + java_
 
 
 for filename in glob.glob("*.java"):
-    print(filename+"--------------------------")
-    with open(filename, "r") as f:
+# for filename in glob.glob("SetsDescriptors.java"):
+    print(filename)
+    with open(filename, "r", encoding='UTF-8') as f:
         java = f.read()
-    java = java[java.index("package org.antlr.v4.test.runtime.descriptors"):] # skip copyright comment
-    update(filename, java)
-
+    java_ = update(filename, java)
+    # os.unlink("/tmp/output")
+    # os.mkdir("/tmp/output")
+    with open(output_dir+filename, "w", encoding='UTF-8') as f:
+        f.write(java_)
