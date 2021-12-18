16663be46fb59f1747272be4edd804a8ac5ddc98
Terence Parr <parrt@antlr.org>
2021-12-17 18:27:16 -0800

    typo in circleci spec

--- .circleci/scripts/run-tests-cpp.sh
+++ .circleci/scripts/run-tests-cpp.sh
@@ -11,9 +11,9 @@ pushd runtime-testsuite
   if [ $GROUP == "LEXER" ]; then
       mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=cpp.** test
   elif [ $GROUP == "PARSER1" ]; then
-      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup1" -Dtest=cpp.** test
+      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsBatch1" -Dtest=cpp.** test
   elif [ $GROUP == "PARSER2" ]; then
-      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup2" -Dtest=cpp.** test
+      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsBatch2" -Dtest=cpp.** test
   elif [ $GROUP == "RECURSION" ]; then
       mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=cpp.** test
   else
