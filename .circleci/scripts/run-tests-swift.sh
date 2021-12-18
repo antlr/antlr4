a00d9f5d0338cd7bfd9b00b5e3ec6e562c041ac7
Terence Parr <parrt@antlr.org>
2021-12-17 18:01:00 -0800

    get group categories working

--- .circleci/scripts/run-tests-swift.sh
+++ .circleci/scripts/run-tests-swift.sh
@@ -18,6 +18,8 @@ if [ $rc == 0 ]; then
         mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=swift.** test
     elif [ $GROUP == "PARSER1" ]; then
         mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsBatch1" -Dtest=swift.** test
+    elif [ $GROUP == "PARSER2" ]; then
+        mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsBatch2" -Dtest=swift.** test
     elif [ $GROUP == "RECURSION" ]; then
         mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=swift.** test
     else
