6fafab250dcd9d63c9cc76df290b88a39f6ccda9
Terence Parr <parrt@antlr.org>
2021-12-18 17:07:43 -0800

    split some of C# parsing tests out into two groups like cpp and swift

--- runtime-testsuite/test/org/antlr/v4/test/runtime/csharp/TestPerformance.java
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/csharp/TestPerformance.java
@@ -9,11 +9,12 @@ package org.antlr.v4.test.runtime.csharp;
 import org.antlr.v4.test.runtime.BaseRuntimeTest;
 import org.antlr.v4.test.runtime.RuntimeTestDescriptor;
 import org.antlr.v4.test.runtime.category.ParserTestsGroup1;
+import org.antlr.v4.test.runtime.category.ParserTestsGroup2;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-@Category(ParserTestsGroup1.class)
+@Category(ParserTestsGroup2.class)
 @RunWith(Parameterized.class)
 public class TestPerformance extends BaseRuntimeTest {
 	public TestPerformance(RuntimeTestDescriptor descriptor) {
