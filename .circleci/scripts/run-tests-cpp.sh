5d5ee8423d760470a0698ff226fd73a8a5e0889f
parrt <parrt@cs.usfca.edu>
2021-12-27 12:44:02 -0800

    [again] don't use "quiet" option for mvn tests...hard to figure out what's wrong when failed.

--- .circleci/scripts/run-tests-cpp.sh
+++ .circleci/scripts/run-tests-cpp.sh
@@ -9,15 +9,15 @@ popd
 pushd runtime-testsuite
   echo "running maven tests..."
   if [ $GROUP == "LEXER" ]; then
-      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=cpp.** test
+      mvn -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=cpp.** test
   elif [ $GROUP == "PARSER1" ]; then
-      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup1" -Dtest=cpp.** test
+      mvn -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup1" -Dtest=cpp.** test
   elif [ $GROUP == "PARSER2" ]; then
-      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup2" -Dtest=cpp.** test
+      mvn -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup2" -Dtest=cpp.** test
   elif [ $GROUP == "RECURSION" ]; then
-      mvn -q -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=cpp.** test
+      mvn -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=cpp.** test
   else
-      mvn -q -Dtest=cpp.** test
+      mvn -Dtest=cpp.** test
   fi
 popd
 
--- .circleci/scripts/run-tests-dart.sh
+++ .circleci/scripts/run-tests-dart.sh
@@ -7,5 +7,5 @@ dart --version
 pushd runtime-testsuite
   echo "running maven tests..."
 #  mvn -q -Dparallel=classes -DthreadCount=4 -Dtest=dart.** test
-  mvn -q -Dtest=dart.** test
+  mvn -Dtest=dart.** test
 popd
