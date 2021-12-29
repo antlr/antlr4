612ef4fee653266b5023c9355aa6e9782d445951
parrt <parrt@cs.usfca.edu>
2021-12-29 12:32:50 -0800

    ok, back off threading for c++ for moment.

--- .circleci/scripts/run-tests-cpp.sh
+++ .circleci/scripts/run-tests-cpp.sh
@@ -9,15 +9,15 @@ popd
 pushd runtime-testsuite
   echo "running maven tests..."
   if [ $GROUP == "LEXER" ]; then
-      mvn -Dparallel=classes -DthreadCount=4 -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=cpp.** test
+      mvn -Dparallel=classes -DthreadCount=1 -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=cpp.** test
   elif [ $GROUP == "PARSER1" ]; then
-      mvn -Dparallel=classes -DthreadCount=4 -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup1" -Dtest=cpp.** test
+      mvn -Dparallel=classes -DthreadCount=1 -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup1" -Dtest=cpp.** test
   elif [ $GROUP == "PARSER2" ]; then
-      mvn -Dparallel=classes -DthreadCount=4 -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup2" -Dtest=cpp.** test
+      mvn -Dparallel=classes -DthreadCount=1 -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup2" -Dtest=cpp.** test
   elif [ $GROUP == "RECURSION" ]; then
-      mvn -Dparallel=classes -DthreadCount=4 -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=cpp.** test
+      mvn -Dparallel=classes -DthreadCount=1 -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=cpp.** test
   else
-      mvn -Dparallel=classes -DthreadCount=4 -Dtest=cpp.** test
+      mvn -Dparallel=classes -DthreadCount=1 -Dtest=cpp.** test
   fi
 popd
 
