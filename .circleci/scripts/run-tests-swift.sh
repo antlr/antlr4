b346aa560b74ab85430d2c6984afd7e3f983d9c2
parrt <parrt@cs.usfca.edu>
2021-12-30 13:00:28 -0800

    simplify Swift like the other tests

--- .circleci/scripts/run-tests-swift.sh
+++ .circleci/scripts/run-tests-swift.sh
@@ -13,19 +13,10 @@ pushd runtime/Swift
   fi
 popd
 
+
 if [ $rc == 0 ]; then
   pushd runtime-testsuite
     echo "running maven tests..."
-    if [ $GROUP == "LEXER" ]; then
-        mvn -Dparallel=classes -DthreadCount=4 -Dgroups="org.antlr.v4.test.runtime.category.LexerTests" -Dtest=swift.** test
-    elif [ $GROUP == "PARSER1" ]; then
-        mvn -Dparallel=classes -DthreadCount=4 -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup1" -Dtest=swift.** test
-    elif [ $GROUP == "PARSER2" ]; then
-        mvn -Dparallel=classes -DthreadCount=4 -Dgroups="org.antlr.v4.test.runtime.category.ParserTestsGroup2" -Dtest=swift.** test
-    elif [ $GROUP == "RECURSION" ]; then
-        mvn -Dparallel=classes -DthreadCount=4 -Dgroups="org.antlr.v4.test.runtime.category.LeftRecursionTests" -Dtest=swift.** test
-    else
-        mvn -Dparallel=classes -DthreadCount=4 -Dtest=swift.** test
-    fi
+    mvn -Dparallel=classes -DthreadCount=4 -Dtest=swift.** test
   popd
 fi
