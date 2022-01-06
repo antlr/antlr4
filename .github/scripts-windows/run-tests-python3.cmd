e131509dd7dd81012e793889048daa5858f76ca2
ericvergnaud <eric.vergnaud@wanadoo.fr>
2022-01-06 10:11:57 +0100

    Update run-tests-python3.cmd

--- .github/scripts-windows/run-tests-python3.cmd
+++ .github/scripts-windows/run-tests-python3.cmd
@@ -3,5 +3,5 @@ where python
 where python3
 
 cd runtime-testsuite
-mvn -Dantlr-python3-python="C:\Python38\python.exe" -Dparallel=classes -DthreadCount=4 -Dtest=python3.** test
+mvn -Dantlr-python3-python="C:\Python38\python.exe" -Dparallel=classes -DthreadCount=4 -Dtest=python3.** test -Dantlr-python3-python="C:\Program Files\Python310\python.exe"
 cd ..
