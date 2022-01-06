668733632294c94f96ca284b79c0e7e0d0846b62
ericvergnaud <eric.vergnaud@wanadoo.fr>
2022-01-06 10:27:26 +0100

    Update run-tests-python2.cmd

--- .github/scripts-windows/run-tests-python2.cmd
+++ .github/scripts-windows/run-tests-python2.cmd
@@ -1,3 +1,3 @@
 cd runtime-testsuite
-mvn -Dantlr-python2-python="C:\Program Files\Python29\python.exe" -Dparallel=classes -DthreadCount=4 -Dtest=python2.** test 
+mvn -Dantlr-python2-python="C:\Program Files\Python27\python.exe" -Dparallel=classes -DthreadCount=4 -Dtest=python2.** test 
 cd ..
