a431bc4a2a4cecc227589c44eb01238f4c7c2eb6
ericvergnaud <eric.vergnaud@wanadoo.fr>
2022-01-06 14:12:29 +0100

    Update run-tests-php.cmd

--- .github/scripts-windows/run-tests-php.cmd
+++ .github/scripts-windows/run-tests-php.cmd
@@ -1,4 +1,4 @@
-C:\ProgramData\chocolatey\bin\choco.exe install php --package-parameters='"/DontAddToPath""/ThreadSafe""/InstallDir:C:\Program Files\php\"'
+REM C:\ProgramData\chocolatey\bin\choco.exe install php --package-parameters='"/DontAddToPath""/ThreadSafe""/InstallDir:C:\Program Files\php\"'
 
 git clone https://github.com/antlr/antlr-php-runtime.git
 move antlr-php-runtime runtime\PHP
