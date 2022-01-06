e9c6200eda6969b0c8b743fb658127fe855a512b
parrt <parrt@cs.usfca.edu>
2022-01-05 16:36:20 -0800

    try new run for python3 windows (again2)

--- .github/scripts-windows/run-tests-python3.cmd
+++ .github/scripts-windows/run-tests-python3.cmd
@@ -1,3 +1,3 @@
 cd runtime-testsuite
-mvn -Dparallel=classes -DthreadCount=4 -Dtest=python3.** test -Dantlr-python3-python="C:\Python35\python.exe"
+mvn -Dantlr-python3-python="C:\Python38\python.exe" -Dparallel=classes -DthreadCount=4 -Dtest=python3.** test
 cd ..
