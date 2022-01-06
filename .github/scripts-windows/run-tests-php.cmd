f2a82bc7d5e8028064d1d7a5268f126e97e0313e
ericvergnaud <eric.vergnaud@wanadoo.fr>
2022-01-06 13:58:13 +0100

    Create run-tests-php.cmd

---
+++ .github/scripts-windows/run-tests-php.cmd
@@ -0,0 +1,8 @@
+C:\ProgramData\chocolatey\bin\choco.exe install php --package-parameters='"/DontAddToPath""/ThreadSafe""/InstallDir:C:\Program Files\php\"'
+
+git clone https://github.com/antlr/antlr-php-runtime.git
+move antlr-php-runtime runtime\PHP
+
+cd runtime-testsuite
+mvn -Dparallel=classes -DthreadCount=4 -Dtest=php.** test -Dantlr-php-php="C:\tools\php\php.exe"
+cd ..
