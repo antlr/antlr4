8600234e380f949fda1bf62255a39a1777b10177
ericvergnaud <eric.vergnaud@wanadoo.fr>
2022-01-09 01:03:41 +0100

    Update run-tests-csharp.cmd

--- .github/scripts-windows/run-tests-csharp.cmd
+++ .github/scripts-windows/run-tests-csharp.cmd
@@ -1,5 +1,5 @@
 dotnet build runtime/CSharp/src/Antlr4.csproj -c Release
 dotnet pack runtime/CSharp/src/Antlr4.csproj -c Release
 cd runtime-testsuite
-mvn -Dparallel=classes -DthreadCount=4 -Dtest=csharp.** test
+mvn -Dtest=csharp.** test
 cd ..
