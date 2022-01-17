40ebc46f75e50ef0a8a8ec38f2cd1247d52c3a32
Ivan Kochurkin <kvanttt@gmail.com>
2022-01-17 23:29:20 +0300

    Add generated LargeLexer test, remove LargeLexer.txt descriptor

--- runtime-testsuite/test/org/antlr/v4/test/runtime/ExtraTests.java
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/ExtraTests.java
@@ -38,4 +38,31 @@ public class ExtraTests {
 				"[@5,7:6='<EOF>',<-1>,3:1]\n";
 		return result;
 	}
+
+	static RuntimeTestDescriptor getLargeLexerDescriptor(String targetName) {
+		UniversalRuntimeTestDescriptor result = new UniversalRuntimeTestDescriptor();
+		result.name = "LargeLexer";
+		result.notes = "This is a regression test for antlr/antlr4#76 \"Serialized ATN strings\n" +
+				"should be split when longer than 2^16 bytes (class file limitation)\"\n" +
+				"https://github.com/antlr/antlr4/issues/76";
+		result.targetName = targetName;
+		result.testType = "Lexer";
+
+		final int tokensCount = 4000;
+
+		String grammarName = "L";
+		StringBuilder grammar = new StringBuilder();
+		grammar.append("lexer grammar ").append(grammarName).append(";\n");
+		grammar.append("WS: [ \\t\\r\\n]+ -> skip;\n");
+		for (int i = 0; i < tokensCount; i++) {
+			grammar.append("KW").append(i).append(" : 'KW' '").append(i).append("';\n");
+		}
+
+		result.grammar = grammar.toString();
+		result.grammarName = grammarName;
+		result.input = "KW400";
+		result.output = "[@0,0:4='KW400',<402>,1:0]\n" +
+				"[@1,5:4='<EOF>',<-1>,1:5]\n";
+		return result;
+	}
 }
