9e04c9faa74757e3cc42802fd7c4e95c82b20f78
parrt <parrt@cs.usfca.edu>
2022-01-29 15:51:43 -0800

    move writeSerializedATNIntegerHistogram out of runtime.

--- tool/src/org/antlr/v4/misc/Utils.java
+++ tool/src/org/antlr/v4/misc/Utils.java
@@ -6,11 +6,14 @@
 
 package org.antlr.v4.misc;
 
+import org.antlr.v4.runtime.misc.IntegerList;
 import org.antlr.v4.tool.ast.GrammarAST;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 /** */
 public class Utils {
@@ -75,6 +78,31 @@ public class Utils {
 //		}
 //		return x;
 //	}
+
+	public static void writeSerializedATNIntegerHistogram(String filename, IntegerList serializedATN) {
+		HashMap<Integer, Integer> histo = new HashMap<>();
+		for (int i : serializedATN.toArray()) {
+			if ( histo.containsKey(i) ) {
+				histo.put(i, histo.get(i) + 1);
+			}
+			else {
+				histo.put(i, 1);
+			}
+		}
+		TreeMap<Integer,Integer> sorted = new TreeMap<>(histo);
+
+		String output = "";
+		output += "value,count\n";
+		for (int key : sorted.keySet()) {
+			output += key+","+sorted.get(key)+"\n";
+		}
+		try {
+			Files.write(Paths.get(filename), output.getBytes(StandardCharsets.UTF_8));
+		}
+		catch (IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
 
 	public static String capitalize(String s) {
 		return Character.toUpperCase(s.charAt(0)) + s.substring(1);
