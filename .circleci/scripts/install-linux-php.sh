ce9e2325a95b8ea7b5a7ea09b4c54ffd1a3ed4b2
Terence Parr <parrt@antlr.org>
2022-04-01 15:11:44 -0700

    php8

--- .circleci/scripts/install-linux-php.sh
+++ .circleci/scripts/install-linux-php.sh
@@ -2,12 +2,11 @@
 
 set -euo pipefail
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo apt-get update -qq
 
 sudo apt update
 
-sudo apt list php
 sudo apt install php
 php -v
 
