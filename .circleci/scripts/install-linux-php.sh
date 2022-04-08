3a76d494c35c405f505db7999811664f34f09e3f
Terence Parr <parrt@antlr.org>
2022-04-08 10:19:24 -0700

    turn off apt update for php circleci

--- .circleci/scripts/install-linux-php.sh
+++ .circleci/scripts/install-linux-php.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo apt-get update -qq
 
-sudo apt update
+#sudo apt update
 
 # Installed now as part of docker image
 #sudo apt install php
