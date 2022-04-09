d0f7b9d7eee9cf01844d1e5971d337f5b14a06ac
Terence Parr <parrt@antlr.org>
2022-04-09 11:02:45 -0700

    php8

--- .circleci/scripts/install-linux-php.sh
+++ .circleci/scripts/install-linux-php.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+sudo apt install composer
+composer self-update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
@@ -11,7 +13,6 @@ sudo apt install php8.0-mbstring
 sudo apt install php8.0-xml
 php -v
 
-sudo apt install composer
 
 git clone https://github.com/antlr/antlr-php-runtime.git runtime/PHP
 composer install -d runtime/PHP
