d480b66e7cafe40a3fd74abdc26d337772d563e7
Ivan Kochurkin <kvanttt@gmail.com>
2022-05-27 14:32:40 +0300

    Drop compiling C# runtime using scripts since it's managed by tests infrastructure
    Signed-off-by: Ivan Kochurkin <kvanttt@gmail.com>

--- .circleci/scripts/install-linux-dotnet.sh
+++ .circleci/scripts/install-linux-dotnet.sh
@@ -12,8 +12,3 @@ sudo apt-get update; \
 export PATH=$PATH:~/.dotnet
 echo "done installing .Net SDK"
 
-# we need to build the runtime before test run, since we used "--no-dependencies"
-# when we call dotnet cli for restore and build, in order to speed up
-echo "building runtime..."
-dotnet build -c Release -f netstandard2.0 runtime/CSharp/src/Antlr4.csproj
-echo "done building runtime"
--- .github/scripts-macosx/run-tests-dotnet.sh
+++ .github/scripts-macosx/run-tests-dotnet.sh
@@ -6,11 +6,6 @@ set -euo pipefail
 
 export PATH=$PATH:~/.dotnet
 
-# we need to build the runtime before test run, since we used "--no-dependencies"
-# when we call dotnet cli for restore and build, in order to speed up
-
-dotnet build -c Release -f netstandard2.0 runtime/CSharp/Antlr4.csproj
-
 # run tests
 pushd runtime-testsuite/
 export MAVEN_OPTS="-Xmx8g"
