07bb7cc61caacb585ae1d8b83255c747211849d8
Terence Parr <parrt@antlr.org>
2022-05-29 12:10:14 -0700

    cleanup
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- tool/src/org/antlr/v4/gui/Intrepreter.java
+++ tool/src/org/antlr/v4/gui/Intrepreter.java
@@ -13,9 +13,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /** Interpret a lexer/parser, optionally printing tree string and dumping profile info
  *
