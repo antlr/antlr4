a81ec05babdcc625466492aa22ba4a06b8ed1448
Ivan Kochurkin <kvanttt@gmail.com>
2022-06-17 13:35:22 +0300

    Use crossplatform semaphore instead of unsafe raw pthread_mutex_t
    Also, it seems faster: https://stackoverflow.com/a/46174300/1046374
    
    Signed-off-by: Ivan Kochurkin <kvanttt@gmail.com>

--- runtime/Swift/Sources/Antlr4/misc/utils/Mutex.swift
+++ runtime/Swift/Sources/Antlr4/misc/utils/Mutex.swift
@@ -1,42 +1,29 @@
 import Foundation
 
 
-/// 
+///
 /// Using class so it can be shared even if
 /// it appears to be a field in a class.
-/// 
+///
 class Mutex {
-    
-    /// 
+    ///
     /// The mutex instance.
-    /// 
-    private var mutex = pthread_mutex_t()
-    
-    /// 
-    /// Initialization
-    /// 
-    init() {
-        pthread_mutex_init(&mutex, nil)
-    }
-    
-    /// 
+    ///
+    private let semaphore = DispatchSemaphore(value: 1)
+
+    ///
     /// Running the supplied closure synchronously.
-    /// 
+    ///
     /// - Parameter closure: the closure to run
     /// - Returns: the value returned by the closure
     /// - Throws: the exception populated by the closure run
-    /// 
+    ///
     @discardableResult
     func synchronized<R>(closure: () throws -> R) rethrows -> R {
-        pthread_mutex_lock(&mutex)
+        semaphore.wait()
         defer {
-            pthread_mutex_unlock(&mutex)
+            semaphore.signal()
         }
         return try closure()
-    }
-
-    deinit {
-        // free the mutex resource
-        pthread_mutex_destroy(&mutex)
     }
 }
