c98a57b083f975bcc7108b83640a6edc2bc8f1be
Ivan Kochurkin <kvanttt@gmail.com>
2022-07-01 23:48:51 +0300

    Update dotnet in tests (3.1 -> 6.0)
    Signed-off-by: Ivan Kochurkin <kvanttt@gmail.com>

--- .circleci/scripts/install-linux-csharp.sh
+++ .circleci/scripts/install-linux-csharp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "installing .Net SDK..."
-wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-3.1
-export PATH=$PATH:~/.dotnet
+  sudo apt-get install -y dotnet-sdk-6.0
 echo "done installing .Net SDK"
