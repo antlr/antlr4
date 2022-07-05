6a1d7f9ac91aea0d87e30997e616a81f5aef8ee5
Terence Parr <parrt@antlr.org>
2022-07-05 11:58:31 -0700

    add comment
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- runtime/Java/src/org/antlr/v4/runtime/tree/JsonSerializer.java
+++ runtime/Java/src/org/antlr/v4/runtime/tree/JsonSerializer.java
@@ -9,14 +9,71 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
+/** This "class" wraps support functions that generate JSON for parse trees.
+ *  The JSON includes everything needed to reconstruct a parse tree:
  *
- * @since 4.10.2
+ *  	Rule names (field: "rules")
+ *  	Input chars (field: "input")
+ *  	Tokens (field: "tokens")
+ *  	Parse tree (field: "tree"; refs rule indexes and token indexes)
+ *
+ *  For example, given input "99" and a simple expression grammar giving parse tree
+ *  "(s (expr 99) EOF)", the full JSON (formatted by jq) looks like:
+ *
+ * 	{
+ * 	  "rules": [
+ * 	    "s",
+ * 	    "expr"
+ * 	  ],
+ * 	  "input": "99",
+ * 	  "tokens": [
+ * 	    {
+ * 	      "type": 3,
+ * 	      "line": 1,
+ * 	      "pos": 0,
+ * 	      "channel": 0,
+ * 	      "start": 0,
+ * 	      "stop": 1
+ * 	    },
+ * 	    {
+ * 	      "type": -1,
+ * 	      "line": 1,
+ * 	      "pos": 2,
+ * 	      "channel": 0,
+ * 	      "start": 2,
+ * 	      "stop": 1
+ * 	    }
+ * 	  ],
+ * 	  "tree": {
+ * 	    "0": [
+ * 	      {
+ * 	        "1": [
+ * 	          0
+ * 	        ]
+ * 	      },
+ * 	      1
+ * 	    ]
+ * 	  }
+ * 	}
+ *
+ *  Notice that the tree is just a series of nested references to integers, which refer to rules
+ *  and tokens.
+ *
+ *  One potential use case: Create an ANTLR server that accepts a grammar and input as parameters then
+ *  returns JSON for the parse tree and the tokens.  This can be deserialized by JavaScript in a web browser
+ *  to display the parse result.
+ *
+ *  @since 4.10.2
  */
 public class JsonSerializer {
-	/** Print out a whole tree in JSON form. {@link #getNodeText} is used on the
-	 *  node payloads to get the text for the nodes.  Detect
-	 *  parse trees and extract data appropriately. Include rulenames, input, tokens.
+	/** Create a JSON representation of a parse tree and include all other information necessary to reconstruct
+	 *  a printable parse tree: the rules, input, tokens, and the tree structure that refers to the rule
+	 *  and token indexes.  Extract all information from the parser, which is assumed to be in a state
+	 *  post-parse and the object that created tree t.
+	 *
+	 * @param t The parse tree to serialize as JSON
+	 * @param recog The parser that created the parse tree and is in the post-recognition state
+	 * @return JSON representing the parse tree
 	 */
 	public static String toJSON(Tree t, Parser recog) {
 		String[] ruleNames = recog != null ? recog.getRuleNames() : null;
@@ -28,6 +85,10 @@ public class JsonSerializer {
 		return toJSON(t, Arrays.asList(ruleNames), tokenStream, inputStream);
 	}
 
+	/** Create a JSON representation of a parse tree and include all other information necessary to reconstruct
+	 *  a printable parse tree: the rules, input, tokens, and the tree structure that refers to the rule
+	 *  and token indexes.  The tree and rule names are required but the token stream and input stream are optional.
+	 */
 	public static String toJSON(Tree t,
 								final List<String> ruleNames,
 								final TokenStream tokenStream,
@@ -74,10 +135,8 @@ public class JsonSerializer {
 		return buf.toString();
 	}
 
-	/** Print out a whole tree in JSON form. {@link #getNodeText} is used on the
-	 *  node payloads to get the text for the nodes.  Detect
-	 *  parse trees and extract data appropriately.
-	 * @since 4.10.2
+	/** Create a JSON representation of a parse tree. The tree is just a series of nested references
+	 *  to integers, which refer to rules and tokens.
 	 */
 	public static String toJSONTree(final Tree t) {
 		StringBuilder buf = new StringBuilder();
@@ -96,7 +155,7 @@ public class JsonSerializer {
 		return buf.toString();
 	}
 
-	/** @since 4.10.2 */
+	/** Create appropriate JSON text for a tree node */
 	public static String getJSONNodeText(Tree t) {
 		if ( t instanceof RuleContext) {
 			int ruleIndex = ((RuleContext)t).getRuleContext().getRuleIndex();
