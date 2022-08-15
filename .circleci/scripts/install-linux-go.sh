d0223777888ee47ff086f80fae15d9fef09b8a32
Terence Parr <parrt@antlr.org>
2022-08-15 09:55:28 -0700

    grr...install snap
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- .circleci/scripts/install-linux-go.sh
+++ .circleci/scripts/install-linux-go.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 echo "installing go SDK..."
 sudo apt update
+sudo apt install snapd
 sudo snap install --classic --channel=1.19/stable go
 go version
 echo "done installing go SDK"
