46f4f0d869a443b2363027e8c5dd89ef5a4a36b6
Terence Parr <parrt@antlr.org>
2022-08-15 11:11:04 -0700

    try downloading golang with curl
    Signed-off-by: Terence Parr <parrt@antlr.org>
    Signed-off-by: Eric Vergnaud <eric.vergnaud@wanadoo.fr>

--- .circleci/scripts/install-linux-go.sh
+++ .circleci/scripts/install-linux-go.sh
@@ -3,7 +3,8 @@
 echo "installing go SDK..."
 sudo apt update
 sudo apt install snapd
-snap --version
-sudo snap install --classic --channel=1.19/stable go
+curl -OL https://go.dev/dl/go1.19.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.19.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
 go version
 echo "done installing go SDK"
