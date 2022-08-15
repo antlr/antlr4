5074363a0e8b053e4622448a63a1ce2d72b843aa
Terence Parr <parrt@antlr.org>
2022-08-15 11:23:24 -0700

    try downloading golang with curl
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- .circleci/scripts/install-linux-go.sh
+++ .circleci/scripts/install-linux-go.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 echo "installing go SDK..."
 sudo apt update
-sudo apt install snapd
 curl -OL https://go.dev/dl/go1.19.linux-amd64.tar.gz
 sudo tar -C /usr/local -xf go1.19.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
--- .circleci/scripts/run-tests-go.sh
+++ .circleci/scripts/run-tests-go.sh
@@ -7,6 +7,6 @@ go version
 
 pushd runtime-testsuite
     echo "running maven tests..."
-    export MAVEN_OPTS="-Xmx8g
+    export MAVEN_OPTS="-Xmx8g"
     mvn -Dparallel=classes -DthreadCount=4 -Dtest=go.** test
 popd
