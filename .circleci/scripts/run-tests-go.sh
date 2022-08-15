be4785982d8b1dec9f16d4bc03d1a1ecfbc1a503
Terence Parr <parrt@antlr.org>
2022-08-15 11:19:02 -0700

    try downloading golang with curl
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- .circleci/scripts/run-tests-go.sh
+++ .circleci/scripts/run-tests-go.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
+export PATH=$PATH:/usr/local/go/bin # for use on linux
 go version
 
 pushd runtime-testsuite
     echo "running maven tests..."
     export MAVEN_OPTS="-Xmx8g
-    export PATH=$PATH:/usr/local/go/bin # for use on linux
     mvn -Dparallel=classes -DthreadCount=4 -Dtest=go.** test
 popd
