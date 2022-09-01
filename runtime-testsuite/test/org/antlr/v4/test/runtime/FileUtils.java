e7f92503346cc81e3bac8427ece5b11268746286
Ivan Kochurkin <kvanttt@gmail.com>
2022-09-01 19:24:36 +0200

    Remove not used methods from FileUtils (runtime tests)
    Signed-off-by: Ivan Kochurkin <kvanttt@gmail.com>

--- runtime-testsuite/test/org/antlr/v4/test/runtime/FileUtils.java
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/FileUtils.java
@@ -11,8 +11,6 @@ import org.antlr.v4.runtime.misc.Utils;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.EnumSet;
 
 import static org.antlr.v4.test.runtime.RuntimeTestUtils.FileSeparator;
 
@@ -38,29 +36,6 @@ public class FileUtils {
 		return null;
 	}
 
-	public static void copyFile(File source, File dest) throws IOException {
-		InputStream is = Files.newInputStream(source.toPath());
-		OutputStream os = Files.newOutputStream(dest.toPath());
-		byte[] buf = new byte[4 << 10];
-		int l;
-		while ((l = is.read(buf)) > -1) {
-			os.write(buf, 0, l);
-		}
-		is.close();
-		os.close();
-	}
-
-	public static boolean moveFile(File sourceFir, String destDir, String fileName) {
-		File file = new File(sourceFir, fileName);
-		File newFile = new File(destDir, fileName);
-		if (newFile.exists()) {
-			if (!newFile.delete()) {
-				return false;
-			}
-		}
-		return file.renameTo(newFile);
-	}
-
 	public static void replaceInFile(Path sourcePath, String target, String replacement) throws IOException {
 		replaceInFile(sourcePath, sourcePath, target, replacement);
 	}
@@ -75,26 +50,8 @@ public class FileUtils {
 
 	public static void mkdir(String dir) {
 		File f = new File(dir);
+		//noinspection ResultOfMethodCallIgnored
 		f.mkdirs();
-	}
-
-	public static void copyDirectory(final Path source, final Path target, final CopyOption... options)
-			throws IOException {
-		Files.walkFileTree(source, EnumSet.of(FileVisitOption.FOLLOW_LINKS), 2147483647, new SimpleFileVisitor<Path>() {
-			@Override
-			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-					throws IOException {
-				Files.createDirectories(target.resolve(source.relativize(dir)));
-				return FileVisitResult.CONTINUE;
-			}
-
-			@Override
-			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
-					throws IOException {
-				Files.copy(file, target.resolve(source.relativize(file)), options);
-				return FileVisitResult.CONTINUE;
-			}
-		});
 	}
 
 	public static void deleteDirectory(File f) throws IOException {
