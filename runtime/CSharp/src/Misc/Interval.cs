e84d6f012f6011e3566a837ca86a933e0d186044
Ivan Kochurkin <kvanttt@gmail.com>
2022-09-02 00:31:26 +0200

    C# runtime: make Interval struct readonly
    Signed-off-by: Ivan Kochurkin <kvanttt@gmail.com>

--- runtime/CSharp/src/Misc/Interval.cs
+++ runtime/CSharp/src/Misc/Interval.cs
@@ -8,7 +8,7 @@ namespace Antlr4.Runtime.Misc
 {
     /// <summary>An immutable inclusive interval a..b.</summary>
     /// <remarks>An immutable inclusive interval a..b.</remarks>
-    public struct Interval
+    public readonly struct Interval
     {
         public static readonly Antlr4.Runtime.Misc.Interval Invalid = new Antlr4.Runtime.Misc.Interval(-1, -2);
 
