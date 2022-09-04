9a45323edf058e362a03dd4694cecbe6ab2f105d
Terence Parr <parrt@antlr.org>
2022-09-04 11:52:10 -0700

    Damn. java target said 4.10.2 not 4.11.0
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
+++ runtime/Java/src/org/antlr/v4/runtime/RuntimeMetaData.java
@@ -67,7 +67,7 @@ public class RuntimeMetaData {
 	 * omitted.</li>
 	 * </ul>
 	 */
-	public static final String VERSION = "4.10.2-SNAPSHOT";
+	public static final String VERSION = "4.11.0-SNAPSHOT";
 
 	/**
 	 * Gets the currently executing version of the ANTLR 4 runtime library.
