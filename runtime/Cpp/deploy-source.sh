4f51c8e0663c63be851847ad585af04794f123c9
Terence Parr <parrt@antlr.org>
2022-09-04 14:49:58 -0700

    clean up deploy c++ source script
    Signed-off-by: Terence Parr <parrt@antlr.org>
    Signed-off-by: Eric Vergnaud <eric.vergnaud@wanadoo.fr>

--- runtime/Cpp/deploy-source.sh
+++ runtime/Cpp/deploy-source.sh
@@ -3,7 +3,8 @@
 # Zip it
 rm -f antlr4-cpp-runtime-source.zip
 zip -r antlr4-cpp-runtime-source.zip "README.md" "cmake" "demo" "runtime" "CMakeLists.txt" "deploy-macos.sh" "deploy-source.sh" "deploy-windows.cmd" "VERSION" \
-  -X -x "*.DS_Store*" "antlrcpp.xcodeproj/xcuserdata/*" "*Build*" "*DerivedData*" "*.jar" "demo/generated/*" "*.vscode*" "runtime/build/*"
+  -X -x "*.DS_Store*" "antlrcpp.xcodeproj/xcuserdata/*" "*Build*" "*DerivedData*" "*.jar" "demo/generated/*" "*.vscode*" "runtime/build/*" \
+  "runtime/*.dylib" "*.a" "runtime/thirdparty/*" "runtime/CMakeFiles/*"
 
 # Add the license file from the ANTLR root as well.
 pushd ../../
