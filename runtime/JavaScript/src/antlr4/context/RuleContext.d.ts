a9b058fffc9eea531998eb5504080bbd76289c3f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-09-06 13:16:57 +0200

    more successful tests
    Signed-off-by: Eric Vergnaud <eric.vergnaud@wanadoo.fr>

--- runtime/JavaScript/src/antlr4/context/RuleContext.d.ts
+++ runtime/JavaScript/src/antlr4/context/RuleContext.d.ts
@@ -1,8 +1,10 @@
 import RuleNode from "../tree/RuleNode";
+import Parser from "../Parser";
 
 export default class RuleContext extends RuleNode {
     parentCtx: RuleContext | undefined;
     invokingState: number;
 
     get ruleContext() : RuleContext;
+    toStringTree(ruleNames: string[] | null, recog: Parser) : string;
 }
