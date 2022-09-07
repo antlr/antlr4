531654ed47955a68c85c4feddf045176a97ac48c
Jim.Idle <jimi@gatherstars.com>
2022-09-07 11:55:17 +0800

    fix: Restore missing changes to v4 of go runtime
    I had not though about this, but I guess some of the fix PRs did not have one fix
    that was in the legacy version of the runtime, copied in to the v4 branch. This
    commit fixes that.
    
    Signed-off-by: Jim.Idle <jimi@gatherstars.com>
    Signed-off-by: 1sand0s <1sand0sardpi@gmail.com>

--- runtime/Go/antlr/antlrdoc.go
+++ runtime/Go/antlr/antlrdoc.go
@@ -48,7 +48,7 @@ And the generate.sh file will look similar to this:
 	#!/bin/sh
 
 	alias antlr4='java -Xmx500M -cp "./antlr4-4.11.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-	antlr4 -Dlanguage=Go -no-visitor -package tgram *.g4
+	antlr4 -Dlanguage=Go -no-visitor -package parser *.g4
 
 depending on whether you want visitors or listeners or any other ANTLR options.
 
