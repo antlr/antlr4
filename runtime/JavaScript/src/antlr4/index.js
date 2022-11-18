1796af4dbe157f3fedff106cc50baaf5780b60de
Terence Parr <parrt@antlr.org>
2022-11-18 15:22:12 -0800

    Make javascript trace sim like java.
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- runtime/JavaScript/src/antlr4/index.js
+++ runtime/JavaScript/src/antlr4/index.js
@@ -5,13 +5,14 @@
 import './utils/stringHashCode.js';
 import './polyfills/codepointat.js';
 import './polyfills/fromcodepoint.js';
-import { default as atn } from './atn/index.js';
-import { default as dfa } from './dfa/index.js';
-import { default as tree } from './tree/index.js';
-import { default as error } from './error/index.js';
+import {default as atn} from './atn/index.js';
+import {default as dfa} from './dfa/index.js';
+import {default as tree} from './tree/index.js';
+import {default as error} from './error/index.js';
+import {default as context} from './context/index.js';
 import Token from './Token.js';
 import CommonToken from './CommonToken.js';
-import { default as CharStreams } from './CharStreams.js';
+import {default as CharStreams} from './CharStreams.js';
 import InputStream from './InputStream.js';
 import FileStream from './FileStream.js';
 import CommonTokenStream from './CommonTokenStream.js';
@@ -22,10 +23,10 @@ import ParserRuleContext from './context/ParserRuleContext.js';
 import Interval from './misc/Interval.js';
 import IntervalSet from './misc/IntervalSet.js';
 import LL1Analyzer from './atn/LL1Analyzer.js';
-import { default as Utils } from './utils/index.js';
+import {default as Utils} from './utils/index.js';
 
 const antlr4 = {
-    atn, dfa, tree, error, Token, CommonToken, CharStreams, InputStream, FileStream, CommonTokenStream, Lexer, Parser,
+    atn, dfa, tree, error, context, Token, CommonToken, CharStreams, InputStream, FileStream, CommonTokenStream, Lexer, Parser,
     PredictionContextCache, ParserRuleContext, Interval, IntervalSet, LL1Analyzer, Utils
 };
 
