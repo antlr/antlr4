a4e924d8fdc36d2a139d5f85bed4f9f493f034b4
Terence Parr <parrt@antlr.org>
2022-11-21 13:16:58 -0800

    More exclude for Swift test grammars in Package.swift
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- Package.swift
+++ Package.swift
@@ -23,11 +23,27 @@ let package = Package(
             dependencies: ["Antlr4"],
             path: "./runtime/Swift/Tests/Antlr4Tests",
             exclude: [
-                "./VisitorBasic.g4",
-                "./VisitorCalc.g4",
-                "./LexerA.g4",
-                "./LexerB.g4",
-                "./Threading.g4"
+                "gen/LexerB.tokens",
+                "gen/VisitorCalcLexer.tokens",
+                "gen/VisitorBasicLexer.tokens",
+                "gen/VisitorBasic.tokens",
+                "gen/Threading.interp",
+                "gen/VisitorCalcLexer.interp",
+                "gen/ThreadingLexer.interp",
+                "gen/VisitorBasicLexer.interp",
+                "gen/LexerB.interp",
+                "gen/Threading.tokens",
+                "gen/VisitorCalc.interp",
+                "gen/LexerA.interp",
+                "gen/ThreadingLexer.tokens",
+                "gen/LexerA.tokens",
+                "gen/VisitorCalc.tokens",
+                "gen/VisitorBasic.interp",
+                "VisitorBasic.g4",
+                "VisitorCalc.g4",
+                "LexerA.g4",
+                "LexerB.g4",
+                "Threading.g4"
             ]
         )
     ]
