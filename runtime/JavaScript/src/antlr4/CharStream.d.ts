e79d8df37c24066e2d20ef19e1947b7779269208
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-12-31 17:07:08 +0100

    fix missing APIs

--- runtime/JavaScript/src/antlr4/CharStream.d.ts
+++ runtime/JavaScript/src/antlr4/CharStream.d.ts
@@ -1,11 +1,17 @@
 export declare class CharStream {
 
     index: number; // defined as property
+    size: number;// defined as property
 
     constructor(data: string);
     constructor(data: string, decodeToUnicodeCodePoints: boolean);
-    seek(index: number): void;
+    reset(): void;
     consume(): void;
-    getText(start: number, stop: number): string;
     LA(offset: number): number;
+    LT(offset: number): number;
+    mark(): number;
+    release(marker: number): void;
+    seek(index: number): void;
+    getText(start: number, stop: number): string;
+    toString(): string;
 }
