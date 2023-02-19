207314792d851bcb11a409ce46ca34f37fd22fbd
Arthur Sonzogni <sonzogniarthur@gmail.com>
2023-02-19 20:20:16 +0100

    cpp runtime: Remove pthread dependency. (#4086)
    ANTLR doesn't use threads, and it used not to depend on pthread library
    either.
    
    It changed recently in 2022: https://github.com/antlr/antlr4/issues/3708
    The patch linked against pthread, because the GNU libstdc++ used to
    depend on it in their implementation of `std::call_once`.
    
    By the way, the libstdc++ stopped it in 2020:
    https://gcc.gnu.org/git/gitweb.cgi?p=gcc.git;h=93e79ed391b9c636f087e6eb7e70f14963cd10ad
    So this is not more needed.
    
    I would like to stop depending on pthread. I am using ANTLR in C++
    WebAssembly for the website:
    https://arthursonzogni.com/Diagon/
    
    It doesn't compile with emscripten anymore, because by default pthread
    is not enabled. It could be enabled, but it would force me to deploy
    cross-origin-isolation:
    https://web.dev/cross-origin-isolation-guide/
    
    Solutions:
    1. Stop linking against pthread, because the libstdc++ stopped depending
       on it for std::call_once.
    2. Implement std::call_once ourself using std::atomic_flag
    3. Implement std::call_once ourself using a boolean flag, assuming we
       don't need to support threads.
    
    I chose to do (2) in this patch.
    
    Signed-off-by: ArthurSonzogni <sonzogniarthur@gmail.com>

--- runtime/Cpp/runtime/src/internal/Synchronization.h
+++ runtime/Cpp/runtime/src/internal/Synchronization.h
@@ -27,12 +27,13 @@
 
 #include "antlr4-common.h"
 
+#include <atomic>
+#include <functional>
 #include <mutex>
 #include <shared_mutex>
 #include <utility>
 
 #if ANTLR4CPP_USING_ABSEIL
-#include "absl/base/call_once.h"
 #include "absl/base/thread_annotations.h"
 #include "absl/synchronization/mutex.h"
 #define ANTLR4CPP_NO_THREAD_SAFTEY_ANALYSIS ABSL_NO_THREAD_SAFETY_ANALYSIS
@@ -135,20 +136,15 @@ namespace antlr4::internal {
     template <typename Callable, typename... Args>
     friend void call_once(OnceFlag &onceFlag, Callable &&callable, Args&&... args);
 
-#if ANTLR4CPP_USING_ABSEIL
-    absl::once_flag _impl;
-#else
-    std::once_flag _impl;
-#endif
+    std::atomic_flag _flag = ATOMIC_FLAG_INIT;
   };
 
   template <typename Callable, typename... Args>
-  void call_once(OnceFlag &onceFlag, Callable &&callable, Args&&... args) {
-#if ANTLR4CPP_USING_ABSEIL
-    absl::call_once(onceFlag._impl, std::forward<Callable>(callable), std::forward<Args>(args)...);
-#else
-    std::call_once(onceFlag._impl, std::forward<Callable>(callable), std::forward<Args>(args)...);
-#endif
+  void call_once(OnceFlag &onceFlag, Callable &&callable, Args &&...args) {
+    if (onceFlag._flag.test_and_set()) {
+      return;
+    }
+    std::invoke(std::forward<Callable>(callable), std::forward<Args>(args)...);
   }
 
 }  // namespace antlr4::internal
