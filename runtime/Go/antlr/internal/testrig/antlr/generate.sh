a10669a4969604ec9ff9f5f5f45aef4463292cf4
Jim.Idle <jimi@idle.ws>
2023-04-13 11:16:04 +0800

    feat: Remove internal test rig from the go runtime source
      - I will store this in my own repo and direct any future maintainers to there
        this will avoid any confusion.
    
    Signed-off-by: Jim.Idle <jimi@idle.ws>

--- runtime/Go/antlr/internal/testrig/antlr/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-alias antlr4='java -Xmx500M -cp "./antlr4-4.12.1-SNAPSHOT-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-
-antlr4 -Dlanguage=Go -visitor -listener -package test -o ../test *.g4
