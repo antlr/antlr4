d0440f4673cd177eba00593685c1575e988b2c94
Robert Einhorn <robert.einhorn.hu@gmail.com>
2024-01-21 19:05:45 +0100

    added Token.DEFAULT_CHANNEL and Token.HIDDEN_CHANNEL (#4516)
    * added DEFAULT_CHANNEL and HIDDEN_CHANNEL constants
    
    based on:
    https://github.com/antlr/antlr4/blob/ebb511a04a60ae5a605aba65471c07dd854e9303/runtime/JavaScript/src/antlr4/Token.js#L59
    
    https://github.com/antlr/antlr4/blob/ebb511a04a60ae5a605aba65471c07dd854e9303/runtime/JavaScript/src/antlr4/Token.js#L65
    Signed-off-by: Robert Einhorn <robert.einhorn.hu@gmail.com>
    
    * added DEFAULT_CHANNEL and HIDDEN_CHANNEL constants
    
    https://github.com/antlr/antlr4/issues/4110#issuecomment-1898214382
    Signed-off-by: Robert Einhorn <robert.einhorn.hu@gmail.com>
    
    ---------
    
    Signed-off-by: Robert Einhorn <robert.einhorn.hu@gmail.com>

--- runtime/JavaScript/src/antlr4/Token.d.ts
+++ runtime/JavaScript/src/antlr4/Token.d.ts
@@ -3,6 +3,9 @@ import {CharStream} from "./CharStream";
 export declare class Token {
 
     static EOF: number;
+    
+    static DEFAULT_CHANNEL: number;
+    static HIDDEN_CHANNEL: number;
 
     tokenIndex: number;
     line: number;
