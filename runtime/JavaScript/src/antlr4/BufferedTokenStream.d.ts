e31915c5acfc1c213f08a7915e12d355bc6b5448
Phlosioneer <mattmdrr2@gmail.com>
2024-03-31 17:12:29 -0400

    Add methods for BufferedTokenStream
    The typescript class was almost completely empty, so it was not possible
    to directly use the token stream in a type-safe way.
    
    Signed-off-by: Phlosioneer <mattmdrr2@gmail.com>

--- runtime/JavaScript/src/antlr4/BufferedTokenStream.d.ts
+++ runtime/JavaScript/src/antlr4/BufferedTokenStream.d.ts
@@ -1,8 +1,26 @@
 import { TokenStream } from './TokenStream';
-import { Lexer } from "./Lexer";
+import { TokenSource } from './TokenSource';
+import { Token } from './Token';
 
 export declare class BufferedTokenStream extends TokenStream {
 
-    tokenSource: Lexer;
+    tokenSource: TokenSource;
+    tokens: Token[];
+    fetchedEof: boolean;
 
+    constructor(source: TokenSource);
+    setTokenSource(tokenSource: TokenSource): void;
+    mark(): number;
+    release(marker: number): void;
+    reset(): void;
+    seek(index: number): void;
+    consume(): void;
+    sync(i: number): boolean;
+    fetch(n: number): number;
+    LB(k: number): Token;
+    nextTokenOnChannel(i: number, channel: number): number;
+    previousTokenOnChannel(i: number, channel: number): number;
+
+    protected lazyInit(): void;
+    protected adjustSeekIndex(i: number): number;
 }
