dc1070b6605d83859e6147749a8d3840ea89b674
eury.tang <eury0811@gmail.com>
2024-05-21 19:15:43 -0700

    fix(TypeScript): update and add missing definition in IntervalSEt.d.ts
    Signed-off-by: eury.tang <eury0811@gmail.com>

--- runtime/JavaScript/src/antlr4/misc/IntervalSet.d.ts
+++ runtime/JavaScript/src/antlr4/misc/IntervalSet.d.ts
@@ -1,13 +1,41 @@
 import {Interval} from "./Interval";
+import { Token } from "../Token";
 
-export declare class IntervalSet {
+export default class IntervalSet {
+    private intervals: Interval[] | null;
+    private readOnly: boolean;
 
-    isNil: boolean;
-    size: number;
-    minElement: number;
-    maxElement: number;
-    intervals: Interval[];
+    constructor();
 
-    contains(i: number): boolean;
-    toString(literalNames?: (string | null)[], symbolicNames?: string[], elemsAreChar?: boolean): string;
+    first(v: number): number;
+
+    addOne(v: number): void;
+
+    addRange(l: number, h: number): void;
+
+    addInterval(toAdd: Interval): void;
+
+    addSet(other: IntervalSet): IntervalSet;
+
+    private reduce(pos: number): void;
+
+    complement(start: number, stop: number): IntervalSet;
+
+    contains(item: number): boolean;
+
+    removeRange(toRemove: Interval): void;
+
+    removeOne(value: number): void;
+
+    toString(literalNames?: string[], symbolicNames?: string[], elemsAreChar?: boolean): string;
+
+    private toCharString(): string;
+
+    private toIndexString(): string;
+
+    private toTokenString(literalNames: string[], symbolicNames: string[]): string;
+
+    private elementName(literalNames: string[], symbolicNames: string[], token: number): string;
+
+    get length(): number;
 }
