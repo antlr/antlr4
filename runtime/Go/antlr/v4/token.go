5b9c537649f80d149d3613fb29eb69f3923fd64f
Robert Einhorn <robert.einhorn.hu@gmail.com>
2024-06-01 06:46:00 +0200

    add some method
    Signed-off-by: Robert Einhorn <robert.einhorn.hu@gmail.com>

--- runtime/Go/antlr/v4/token.go
+++ runtime/Go/antlr/v4/token.go
@@ -192,19 +192,13 @@ func NewCommonToken(source *TokenSourceCharStreamPair, tokenType, channel, start
 
 //CommonToken.EMPTY_SOURCE = [ nil, nil ]
 
-// Constructs a New{@link CommonToken} as a copy of another {@link Token}.
-//
-// <p>
-// If {@code oldToken} is also a {@link CommonToken} instance, the newly
-// constructed token will share a reference to the {@link //text} field and
-// the {@link Pair} stored in {@link //source}. Otherwise, {@link //text} will
-// be assigned the result of calling {@link //GetText}, and {@link //source}
-// will be constructed from the result of {@link Token//GetTokenSource} and
-// {@link Token//GetInputStream}.</p>
-//
-// @param oldToken The token to copy.
-func (c *CommonToken) Clone() *CommonToken {
-	return &CommonToken{c.BaseToken}
+func NewCommonTokenFromToken(oldToken Token) *CommonToken {
+	t := NewCommonToken(oldToken.GetSource(), oldToken.GetTokenType(), oldToken.GetChannel(), oldToken.GetStart(), oldToken.GetStop())
+	t.tokenIndex = oldToken.GetTokenIndex()
+	t.line = oldToken.GetLine()
+	t.column = oldToken.GetColumn()
+	t.text = oldToken.GetText()
+	return t
 }
 
 func (c *CommonToken) SetChannel(channel int) {
