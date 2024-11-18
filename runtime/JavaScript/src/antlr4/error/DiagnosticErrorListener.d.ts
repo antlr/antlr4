415030cdb46773187833d0530fee31070f426670
Francisco Rodriguez Elorza <francisco.rodriguez.elorza@oracle.com>
2024-11-18 11:36:48 -0800

    update DiagnosticErrorListener type
    Signed-off-by: Francisco Rodriguez Elorza <francisco.rodriguez.elorza@oracle.com>

--- runtime/JavaScript/src/antlr4/error/DiagnosticErrorListener.d.ts
+++ runtime/JavaScript/src/antlr4/error/DiagnosticErrorListener.d.ts
@@ -1,16 +1,3 @@
-import {ATNConfigSet} from "../atn";
-import {BitSet} from "../misc/BitSet";
-import {DFA} from "../dfa";
 import {ErrorListener} from "./ErrorListener";
-import {Recognizer} from "../Recognizer";
-import {RecognitionException} from "./RecognitionException";
 
-export declare class DiagnosticErrorListener<TSymbol> implements ErrorListener<TSymbol> {
-    reportAmbiguity(recognizer: Recognizer<TSymbol>, dfa: DFA, startIndex: number, stopIndex: number, exact: boolean, ambigAlts: BitSet, configs: ATNConfigSet): void;
-    
-    reportAttemptingFullContext(recognizer: Recognizer<TSymbol>, dfa: DFA, startIndex: number, stopIndex: number, conflictingAlts: BitSet, configs: ATNConfigSet): void;
-    
-    reportContextSensitivity(recognizer: Recognizer<TSymbol>, dfa: DFA, startIndex: number, stopIndex: number, prediction: number, configs: ATNConfigSet): void;
-
-    syntaxError(recognizer: Recognizer<TSymbol>, offendingSymbol: TSymbol, line: number, column: number, msg: string, e: RecognitionException | undefined): void;
-}
+export declare class DiagnosticErrorListener<TSymbol> extends ErrorListener<TSymbol> {}
