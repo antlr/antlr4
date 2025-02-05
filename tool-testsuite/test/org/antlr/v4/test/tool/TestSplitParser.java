93979bc183ccba347bb8fd4b159b31f32d53d3e2
Eric Vergnaud <eric.vergnaud@databricks.com>
2025-02-05 09:40:46 +0100

    check that generated files compile

--- tool-testsuite/test/org/antlr/v4/test/tool/TestSplitParser.java
+++ tool-testsuite/test/org/antlr/v4/test/tool/TestSplitParser.java
@@ -1,12 +1,17 @@
 package org.antlr.v4.test.tool;
 
 import org.antlr.v4.Tool;
-import org.antlr.v4.runtime.ParserRuleContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -15,9 +20,12 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.antlr.v4.test.runtime.RuntimeTestUtils.PathSeparator;
+
 
 public class TestSplitParser {
 
+	JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 	Path outDir = null;
 
 	@BeforeEach
@@ -33,6 +41,7 @@ public class TestSplitParser {
 		assert created.contains("JavaParser.java");
 		assert created.contains("JavaBaseListener.java");
 		assert created.contains("JavaListener.java");
+		checkCompiles(created);
 	}
 
 	@Test
@@ -41,6 +50,7 @@ public class TestSplitParser {
 		assert created.size() == 2;
 		assert created.contains("JavaLexer.java");
 		assert created.contains("JavaParser.java");
+		checkCompiles(created);
 	}
 
 	@Test
@@ -53,6 +63,7 @@ public class TestSplitParser {
 		assert created.contains("JavaListener.java");
 		assert created.contains("JavaBaseVisitor.java");
 		assert created.contains("JavaVisitor.java");
+		checkCompiles(created);
 	}
 
 	@Test
@@ -65,11 +76,14 @@ public class TestSplitParser {
 		assert created.contains("JavaParserDFA.java");
 		String content = Files.readString(Path.of(outDir.toString(), "JavaParser.java"));
 		assert !content.contains("ATNDeserializer()");
-		// assert !content.contains("extends ParserRuleContext");
+		assert !content.contains("extends ParserRuleContext");
 		content = Files.readString(Path.of(outDir.toString(), "JavaParserContexts.java"));
-		// assert content.contains("extends ParserRuleContext");
+		assert !content.contains("ATNDeserializer()");
+		assert content.contains("extends ParserRuleContext");
 		content = Files.readString(Path.of(outDir.toString(), "JavaParserDFA.java"));
 		assert content.contains("ATNDeserializer()");
+		assert !content.contains("extends ParserRuleContext");
+		checkCompiles(created);
 	}
 
 	Set<String> generate(String ... args) throws IOException {
@@ -80,6 +94,8 @@ public class TestSplitParser {
 		String path = url.getPath();
 		options.add("-o");
 		options.add(outDir.toString());
+		options.add("-package");
+		options.add("antlr4.split");
 		options.add(path);
 		Tool antlr = new Tool(options.toArray(new String[] {}));
 		antlr.processGrammarsOnCommandLine();
@@ -89,5 +105,28 @@ public class TestSplitParser {
 			.filter(name -> name.endsWith(".java"))
 			.collect(Collectors.toSet());
 
+	}
+
+	void checkCompiles(Set<String> generated) throws Exception, ClassNotFoundException {
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+		List<File> files = generated.stream()
+			.map(name -> Path.of(outDir.toString(), name))
+			.map(Path::toFile)
+			.collect(Collectors.toList());
+		Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(files);
+		Iterable<String> compileOptions =
+			Arrays.asList("-g", "-source", "1.8", "-target", "1.8", "-implicit:class", "-Xlint:-options", "-d",
+				outDir.toString(), "-cp", outDir + PathSeparator + System.getProperty("java.class.path"));
+		JavaCompiler.CompilationTask task =
+			compiler.getTask(null, fileManager, null, compileOptions, null,
+				compilationUnits);
+		task.call();
+		ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+		ClassLoader loader = new URLClassLoader(new URL[]{outDir.toUri().toURL()}, systemClassLoader);
+		for (String name : generated) {
+			name = "antlr4.split." + name.substring(0, name.indexOf(".java"));
+			Class<?> klass = loader.loadClass(name);
+			assert klass != null;
+		}
 	}
 }
