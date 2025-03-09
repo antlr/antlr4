1cc60719a56a4ce9c0564a7705dbe0275d78b3df
Henner Zeller <h.zeller@acm.org>
2025-03-09 16:25:45 -0700

    Include `<unordered_map>` in files using `std::unordered_map`
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

--- runtime/Cpp/runtime/src/TokenStreamRewriter.h
+++ runtime/Cpp/runtime/src/TokenStreamRewriter.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include <unordered_map>
 #include <map>
 #include <vector>
 #include <string>
--- runtime/Cpp/runtime/src/atn/PredictionMode.cpp
+++ runtime/Cpp/runtime/src/atn/PredictionMode.cpp
@@ -3,6 +3,7 @@
  * can be found in the LICENSE.txt file in the project root.
  */
 
+#include <unordered_map>
 #include <memory>
 #include <vector>
 #include <cstddef>
